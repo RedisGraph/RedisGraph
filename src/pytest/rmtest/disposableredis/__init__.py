575af4ec12196da587d0ec76d55e1b1e2d6a1da8
swilly22 <roi@redislabs.com>
2017-01-27 14:24:15 +0200

    functional test

---
+++ src/pytest/rmtest/disposableredis/__init__.py
@@ -0,0 +1,107 @@
+import subprocess
+import socket
+import tempfile
+import redis
+import time
+import os
+import itertools
+from contextlib import contextmanager
+
+
+def get_random_port():
+    sock = socket.socket()
+    sock.listen(0)
+    _, port = sock.getsockname()
+    sock.close()
+
+    return port
+
+
+class Client(redis.StrictRedis):
+
+    def __init__(self, disposable_redis, port):
+
+        redis.StrictRedis.__init__(self, port=port)
+        self.dr = disposable_redis
+
+    def retry_with_rdb_reload(self):
+
+            yield 1
+            self.dr.dump_and_reload()
+            yield 2
+
+class DisposableRedis(object):
+
+    def __init__(self, port=None, path='redis-server', **extra_args):
+        """
+        :param port: port number to start the redis server on. Specify none to automatically generate
+        :type port: int|None
+        :param extra_args: any extra arguments kwargs will be passed to redis server as --key val
+        """
+
+        self._port = port
+
+        # this will hold the actual port the redis is listening on. It's equal to `_port` unless `_port` is None
+        # in that case `port` is randomly generated
+        self.port = None
+        self.extra_args = list(itertools.chain(
+            *(('--%s' % k, v) for k, v in extra_args.items())
+        ))
+        self.path = path
+        self.dumped = False
+
+    def _startProcess(self):
+
+        self.process = subprocess.Popen(
+            self.args,
+            stdin=subprocess.PIPE,
+            stdout=open(os.devnull, 'w')
+        )
+
+        while True:
+            try:
+                self.client().ping()
+                break
+            except redis.ConnectionError:
+                self.process.poll()
+                if self.process.returncode is not None:
+                    raise RuntimeError(
+                        "Process has exited with code {}".format(self.process.returncode))
+                time.sleep(0.1)
+
+    def __enter__(self):
+        if self._port is None:
+            self.port = get_random_port()
+        else:
+            self.port = self._port
+
+        self.dumpfile = 'dump.%s.rdb' % self.port
+        self.args = [self.path,
+                '--port', str(self.port),
+                '--dir', tempfile.gettempdir(),
+                '--save', '',
+                '--dbfilename', self.dumpfile] + self.extra_args
+
+
+        self._startProcess()
+        return self.client()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.process.terminate()
+        if self.dumped:
+            os.unlink(os.path.join(tempfile.gettempdir(), self.dumpfile))
+
+    def dump_and_reload(self):
+
+        conn = self.client()
+        conn.save()
+        self.dumped = True
+        self.process.terminate()
+        self._startProcess()
+
+    def client(self):
+        """
+        :rtype: redis.StrictRedis
+        """
+
+        return Client(self, self.port)
---
+++ src/pytest/test.py
@@ -0,0 +1,183 @@
+from rmtest import ModuleTestCase
+import unittest
+
+class GraphTestCase(ModuleTestCase('../libmodule.so')):
+
+    def create_graph(self, r):
+        """Creates a simple graph"""
+        r.flushdb()
+        r.hmset('Roi', {'age':'32', 'name': 'Roi'})
+        r.hmset('Alon', {'age': '32', 'name': 'Alon'})
+        r.hmset('Omri', {'age': '33', 'name': 'Omri'})
+        r.hmset('Boaz', {'age': '31', 'name': 'Boaz'})
+        self.assertOk(r.execute_command(
+            'graph.ADDEDGE', 'social', 'Roi', 'friend', 'Alon'))
+        self.assertOk(r.execute_command(
+            'graph.ADDEDGE', 'social', 'Roi', 'friend', 'Omri'))
+        self.assertOk(r.execute_command(
+            'graph.ADDEDGE', 'social', 'Roi', 'friend', 'Boaz'))
+
+    def tes_graph_add(self):
+        """Validates adding edge to graph"""
+        with self.redis() as r:
+            r.flushdb()
+            r.execute_command(
+                'graph.ADDEDGE', 'social', 'Roi Lipman', 'friend', 'Alon Fital')
+
+    def tes_graph_remove(self):
+        """Validates removing edge from graph"""
+        with self.redis() as r:
+            r.flushdb()
+
+            r.execute_command(
+                'graph.ADDEDGE', 'social', 'Roi Lipman', 'friend', 'Alon Fital')
+
+            self.assertOk(r.execute_command(
+                'graph.REMOVEEDGE', 'social', 'Roi Lipman', 'friend', 'Alon Fital'))
+
+            # Second deletion
+            self.assertOk(r.execute_command(
+                'graph.REMOVEEDGE', 'social', 'Roi Lipman', 'friend', 'Alon Fital'))
+
+    def tes_graph_delete(self):
+        """Validates graph deletion"""
+        with self.redis() as r:
+            r.flushdb()
+            r.execute_command(
+                'graph.ADDEDGE', 'social', 'Roi Lipman', 'friend', 'Alon Fital')
+
+            self.assertOk(r.execute_command('graph.DELETE', 'social'))
+
+    def test_agg_sum(self):
+        """Validates sum aggregate function"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY', 'social', 'MATCH (ME:Roi)-[friend]->(f) RETURN sum(f.age)')
+
+            age_sum = int(float(result_set[0]))
+            self.assertEqual(age_sum, 96)
+
+    def test_agg_avg(self):
+        """Validates avg aggregate function"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY', 'social', 'MATCH (ME:Roi)-[friend]->(f) RETURN avg(f.age)')
+
+            age_avg = int(float(result_set[0]))
+            self.assertEqual(age_avg, 32)
+
+    def test_agg_max(self):
+        """Validates max aggregate function"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY', 'social', 'MATCH (ME:Roi)-[friend]->(f) RETURN max(f.age)')
+
+            age_max = int(float(result_set[0]))
+            self.assertEqual(age_max, 33)
+
+    def test_agg_min(self):
+        """Validates min aggregate function"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY', 'social', 'MATCH (ME:Roi)-[friend]->(f) RETURN min(f.age)')
+
+            age_min = int(float(result_set[0]))
+            self.assertEqual(age_min, 31)
+
+    def test_agg_count(self):
+        """Validates count aggregate function"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY', 'social', 'MATCH (ME:Roi)-[friend]->(f) RETURN count(f.age)')
+
+            age_count = int(float(result_set[0]))
+            self.assertEqual(age_count, 3)
+
+    def test_limit(self):
+        """Validates limited result-set"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY', 'social', 'MATCH (ME:Roi)-[friend]->(f) RETURN f.name LIMIT 2'
+            )
+
+            # additional record for run time stats
+            self.assertEqual(len(result_set), 3)
+
+    def test_order(self):
+        """Validates result-set order default is ascending"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY',
+                'social',
+                'MATCH (ME:Roi)-[friend]->(f) RETURN f.age ORDER BY f.age')
+
+            for i in range(len(result_set)-2):
+                age = int(float(result_set[i]))
+                next_age = int(float(result_set[i+1]))
+                self.assertLessEqual(age, next_age)
+
+    def test_order_asc(self):
+        """Validates result-set order, default is ascending"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY',
+                'social',
+                'MATCH (ME:Roi)-[friend]->(f) RETURN f.age ORDER BY f.age ASC')
+
+            for i in range(len(result_set)-2):
+                age = int(float(result_set[i]))
+                next_age = int(float(result_set[i+1]))
+                self.assertLessEqual(age, next_age)
+
+    def test_order_desc(self):
+        """Validates result-set order"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY',
+                'social',
+                'MATCH (ME:Roi)-[friend]->(f) RETURN f.age ORDER BY f.age DESC')
+
+            for i in range(len(result_set)-2):
+                age = int(float(result_set[i]))
+                next_age = int(float(result_set[i+1]))
+                self.assertGreaterEqual(age, next_age)
+
+    def test_limited_order(self):
+        """Validates result-set order"""
+        with self.redis() as r:
+            self.create_graph(r)
+
+            result_set = r.execute_command(
+                'graph.QUERY',
+                'social',
+                'MATCH (ME:Roi)-[friend]->(f) RETURN f.age ORDER BY f.age LIMIT 2')
+
+            # additional record for run time stats
+            self.assertEquals(len(result_set), 3)
+
+            for i in range(len(result_set)-2):
+                age = int(float(result_set[i]))
+                next_age = int(float(result_set[i+1]))
+                self.assertLessEqual(age, next_age)
+
+
+if __name__ == '__main__':
+    unittest.main()
