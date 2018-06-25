725cdf433a40439a2e6b641263dab856eb9c2c5c
Jeffrey Lovitz <jlovitz@emcien.com>
2018-06-25 12:25:09 -0400

    Test suite uses updated string functions

--- tests/unit/test_value.c
+++ tests/unit/test_value.c
@@ -6,26 +6,26 @@
 void test_value_parse() {
     SIValue v;
     char *str = "12345";
-    SIValue_FromString(&v, str, strlen(str));
+    SIValue_FromString(&v, str);
     assert(v.type == T_DOUBLE);
     assert(v.doubleval == 12345);
 
     str = "3.14";
-    SIValue_FromString(&v, str, strlen(str));
+    SIValue_FromString(&v, str);
     assert(v.type == T_DOUBLE);
     
     /* Almost equals. */
     assert((v.doubleval - 3.14) < 0.0001);
 
     str = "-9876";
-    SIValue_FromString(&v, str, strlen(str));
+    SIValue_FromString(&v, str);
     assert(v.type == T_DOUBLE);
     assert(v.doubleval == -9876);
 
     str = "Test!";
-    SIValue_FromString(&v, str, strlen(str));
+    SIValue_FromString(&v, str);
     assert(v.type == T_STRING);
-    assert(strcmp(v.stringval.str, "Test!") == 0);
+    assert(strcmp(v.stringval, "Test!") == 0);
 }
 
 int main(int argc, char **argv) {
