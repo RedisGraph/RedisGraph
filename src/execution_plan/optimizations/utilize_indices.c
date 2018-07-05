e178a700cdad609d1cf866edd19ea7f3fe483816
Jeffrey Lovitz <jlovitz@emcien.com>
2018-07-05 15:08:08 -0400

    WIP Improve logic for injecting index scans

--- src/execution_plan/optimizations/utilize_indices.c
+++ src/execution_plan/optimizations/utilize_indices.c
@@ -9,28 +9,31 @@
 void _utilizeIndices(RedisModuleCtx *ctx, ExecutionPlan *plan, const char *graph_name, Graph *g, OpNode *op) {
   if (op == NULL) return;
 
-  NodeByLabelScan *scan_op;
+  OpNode *scan_op;
+  Node **scan_target;
   OpNode *index_op;
   char *label;
   if (op->operation->type == OPType_FILTER) {
-    if (op->childCount == 1 && op->children[0]->operation->type == OPType_NODE_BY_LABEL_SCAN) {
-      scan_op = (NodeByLabelScan*)op->children[0]->operation;
-      Vector *filters = FilterTree_CollectAliasConsts(plan->filter_tree,
-          QueryGraph_GetNodeAlias(plan->graph, *scan_op->node));
+    if (op->children[0]->operation->type == OPType_NODE_BY_LABEL_SCAN) {
+      scan_op = op->children[0];
+      scan_target = ((NodeByLabelScan*)scan_op->operation)->node;
+      // TODO replace with better FT traversal
+      Vector *filters = FilterTree_CollectAliasConsts(((Filter*)op->operation)->filterTree,
+          QueryGraph_GetNodeAlias(plan->graph, *scan_target));
       if (filters != NULL) {
-        IndexCreateIter *iter = Index_IntersectFilters(ctx, graph_name, filters, (*scan_op->node)->label);
+        IndexIter *iter = Index_IntersectFilters(ctx, graph_name, filters, (*scan_target)->label);
         Vector_Free(filters);
         if (iter != NULL) {
-          index_op = NewOpNode(NewIndexScanOp(plan->graph, g, scan_op->node, iter));
-          NodeByLabelScanFree((OpBase*)scan_op);
-          op->children[0] = index_op;
+          index_op = NewOpNode(NewIndexScanOp(plan->graph, g, scan_target, iter));
+          ExecutionPlan_ReplaceOp(scan_op, index_op);
         }
       }
     }
-
   }
-  if (op->childCount <= 0) return;
-  return _utilizeIndices(ctx, plan, graph_name, g, op->children[0]);
+
+  for (int i = 0; i < op->childCount; i ++) {
+    _utilizeIndices(ctx, plan, graph_name, g, op->children[i]);
+  }
 }
 
 void utilizeIndices(RedisModuleCtx *ctx, ExecutionPlan *plan, const char *graph_name, Graph *g) {
