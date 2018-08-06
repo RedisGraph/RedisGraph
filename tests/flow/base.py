baabfedcc57b1d4aaaada8cdec6abb749f712d3e
swilly22 <roi@redislabs.com>
2018-08-06 12:25:58 +0300

    do not fail test on query execution time

--- tests/flow/base.py
+++ tests/flow/base.py
@@ -1,4 +1,5 @@
 import os
+import warnings
 from rmtest import ModuleTestCase
 
 
@@ -41,7 +42,11 @@ class FlowTestsBase(ModuleTestCase(os.path.dirname(os.path.abspath(__file__)) + 
                          (str(actual_result_set), str(query_info.expected_result)))
 
     def _assert_run_time(self, actual_result, query_info):
-        self.assertLessEqual(actual_result.run_time_ms,
+
+        try:
+            self.assertLessEqual(actual_result.run_time_ms,
                              query_info.max_run_time_ms,
                              'Maximum runtime for query \"%s\" was: %s, but shoud be %s' %
                              (query_info.description, str(actual_result.run_time_ms), str(query_info.max_run_time_ms)))
+        except AssertionError as e:
+            warnings.warn(e.message)
