d2f2248268863542a98108dc97b25f3ec2e8b168
Roi Lipman <swilly22@users.noreply.github.com>
2018-10-14 08:31:42 +0300

    semantic version (#154)

---
+++ src/version.h
@@ -0,0 +1,17 @@
+/*
+* Copyright 2018-2019 Redis Labs Ltd. and Contributors
+*
+* This file is available under the Apache License, Version 2.0,
+* modified with the Commons Clause restriction.
+*/
+
+#ifndef REDISGRAPH_MODULE_VERSION
+
+#define REDISGRAPH_VERSION_MAJOR 1
+#define REDISGRAPH_VERSION_MINOR 0
+#define REDISGRAPH_VERSION_PATCH 0
+
+#define REDISGRAPH_MODULE_VERSION \
+  (REDISGRAPH_VERSION_MAJOR * 10000 + REDISGRAPH_VERSION_MINOR * 100 + REDISGRAPH_VERSION_PATCH)
+
+#endif
