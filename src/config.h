0f2fe7ef8928c648b5623c50689a1a5c1ffed9a7
Roi Lipman <swilly22@users.noreply.github.com>
2018-10-29 20:54:01 +0200

    try to get number of threads from command line arguments (#184)

---
+++ src/config.h
@@ -0,0 +1,25 @@
+/*
+* Copyright 2018-2019 Redis Labs Ltd. and Contributors
+*
+* This file is available under the Apache License, Version 2.0,
+* modified with the Commons Clause restriction.
+*/
+
+#ifndef _REDISGRAPH_CONFIG_
+#define _REDISGRAPH_CONFIG_
+
+#include "redismodule.h"
+
+#define THREAD_COUNT "THREAD_COUNT" // Config param, number of threads in thread pool
+
+// Tries to fetch number of threads from
+// command line arguments if specified
+// otherwise returns thread count equals to the number
+// of cores available. 
+long long Config_GetThreadCount (
+    RedisModuleCtx *ctx,
+    RedisModuleString **argv,
+    int argc
+);
+
+#endif
