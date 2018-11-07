480a826d43d9b5263199ba62047597cd0e5d153a
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2018-11-07 07:59:17 -0500

    Don't attempt to look up stores from empty strings

--- src/execution_plan/ops/op_update.c
+++ src/execution_plan/ops/op_update.c
@@ -135,16 +135,18 @@ void _UpdateSchemas(const OpUpdate *op) {
 
         /* Locate node label. */
         AST_GraphEntity* ge = MatchClause_GetEntity(op->ast->matchNode, entityAlias);
-        char *l = ge->label;
+        LabelStoreType t = (ge->t == N_ENTITY) ? STORE_NODE : STORE_EDGE;
+        LabelStore *allStore = GraphContext_AllStore(op->gc, t);
+        LabelStore_UpdateSchema(allStore, 1, &entityProp);
+
         // TODO If the match clause does not provide a label, we must update all the stores
         // affected by the SET clause.
-        LabelStoreType t = (ge->t == N_ENTITY) ? STORE_NODE : STORE_EDGE;
+        char *l = ge->label;
+        if (!l) continue;
         LabelStore *store = GraphContext_GetStore(op->gc, l, t);
         if (!store) continue;
 
-        LabelStore *allStore = GraphContext_AllStore(op->gc, t);
         LabelStore_UpdateSchema(store, 1, &entityProp);
-        LabelStore_UpdateSchema(allStore, 1, &entityProp);
     }
 }
 
