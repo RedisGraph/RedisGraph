455cb7a68811c06e904dd46146e086519ff9d197
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2018-11-10 02:27:20 -0800

    Edge serialization properly accounts for deleted nodes (#226)

--- tests/flow/test_persistency.py
+++ tests/flow/test_persistency.py
@@ -10,7 +10,9 @@ from base import FlowTestsBase
 
 redis_con = None
 GRAPH_NAME = "G"
+DENSE_GRAPH_NAME = "H"
 redis_graph = None
+dense_graph = None
 people = ["Roi", "Alon", "Ailon", "Boaz", "Tal", "Omri", "Ori"]
 countries = ["Israel", "USA", "Japan", "United Kingdom"]
 visits = [("Roi", "USA"), ("Alon", "Israel"), ("Ailon", "Japan"), ("Boaz", "United Kingdom")]
@@ -23,16 +25,20 @@ class GraphPersistency(FlowTestsBase):
     def setUpClass(cls):
         print "GraphPersistency"
         global redis_graph
+        global dense_graph
         global redis_con
         cls.r = redis()
         cls.r.start()
         redis_con = cls.r.client()
         redis_graph = Graph(GRAPH_NAME, redis_con)
 
+        dense_graph = Graph(DENSE_GRAPH_NAME, redis_con)
+
         # redis_con = redis.Redis()
         # redis_graph = Graph("G", redis_con)
 
         cls.populate_graph()
+        cls.populate_dense_graph()
 
     @classmethod
     def tearDownClass(cls):
@@ -47,17 +53,17 @@ class GraphPersistency(FlowTestsBase):
             personNodes = {}
             countryNodes = {}
             # Create entities
-            
+
             for p in people:
                 person = Node(label="person", properties={"name": p})
                 redis_graph.add_node(person)
                 personNodes[p] = person
-            
+
             for p in countries:
                 country = Node(label="country", properties={"name": p})
                 redis_graph.add_node(country)
                 countryNodes[p] = country
-            
+
             for v in visits:
                 person = v[0]
                 country = v[1]
@@ -77,9 +83,25 @@ class GraphPersistency(FlowTestsBase):
             actual_result = redis_con.execute_command("GRAPH.QUERY", GRAPH_NAME, "CREATE INDEX ON :person(name)")
             actual_result = redis_con.execute_command("GRAPH.QUERY", GRAPH_NAME, "CREATE INDEX ON :country(name)")
 
-    # Connect a single node to all other nodes.
+    @classmethod
+    def populate_dense_graph(cls):
+        global dense_graph
+
+        if not redis_con.exists(DENSE_GRAPH_NAME):
+            nodes = []
+            for i in range(10):
+                node = Node(label="n", properties={"val":i})
+                dense_graph.add_node(node)
+                nodes.append(node)
+
+            for n_idx, n in enumerate(nodes):
+                for m_idx, m in enumerate(nodes[:n_idx]):
+                    dense_graph.add_edge(Edge(n, "connected", m))
+
+            dense_graph.commit()
+
+    #  Connect a single node to all other nodes.
     def test01_save_load_rdb(self):
-        pass
         for i in range(2):
             if i == 1:
                 # Save RDB & Load from RDB
@@ -110,7 +132,7 @@ class GraphPersistency(FlowTestsBase):
 
             # Expecting 2 visit edges.
             query = """MATCH (n:person)-[e:visit]->(c:country) WHERE e.purpose='pleasure' RETURN COUNT(e)"""
-            actual_result = redis_graph.query(query)        
+            actual_result = redis_graph.query(query)
             edgeCount = int(float(actual_result.result_set[1][0]))
             assert(edgeCount == 2)
 
@@ -120,6 +142,20 @@ class GraphPersistency(FlowTestsBase):
 
             actual_result = redis_con.execute_command("GRAPH.EXPLAIN", "G", "match (n:country) where n.name = 'Israel' RETURN n")
             assert("Index Scan" in actual_result)
+
+    # Verify that edges are not modified after entity deletion
+    def test02_deleted_entity_migration(self):
+        query = """MATCH (p) WHERE ID(p) = 0 OR ID(p) = 3 OR ID(p) = 7 OR ID(p) = 9 DELETE p"""
+        actual_result = dense_graph.query(query)
+        #  assert(actual_result.nodes_deleted == 3)
+        query = """MATCH (p)-[]->(q) RETURN p, q ORDER BY p.val, q.val"""
+        first_result = dense_graph.query(query)
+
+        # Save RDB & Load from RDB
+        redis_con.execute_command("DEBUG", "RELOAD")
+
+        second_result = dense_graph.query(query)
+        assert(first_result.result_set == second_result.result_set)
 
 if __name__ == '__main__':
     unittest.main()
