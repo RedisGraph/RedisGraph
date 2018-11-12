75ab893861929a7e548310d67c5deb53b3d5802e
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2018-11-12 06:56:34 -0800

    Don't execute queries from Lua or MULTI/EXEC blocks (#225)

--- src/commands/cmd_delete.c
+++ src/commands/cmd_delete.c
@@ -81,6 +81,13 @@ cleanup:
 int MGraph_Delete(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
     if (argc != 2) return RedisModule_WrongArity(ctx);
 
+    // Return immediately if invocation context is Lua or a MULTI/EXEC block
+    int flags = RedisModule_GetContextFlags(ctx);
+    if (flags & (REDISMODULE_CTX_FLAGS_MULTI | REDISMODULE_CTX_FLAGS_LUA)) {
+        RedisModule_ReplyWithError(ctx, "RedisGraph commands may not be called from non-blocking contexts.");
+        return REDISMODULE_OK;
+    }
+
     // Construct delete operation context.
     RedisModuleString *graph_name = argv[1];
     RedisModuleBlockedClient *bc = RedisModule_BlockClient(ctx, NULL, NULL, NULL, 0);
