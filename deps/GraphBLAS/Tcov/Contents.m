c8a7c84b44e02670c28063ec43c1ad4623859774
swilly22 <roi@redislabs.com>
2018-11-29 09:51:11 +0200

    removed previous GraphBLAS version

--- deps/GraphBLAS/Tcov/Contents.m
+++
@@ -1,7 +0,0 @@
-% GraphBLAS/Tcov/Contents.m:
-%
-%   gbcover      - compile GraphBLAS for statement coverage testing
-%   gbcover_edit - create a version of GraphBLAS for statement coverage tests
-%   testcov      - run all GraphBLAS tests, with statement coverage
-%   gbshow       - create a test coverage report in cover_gb_report.c
-
