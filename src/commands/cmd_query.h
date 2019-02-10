eb04ee53edda11a4483f755e0646e0a3f0276b51
swilly22 <roi@redislabs.com>
2019-02-10 10:52:11 +0200

    support multi-exec

--- src/commands/cmd_query.h
+++ src/commands/cmd_query.h
@@ -9,18 +9,9 @@
 #define GRAPH_QUERY_H
 
 #include "../redismodule.h"
-#include "../parser/ast.h"
 #include "../util/thpool/thpool.h"
 
 extern threadpool _thpool;
-
-/* Query context, used for concurent query processing. */
-typedef struct {
-    RedisModuleBlockedClient *bc;   // Blocked client.
-    AST *ast;                       // Parsed AST.
-    char *graphName;                // Graph ID.
-    double tic[2];                  // timings.
-} QueryContext;
 
 int MGraph_Query(RedisModuleCtx *ctx, RedisModuleString **argv, int argc);
 
