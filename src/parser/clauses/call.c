74ddb8765b077de43be2ab133687659e7297b935
swilly22 <roi@redislabs.com>
2019-03-24 15:11:09 +0200

    procedure declare output type

--- src/parser/clauses/call.c
+++ src/parser/clauses/call.c
@@ -7,6 +7,8 @@
 #include "./call.h"
 #include "../../util/arr.h"
 #include "../../util/rmalloc.h"
+#include "../ast_common.h"
+#include "../../procedures/procedure.h"
 
 AST_ProcedureCallNode* New_AST_ProcedureCallNode(char *procedure, char **arguments, char **yield) {
     AST_ProcedureCallNode *procCall = rm_malloc(sizeof(AST_ProcedureCallNode));
@@ -23,12 +25,38 @@ void ProcedureCallClause_ReferredEntities(const AST_ProcedureCallNode *node, Tri
 
 void ProcedureCallClause_DefinedEntities(const AST_ProcedureCallNode *node, TrieMap *definedEntities) {
     if(!node) return;
+
+    ProcedureCtx *proc = Proc_Get(node->procedure);
+    if(!proc) return;
+
     for(int i = 0; i < array_len(node->yield); i++) {
-        TrieMap_Add(definedEntities,
+        for(int j = 0; j < array_len(proc->output); j++) {
+            ProcedureOutput *out = proc->output[j];
+            if(strcmp(node->yield[i], out->name) == 0) {
+                // TODO: leak!
+                AST_GraphEntity *entity = rm_calloc(1, sizeof(AST_GraphEntity));
+                switch(out->type) {
+                    case T_NODE:
+                        entity->t = N_ENTITY;
+                        entity->alias = out->name;
+                        break;
+                    case T_EDGE:
+                        entity->t = N_LINK;
+                        entity->alias = out->name;
+                        break;
+                    default:
+                        // Missing entities are interpreted as scalars.
+                        continue;
+                }
+
+                TrieMap_Add(definedEntities,
                     node->yield[i],
                     strlen(node->yield[i]),
-                    NULL,
-                    TrieMap_DONT_CARE_REPLACE);    
+                    entity,
+                    TrieMap_DONT_CARE_REPLACE);
+                break;
+            }            
+        }
     }
 }
 
