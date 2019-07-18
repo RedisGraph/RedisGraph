accc24074b1111c6c5827b34c68923afa4cda976
swilly22 <roi@redislabs.com>
2019-07-18 09:30:34 +0300

    drop full-text index

---
+++ src/procedures/proc_fulltext_drop_index.c
@@ -0,0 +1,55 @@
+/*
+* Copyright 2018-2019 Redis Labs Ltd. and Contributors
+*
+* This file is available under the Redis Labs Source Available License Agreement
+*/
+
+#include "proc_fulltext_drop_index.h"
+#include "../value.h"
+#include "../util/arr.h"
+#include "../util/rmalloc.h"
+#include "../graph/graphcontext.h"
+
+//------------------------------------------------------------------------------
+// fulltext createNodeIndex
+//------------------------------------------------------------------------------
+
+// CALL db.index.fulltext.drop(label)
+// CALL db.index.fulltext.drop('books')
+
+ProcedureResult Proc_FulltextDropIndexInvoke(ProcedureCtx *ctx, char **args) {
+    if(array_len(args) != 1) return PROCEDURE_ERR;
+
+    char *label = args[0];
+    GraphContext *gc = GraphContext_GetFromTLS();
+    Schema *s = GraphContext_GetSchema(gc, label, SCHEMA_NODE);
+    // Schema doesn't exists, TODO: report error.
+    if(!s) return PROCEDURE_ERR;
+
+    if(Schema_RemoveIndex(s, NULL, IDX_FULLTEXT) == INDEX_FAIL) return PROCEDURE_OK;
+
+    return PROCEDURE_ERR;
+}
+
+SIValue* Proc_FulltextDropIndexStep(ProcedureCtx *ctx) {
+    return NULL;
+}
+
+ProcedureResult Proc_FulltextDropIndexFree(ProcedureCtx *ctx) {
+    // Clean up.
+    return PROCEDURE_OK;
+}
+
+ProcedureCtx* Proc_FulltextDropIndexGen() {
+    void *privateData = NULL;
+    ProcedureOutput **output = array_new(ProcedureOutput*, 0);
+    ProcedureCtx *ctx = ProcCtxNew("db.index.fulltext.drop",
+                                    1,
+                                    output,
+                                    Proc_FulltextDropIndexStep,
+                                    Proc_FulltextDropIndexInvoke,
+                                    Proc_FulltextDropIndexFree,
+                                    privateData);
+
+    return ctx;
+}
