15b5ef2a60d4562d57c0d9dd600e584ac85e8d28
swilly22 <roi@redislabs.com>
2019-09-01 15:00:45 +0300

    removed redisearch

--- deps/RediSearch/src/query_parser/parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef __QUERY_PARSER_PARSE_H__
-#define __QUERY_PARSER_PARSE_H__
-
-#include "tokenizer.h"
-#include "../query.h"
-//#include "../rmutil/alloc.h"
-
-#endif  // !__QUERY_PARSER_PARSE_H__
