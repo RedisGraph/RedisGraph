87a7247f7ae8473162e105270c75f0bb6abf1bdf
Gleb Pomykalov <gleb@lancastr.com>
2019-09-08 11:21:42 -0700

    More effective implementation of UUID4 generation

--- src/arithmetic/arithmetic_expression.c
+++ src/arithmetic/arithmetic_expression.c
@@ -1327,28 +1327,28 @@ SIValue AR_TIMESTAMP(SIValue *argv, int argc) {
 SIValue AR_RANDOMUUID(SIValue *argv, int argc) {
 	/* Implementation is based on https://www.cryptosys.net/pki/uuid-rfc4122.html */
 
-	char *buf = rm_malloc(37 * sizeof(char));
-	unsigned char b[16];
+	// Generate 16 random bytes.
+	unsigned char r[16];
 	int i;
 
 	for (i=0; i<16; i++) {
-		b[i] = rand() % 0xff;
+		r[i] = rand() % 0xff;
 	}
 
-	b[6] = (b[6] & 0b00001111) | 0b01000000;
-	b[8] = (b[8] & 0b00111111) | 0b10000000;
+	char *uuid = rm_malloc(37 * sizeof(char));
+	sprintf(uuid, "%08x-%04x-%04x-%04x-%04x%08x",
+		*((uint32_t*)r),
+		*((uint16_t*)(r + 4)),
+		// Set the four most significant bits of the 7th byte to 0100'B, so the high nibble is "4".
+		(*((uint16_t*)(r + 6)) & 0b0000111111111111) | 0b0100000000000000,
+		// Set the two most significant bits of the 9th byte to 10'B, so the high nibble will be one of "8", "9", "A", or "B" (see Note 1).
+		(*((uint16_t*)(r + 8)) & 0b0011111111111111) | 0b1000000000000000,
+		*((uint16_t*)(r + 10)),
+		*((uint32_t*)(r + 12)));
 
-	char *p = buf;
-	for (i=0; i<16; i++) {
-		sprintf(p, "%02x", b[i]);
-		p += 2;
-		if (i == 3 || i == 5 || i == 7 || i == 9) {
-			*p++ = '-';
-		}
-	}
-	*p = '\0';
+	uuid[36] = '\0';
 
-	return SI_TransferStringVal(buf);
+	return SI_TransferStringVal(uuid);
 }
 
 bool AR_FuncExists(const char *func_name) {
