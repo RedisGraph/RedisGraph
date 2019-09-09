f925e01f4c33c668216b34683bfe051bc48beb69
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2019-09-09 12:26:24 -0400

    Re-introduce RandomUUID function

--- src/arithmetic/string_funcs/string_funcs.c
+++ src/arithmetic/string_funcs/string_funcs.c
@@ -234,6 +234,37 @@ SIValue AR_ENDSWITH(SIValue *argv, int argc) {
 	return SI_BoolVal(true);
 }
 
+//==============================================================================
+//=== Scalar functions =========================================================
+//==============================================================================
+
+SIValue AR_RANDOMUUID(SIValue *argv, int argc) {
+	/* Implementation is based on https://www.cryptosys.net/pki/uuid-rfc4122.html */
+
+	// Generate 16 random bytes.
+	unsigned char r[16];
+	int i;
+
+	for(i = 0; i < 16; i++) {
+		r[i] = rand() % 0xff;
+	}
+
+	char *uuid = rm_malloc(37 * sizeof(char));
+	sprintf(uuid, "%08x-%04x-%04x-%04x-%04x%08x",
+			*((uint32_t *)r),
+			*((uint16_t *)(r + 4)),
+			// Set the four most significant bits of the 7th byte to 0100'B, so the high nibble is "4".
+			(*((uint16_t *)(r + 6)) & 0b0000111111111111) | 0b0100000000000000,
+			// Set the two most significant bits of the 9th byte to 10'B, so the high nibble will be one of "8", "9", "A", or "B" (see Note 1).
+			(*((uint16_t *)(r + 8)) & 0b0011111111111111) | 0b1000000000000000,
+			*((uint16_t *)(r + 10)),
+			*((uint32_t *)(r + 12)));
+
+	uuid[36] = '\0';
+
+	return SI_TransferStringVal(uuid);
+}
+
 void Register_StringFuncs() {
 	SIType *types;
 	AR_FuncDesc *func_desc;
@@ -308,6 +339,10 @@ void Register_StringFuncs() {
 	types = array_append(types, (T_STRING | T_NULL));
 	types = array_append(types, (T_STRING | T_NULL));
 	func_desc = AR_FuncDescNew("ends with", AR_ENDSWITH, 2, types);
+	AR_RegFunc(func_desc);
+
+	types = array_new(SIType, 0);
+	func_desc = AR_FuncDescNew("randomuuid", AR_RANDOMUUID, 0, types);
 	AR_RegFunc(func_desc);
 }
 
