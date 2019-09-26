fde7b39345d665f89178db70a9d0b4ce10b67484
swilly22 <roi@redislabs.com>
2019-09-26 09:57:06 +0300

    full ast scan to find anonymous entities

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -7,73 +7,45 @@
 #include "parser.h"
 #include <assert.h>
 
-static void _name_anonymous_entities_in_path(const cypher_astnode_t *path, int *anon_count) {
+static cypher_astnode_t *_create_anon_identifier(const cypher_astnode_t *node, int anon_count) {
 	char *alias;
-	uint nelems = cypher_ast_pattern_path_nelements(path);
-	for(uint k = 0; k < nelems; k++) {
-		const cypher_astnode_t *identifier = NULL;
-		const cypher_astnode_t *element = cypher_ast_pattern_path_get_element(path, k);
-		cypher_astnode_type_t element_type = cypher_astnode_type(element);
+	int alias_len = asprintf(&alias, "anon_%d", anon_count);
+	struct cypher_input_range range = cypher_astnode_range(node);
+	cypher_astnode_t *identifier = cypher_ast_identifier(alias, alias_len, range);
+	free(alias);
+	return identifier;
+}
 
-		assert(element_type == CYPHER_AST_NODE_PATTERN || element_type == CYPHER_AST_REL_PATTERN);
-		if(element_type == CYPHER_AST_NODE_PATTERN) {
-			identifier = cypher_ast_node_pattern_get_identifier(element);
-		} else {
-			identifier = cypher_ast_rel_pattern_get_identifier(element);
-		}
+static void __name_anonymous_entities_in_pattern(const cypher_astnode_t *node, uint *anon_count) {
+	const cypher_astnode_t *identifier = NULL;
+	cypher_astnode_type_t t = cypher_astnode_type(node);
 
+	if(t == CYPHER_AST_NODE_PATTERN) {
+		identifier = cypher_ast_node_pattern_get_identifier(node);
 		if(!identifier) {
 			// Create  and set identifier.
-			struct cypher_input_range range = cypher_astnode_range(element);
-			int alias_len = asprintf(&alias, "anon_%d", (*anon_count)++);
-			identifier = cypher_ast_identifier(alias, alias_len, range);
-			assert(identifier);
-			if(element_type == CYPHER_AST_NODE_PATTERN) {
-				cypher_ast_node_pattern_set_identifier(element, identifier);
-			} else {
-				cypher_ast_rel_pattern_set_identifier(element, identifier);
-			}
-			free(alias);
+			identifier = _create_anon_identifier(node, (*anon_count)++);
+			cypher_ast_node_pattern_set_identifier(node, identifier);
 		}
+	}
+	if(t == CYPHER_AST_REL_PATTERN) {
+		identifier = cypher_ast_rel_pattern_get_identifier(node);
+		if(!identifier) {
+			identifier = _create_anon_identifier(node, (*anon_count)++);
+			cypher_ast_rel_pattern_set_identifier(node, identifier);
+		}
+	}
+
+	uint child_count = cypher_astnode_nchildren(node);
+	for(uint i = 0; i < child_count; i++) {
+		const cypher_astnode_t *child = cypher_astnode_get_child(node, i);
+		__name_anonymous_entities_in_pattern(child, anon_count);
 	}
 }
 
-/* Name each anonymous graph entity */
-static void _name_anonymous_entities_in_pattern(const cypher_astnode_t *root) {
-	/* Graph entities can be found in
-	 * MATCH, MERGE and CREATE clauses */
-
-	int anon_count = 0;
-	uint clause_count = cypher_ast_query_nclauses(root);
-
-	for(uint i = 0; i < clause_count; i ++) {
-		const cypher_astnode_t *clause = cypher_ast_query_get_clause(root, i);
-		cypher_astnode_type_t clause_type = cypher_astnode_type(clause);
-		const cypher_astnode_t *pattern = NULL;
-		if(clause_type == CYPHER_AST_MATCH) {
-			pattern = cypher_ast_match_get_pattern(clause);
-
-			/* Name anonymous entities presented within a filter:
-			 * WHERE (a)-[]->(z) */
-			const cypher_astnode_t *predicate = cypher_ast_match_get_predicate(clause);
-			if(cypher_astnode_type(predicate) == CYPHER_AST_PATTERN_PATH) {
-				_name_anonymous_entities_in_path(predicate, &anon_count);
-			}
-		} else if(clause_type == CYPHER_AST_CREATE) {
-			pattern = cypher_ast_create_get_pattern(clause);
-		} else if(clause_type == CYPHER_AST_MERGE) {
-			const cypher_astnode_t *path = cypher_ast_merge_get_pattern_path(clause);
-			_name_anonymous_entities_in_path(path, &anon_count);
-			continue;
-		}
-		if(!pattern) continue;
-
-		uint npaths = cypher_ast_pattern_npaths(pattern);
-		for(uint j = 0; j < npaths; j ++) {
-			const cypher_astnode_t *path = cypher_ast_pattern_get_path(pattern, j);
-			_name_anonymous_entities_in_path(path, &anon_count);
-		}
-	}
+static void _name_anonymous_entities_in_pattern(const cypher_astnode_t *node) {
+	uint anon_count = 0;
+	__name_anonymous_entities_in_pattern(node, &anon_count);
 }
 
 static void _enrich_ast(const cypher_astnode_t *root) {
