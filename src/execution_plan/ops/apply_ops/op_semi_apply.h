ee6c40f13aed945a0ca5820f7b0f9b402b85b632
DvirDukhan <dvir@redislabs.com>
2019-12-09 22:01:53 +0200

    wip

--- src/execution_plan/ops/apply_ops/op_semi_apply.h
+++ src/execution_plan/ops/apply_ops/op_semi_apply.h
@@ -10,7 +10,6 @@
 #include "../op_argument.h"
 #include "../../execution_plan.h"
 
-<<<<<<< HEAD
 /* SemiApply operation tests for the presence of a pattern
  * Normal Semi Apply: Starts by pulling on the main execution plan branch,
  * for each record received it tries to get a record from the match branch
@@ -20,29 +19,34 @@
  * for each record received it tries to get a record from the match branch
  * if no data is produced the main execution plan branch record is passed onward
  * otherwise it will try to fetch a new data point from the main execution plan branch. */
-=======
->>>>>>> introduced semi-apply
 struct OpSemiApply;
 
 typedef Record(*ApplyLogic)(struct OpSemiApply *);
 
-<<<<<<< HEAD
+<<< <<< < HEAD
+<<< <<< < HEAD
 typedef struct OpSemiApply {
 	OpBase op;
 	Record r;               // Main execution plan branch record..
 	Argument *op_arg;       // Match branch tap.
-=======
-/* SemiApply operation tests for the presence of a pattern
- * It starts by pulling on the left-hand side branch,
- * for each record received it tries to get a record from the right-hand side
- * if no data is produced it will try to fetch a new data point from the left-hand side
- * otherwise the left-hand side record is passed onward. */
-typedef struct OpSemiApply {
-	OpBase op;
-	Record r;               // Lefthand-side record.
-	Argument *op_arg;       // Righthand-side tap.
->>>>>>> introduced semi-apply
-	ApplyLogic apply_func;  // Which apply method to invoke (semi or anti semi apply)
-} OpSemiApply;
+	== == == =
+		/* SemiApply operation tests for the presence of a pattern
+		 * It starts by pulling on the left-hand side branch,
+		 * for each record received it tries to get a record from the right-hand side
+		 * if no data is produced it will try to fetch a new data point from the left-hand side
+		 * otherwise the left-hand side record is passed onward. */
+	typedef struct OpSemiApply {
+		OpBase op;
+		Record r;               // Lefthand-side record.
+		Argument *op_arg;       // Righthand-side tap.
+		>>> >>> > introduced semi - apply
+		== == == =
+		typedef struct OpSemiApply {
+			OpBase op;
+			Record r;               // Main execution plan branch record..
+			Argument *op_arg;       // Match branch tap.
+			>>> >>> > wip
+			ApplyLogic apply_func;  // Which apply method to invoke (semi or anti semi apply)
+		} OpSemiApply;
 
-OpBase *NewSemiApplyOp(ExecutionPlan *plan, bool anti);
+		OpBase *NewSemiApplyOp(ExecutionPlan *plan, bool anti);
