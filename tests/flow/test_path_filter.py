e682f988f923c3e7d2393563a21520f1bd05f6b3
DvirDukhan <dvir@redislabs.com>
2019-12-11 21:59:20 +0200

    added advanced tests

--- tests/flow/test_path_filter.py
+++ tests/flow/test_path_filter.py
@@ -67,7 +67,7 @@ class testPathFilter(FlowTestsBase):
         result_set = redis_graph.query(query)
         expected_results = [[node0],[node1]]
         query_info = QueryInfo(query = query, description="Tests OR condition with simple filter and path filter", expected_result = expected_results)
-        self._assert_resultset_equals_expected(result_set, query_info)
+        self._assert_resultset_and_expected_mutually_included(result_set, query_info)
 
     def test03_test_or_multiplexer_with_path(self):
         node0 = Node(node_id=0, label="L")
@@ -82,7 +82,7 @@ class testPathFilter(FlowTestsBase):
         result_set = redis_graph.query(query)
         expected_results = [[node0],[node1]]
         query_info = QueryInfo(query = query, description="Tests OR condition with path and negated path filters", expected_result = expected_results)
-        self._assert_resultset_equals_expected(result_set, query_info)
+        self._assert_resultset_and_expected_mutually_included(result_set, query_info)
 
     def test04_test_and_multiplexer_with_filter(self):
         node0 = Node(node_id=0, label="L")
@@ -97,8 +97,41 @@ class testPathFilter(FlowTestsBase):
         result_set = redis_graph.query(query)
         expected_results = [[node0],[node1]]
         query_info = QueryInfo(query = query, description="Tests AND condition with simple filter and negated path filter", expected_result = expected_results)
-        self._assert_resultset_equals_expected(result_set, query_info)
+        self._assert_resultset_and_expected_mutually_included(result_set, query_info)
 
+    def test05_test_and_multiplexer_with_nested_or_multiplexer(self):
+        node0 = Node(node_id=0, label="L")
+        node1 = Node(node_id=1, label="L", properties={'x':1})
+        edge01 = Edge(src_node=node0, dest_node=node1, relation="R")
+        redis_graph.add_node(node0)
+        redis_graph.add_node(node1)
+        redis_graph.add_edge(edge01)
+        redis_graph.flush()
+
+        query = "MATCH(n:L) WHERE (n)-[:R]->(:L) OR (n.x=1 AND (n.x = 2 OR NOT (n)-[:R]->(:L))) RETURN n"
+        result_set = redis_graph.query(query)
+        expected_results = [[node0],[node1]]
+        query_info = QueryInfo(query = query, description="Tests AND condition with simple filter and nested OR", expected_result = expected_results)
+        self._assert_resultset_and_expected_mutually_included(result_set, query_info)
+    
+    def test06_test_and_multiplexer_with_nested_or_multiplexer_over_paths(self):
+        node0 = Node(node_id=0, label="L")
+        node1 = Node(node_id=1, label="L", properties={'x':1})
+        node2 = Node(node_id=2, label="L2")
+        edge01 = Edge(src_node=node0, dest_node=node1, relation="R")
+        edge12 = Edge(src_node=node1, dest_node=node2, relation="R2")
+        redis_graph.add_node(node0)
+        redis_graph.add_node(node1)
+        redis_graph.add_node(node2)
+        redis_graph.add_edge(edge01)
+        redis_graph.add_edge(edge12)
+        redis_graph.flush()
+
+        query = "MATCH(n:L) WHERE (n)-[:R]->(:L) OR (n.x=1 AND ((n)-[:R2]->(:L2) OR (n)-[:R]->(:L))) RETURN n"
+        result_set = redis_graph.query(query)
+        expected_results = [[node0],[node1]]
+        query_info = QueryInfo(query = query, description="Tests AND condition with simple filter and nested OR", expected_result = expected_results)
+        self._assert_resultset_and_expected_mutually_included(result_set, query_info)
 
 
 
