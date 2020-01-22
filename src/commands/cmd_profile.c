af6c707285c34bc2568ad2c5bc36f24a8bd798dd
DvirDukhan <dvir@redislabs.com>
2020-01-22 13:34:38 +0200

    switched ast_validation and ast_readonly places

--- src/commands/cmd_profile.c
+++ src/commands/cmd_profile.c
@@ -27,10 +27,10 @@ void Graph_Profile(void *args) {
 	cypher_parse_result_t *parse_result = parse(command_ctx->query);
 	if(parse_result == NULL) goto cleanup;
 
-	bool readonly = AST_ReadOnly(parse_result);
-
 	// Perform query validations
 	if(AST_Validate(ctx, parse_result) != AST_VALID) goto cleanup;
+
+	bool readonly = AST_ReadOnly(parse_result);
 
 	// Prepare the constructed AST for accesses from the module
 	ast = AST_Build(parse_result);
