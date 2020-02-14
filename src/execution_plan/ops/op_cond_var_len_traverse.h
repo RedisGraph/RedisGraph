ec124e94edaf994e8367faa342a855b6725be09a
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2020-02-14 16:39:14 -0500

    Fix variable-length QueryGraph pointer

--- src/execution_plan/ops/op_cond_var_len_traverse.h
+++ src/execution_plan/ops/op_cond_var_len_traverse.h
@@ -17,12 +17,11 @@ typedef struct {
 	OpBase op;
 	Graph *g;
 	Record r;
-    QueryGraph *qg;                 /* Query graph. */
 	AlgebraicExpression *ae;
 	int srcNodeIdx;                 /* Node set by operation. */
 	int edgesIdx;                   /* Edges set by operation. */
 	int destNodeIdx;                /* Node set by operation. */
-	bool expandInto;                /* Both src and dest already resolved. */    
+	bool expandInto;                /* Both src and dest already resolved. */
 	unsigned int minHops;           /* Maximum number of hops to perform. */
 	unsigned int maxHops;           /* Maximum number of hops to perform. */
 	int edgeRelationCount;          /* Length of edgeRelationTypes. */
