76069aa44bd6b607250c72e79f0915846477fd6b
DvirDukhan <dvir@redislabs.com>
2020-02-20 13:26:55 +0200

    fixed PR comments

--- tests/unit/test_arr.cpp
+++ tests/unit/test_arr.cpp
@@ -9,7 +9,7 @@ extern "C" {
 }
 #endif
 
-class UtilsTest: public ::testing::Test {
+class ArrTest: public ::testing::Test {
   protected:
 	static void SetUpTestCase() {// Use the malloc family for allocations
 		Alloc_Reset();
@@ -21,11 +21,13 @@ class UtilsTest: public ::testing::Test {
 
 };
 
-TEST_F(UtilsTest, TestArrCloneWithCB) {
+TEST_F(ArrTest, TestArrCloneWithCB) {
 	int *arr = array_new(int, 10);
 	for(int i = 0; i < 10; i++) arr = array_append(arr, i);
 	int *arr_clone;
 	array_clone_with_cb(arr_clone, arr, int_identity);
 	ASSERT_EQ(array_len(arr), array_len(arr_clone));
 	for(int i = 0; i < 10; i++) ASSERT_EQ(arr[i], arr_clone[i]);
+	array_free(arr);
+	array_free(arr_clone);
 }
