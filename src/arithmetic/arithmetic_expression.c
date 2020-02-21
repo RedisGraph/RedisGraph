2a09c2530411d8e0e17ab5d17d23ae765e6168ad
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2020-02-21 15:27:21 -0500

    Fix leak in children of aggregate function call

--- src/arithmetic/arithmetic_expression.c
+++ src/arithmetic/arithmetic_expression.c
@@ -410,6 +410,7 @@ void AR_EXP_Aggregate(const AR_ExpNode *root, const Record r) {
 			/* Aggregate. */
 			AggCtx *agg = root->op.agg_func;
 			agg->Step(agg, sub_trees, root->op.child_count);
+			_AR_EXP_FreeResultsArray(sub_trees, root->op.child_count);
 		} else {
 			/* Keep searching for aggregation nodes. */
 			for(int i = 0; i < root->op.child_count; i++) {
