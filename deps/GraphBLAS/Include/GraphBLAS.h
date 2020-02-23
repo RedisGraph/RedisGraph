fdc294a9545f2cb3edaa4556fdd026000eabf26e
swilly22 <roi@redislabs.com>
2020-02-23 15:44:36 +0200

    update GraphBLAS header to break circleci cache

--- deps/GraphBLAS/Include/GraphBLAS.h
+++ deps/GraphBLAS/Include/GraphBLAS.h
@@ -107,10 +107,10 @@
 
 // The version of this implementation, and the GraphBLAS API version:
 #define GxB_IMPLEMENTATION_NAME "SuiteSparse:GraphBLAS"
-#define GxB_IMPLEMENTATION_DATE "Feb 20, 2020"
+#define GxB_IMPLEMENTATION_DATE "Feb 23, 2020"
 #define GxB_IMPLEMENTATION_MAJOR 3
 #define GxB_IMPLEMENTATION_MINOR 2
-#define GxB_IMPLEMENTATION_SUB   0
+#define GxB_IMPLEMENTATION_SUB   1
 #define GxB_SPEC_DATE "May 18, 2018"
 #define GxB_SPEC_MAJOR 1
 #define GxB_SPEC_MINOR 2
