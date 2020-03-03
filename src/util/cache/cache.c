edfdd988fbfe10cac8d723ffbc262f27ec0c0228
DvirDukhan <dvir@redislabs.com>
2020-03-03 11:43:49 +0200

    added single threaded LRU cache

---
+++ src/util/cache/cache.c
@@ -0,0 +1,89 @@
+/*
+ *Copyright 2018 - 2020 Redis Labs Ltd. and Contributors
+ *
+ *This file is available under the Redis Labs Source Available License Agreement
+ */
+
+#include "cache.h"
+#include "xxhash.h"
+
+/**
+ * @brief  Hash a query using XXHASH into a 64 bit.
+ * @param  *key - string to be hashed.
+ * @param  keyLen: key length
+ * @retval hash value
+ */
+static inline hash_key_t _Cache_HashKey(const char *key, size_t keyLen) {
+	return  XXH64(key, keyLen, 0);
+}
+
+static inline void _Cache_DataFree(void *voidPtr) {
+	CacheData *cacheData = voidPtr;
+	if(cacheData->freeFunc) cacheData->freeFunc(cacheData->value);
+}
+
+inline Cache *Cache_New(size_t cacheSize, cacheValueFreeFunc freeCB) {
+	// Memory allocations.
+	Cache *cache = rm_malloc(sizeof(Cache));
+	// Members initialization.
+	cache->priorityQueue = PriorityQueue_New(cacheSize, CacheData, (QueueDataFreeFunc)_Cache_DataFree);
+	cache->rt = raxNew();
+	cache->cacheValueFree = freeCB;
+	return cache;
+}
+
+inline void *Cache_GetValue(Cache *cache, const char *key, size_t keyLen) {
+	unsigned long long const hashKey = _Cache_HashKey(key, keyLen);
+	CacheData *cacheData = raxFind(cache->rt, (unsigned char *)&hashKey, HASH_KEY_LENGTH);
+	if(cacheData != raxNotFound) PriorityQueue_MoveToHead(cache->priorityQueue, cacheData);
+	return (cacheData && cacheData != raxNotFound) ? cacheData->value : NULL;
+}
+
+void Cache_SetValue(Cache *cache, const char *key, size_t keyLen, void *value) {
+	CacheData cacheData;
+	cacheData.hashKey = _Cache_HashKey(key, keyLen);
+	cacheData.value = value;
+	cacheData.freeFunc = cache->cacheValueFree;
+	// See if no one managed to cache it before.
+	if(raxFind(cache->rt, (unsigned char *)&cacheData.hashKey, HASH_KEY_LENGTH) == raxNotFound) {
+		// Remove less recently used entry.
+		if(PriorityQueue_IsFull(cache->priorityQueue)) {
+			// Get cache data from queue.
+			CacheData *evictedCacheData = (CacheData *)PriorityQueue_Dequeue(cache->priorityQueue);
+			// Remove from storage.
+			raxRemove(cache->rt, (unsigned char *)&evictedCacheData->hashKey, HASH_KEY_LENGTH, NULL);
+		}
+		// Add to PriorityQueue.
+		CacheData *insertedCacheData = (CacheData *)PriorityQueue_Enqueue(cache->priorityQueue, &cacheData);
+		// Store in storage.
+		raxInsert(cache->rt, (unsigned char *)&insertedCacheData->hashKey, HASH_KEY_LENGTH,
+				  insertedCacheData, NULL);
+	}
+}
+
+void Cache_RemoveValue(Cache *cache, const char *key, size_t keyLen) {
+	unsigned long long const hashKey = _Cache_HashKey(key, keyLen);
+	// Get data.
+	CacheData *cacheData = raxFind(cache->rt, (unsigned char *)&hashKey, HASH_KEY_LENGTH);
+	if(cacheData != raxNotFound) {
+		PriorityQueue_RemoveFromQueue(cache->priorityQueue, cacheData);
+		raxRemove(cache->rt, (unsigned char *)&cacheData->hashKey, HASH_KEY_LENGTH, NULL);
+	}
+}
+
+inline void Cache_Clear(Cache *cache) {
+	// Empty the priority queue.
+	PriorityQueue_EmptyQueue(cache->priorityQueue);
+	// Free rax.
+	raxFree(cache->rt);
+	// Re alloc rax.
+	cache->rt = raxNew();
+}
+
+inline void Cache_Free(Cache *cache) {
+	// Members destructors.
+	PriorityQueue_Free(cache->priorityQueue);
+	raxFree(cache->rt);
+	// Memory release.
+	rm_free(cache);
+}
---
+++ tests/unit/test_priority_queue.cpp
@@ -0,0 +1,210 @@
+#include "gtest.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "../../src/util/rmalloc.h"
+#include "../../src/util/priority_queue.h"
+#ifdef __cplusplus
+}
+#endif
+
+#define QUEUE_SIZE 10
+
+class PriorityQueueTest:
+	public ::testing::Test {
+  protected:
+	static void SetUpTestCase() {// Use the malloc family for allocations
+		Alloc_Reset();
+	}
+};
+
+TEST_F(PriorityQueueTest, NewQueueTest) {
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	ASSERT_TRUE(priority_queue != NULL);
+	ASSERT_TRUE(PriorityQueue_IsEmpty(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, SimpleEnqueueDequeue) {
+	int i;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	// Enqueue.
+	for(i = 0; i < QUEUE_SIZE; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Enqueue(priority_queue, &i));
+	ASSERT_TRUE(PriorityQueue_IsFull(priority_queue));
+	ASSERT_TRUE(PriorityQueue_Enqueue(priority_queue, &i) == NULL);
+	// Dequeue.
+	for(i = 0; i < QUEUE_SIZE; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, RemoveFromQueue) {
+	int i;
+	int *data_ptr;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	// Enqueue.
+	for(i = 0; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	ASSERT_TRUE(PriorityQueue_IsFull(priority_queue));
+	PriorityQueue_RemoveFromQueue(priority_queue, (void *)data_ptr);
+	ASSERT_FALSE(PriorityQueue_IsFull(priority_queue));
+	ASSERT_FALSE(PriorityQueue_IsEmpty(priority_queue));
+	// Dequeue.
+	for(i = 0; i < QUEUE_SIZE - 1; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, DecreasePriorityFullQueue) {
+	int i;
+	int *data_ptr;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	// Enqueue.
+	for(i = 0; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	for(i = 0; i < QUEUE_SIZE - 1; i++)
+		PriorityQueue_DecreasePriority(priority_queue, (void *)data_ptr);
+	// Queue is [9,0,1,2,3,4,5,6,7,8]
+	ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+	// Dequeue.
+	for(i = 0; i < QUEUE_SIZE - 1; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, MoveToTailFullQueue) {
+	int i;
+	int *data_ptr;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	// Enqueue.
+	for(i = 0; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	PriorityQueue_MoveToTail(priority_queue, data_ptr);
+	// Queue is [9,0,1,2,3,4,5,6,7,8]
+	ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+	// Dequeue.
+	for(i = 0; i < QUEUE_SIZE - 1; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, IncreasePriorityFullQueue) {
+	int i;
+	int *data_ptr;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	// Enqueue.
+	i = 0;
+	data_ptr = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	for(i = 1; i < QUEUE_SIZE; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	for(i = 0; i < QUEUE_SIZE - 1; i++)
+		PriorityQueue_IncreasePriority(priority_queue, (void *)data_ptr);
+	// Queue is [1,2,3,4,5,6,7,8,9,0]
+	// Dequeue.
+	for(i = 1; i < QUEUE_SIZE; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+	ASSERT_EQ(0, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, MoveToHeadFullQueue) {
+	int i;
+	int *data_ptr;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	// Enqueue.
+	i = 0;
+	data_ptr = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	for(i = 1; i < QUEUE_SIZE; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	PriorityQueue_MoveToHead(priority_queue, data_ptr);
+	// Queue is [1,2,3,4,5,6,7,8,9,0]
+	// Dequeue.
+	for(i = 1; i < QUEUE_SIZE; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+	ASSERT_EQ(0, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, DecreasePriorityAfterRemoval) {
+	int i;
+	int *data_ptr_removed;
+	int *data_ptr_modified;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	i = 0;
+	data_ptr_removed = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Enqueue.
+	for(i = 1; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr_modified = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	PriorityQueue_RemoveFromQueue(priority_queue, (void *)data_ptr_removed);
+	// Queue is [1,2,3,4,5,6,7,8,9]
+	for(i = 0; i < QUEUE_SIZE - 2; i++)
+		PriorityQueue_DecreasePriority(priority_queue, (void *)data_ptr_modified);
+	// Queue is [9,1,2,3,4,5,6,7,8]
+	ASSERT_EQ(QUEUE_SIZE - 1, *(int *)PriorityQueue_Dequeue(priority_queue));
+	// Dequeue.
+	for(i = 1; i < QUEUE_SIZE - 2; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, MoveToTailAfterRemoval) {
+	int i;
+	int *data_ptr_removed;
+	int *data_ptr_modified;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	i = 0;
+	data_ptr_removed = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Enqueue.
+	for(i = 1; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr_modified = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	PriorityQueue_RemoveFromQueue(priority_queue, (void *)data_ptr_removed);
+	// Queue is [1,2,3,4,5,6,7,8,9]
+	PriorityQueue_MoveToTail(priority_queue, (void *)data_ptr_modified);
+	// Queue is [9,1,2,3,4,5,6,7,8]
+	ASSERT_EQ(*data_ptr_modified, *(int *)PriorityQueue_Dequeue(priority_queue));
+	// Dequeue.
+	for(i = 1; i < QUEUE_SIZE - 2; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, IncreasePriorityAfterRemoval) {
+	int i;
+	int *data_ptr_removed;
+	int *data_ptr_modified;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	i = 0;
+	data_ptr_modified = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Enqueue.
+	for(i = 1; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr_removed = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	PriorityQueue_RemoveFromQueue(priority_queue, (void *)data_ptr_removed);
+	// Queue is [0,1,2,3,4,5,6,7,8]
+	for(i = 0; i < QUEUE_SIZE - 2; i++)
+		PriorityQueue_IncreasePriority(priority_queue, (void *)data_ptr_modified);
+	// Queue is [1,2,3,4,5,6,7,8,0]
+	// Dequeue.
+	for(i = 1; i < QUEUE_SIZE - 1; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+	ASSERT_EQ(*data_ptr_modified, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
+TEST_F(PriorityQueueTest, MoveToHeadAfterRemoval) {
+	int i;
+	int *data_ptr_removed;
+	int *data_ptr_modified;
+	PriorityQueue *priority_queue = PriorityQueue_New(QUEUE_SIZE, int, NULL);
+	i = 0;
+	data_ptr_modified = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Enqueue.
+	for(i = 1; i < QUEUE_SIZE - 1; i++) PriorityQueue_Enqueue(priority_queue, &i);
+	data_ptr_removed = (int *)PriorityQueue_Enqueue(priority_queue, &i);
+	// Queue is [0,1,2,3,4,5,6,7,8,9]
+	PriorityQueue_RemoveFromQueue(priority_queue, (void *)data_ptr_removed);
+	// Queue is [0,1,2,3,4,5,6,7,8]
+	PriorityQueue_MoveToHead(priority_queue, (void *)data_ptr_modified);
+	// Queue is [1,2,3,4,5,6,7,8,0]
+	// Dequeue.
+	for(i = 1; i < QUEUE_SIZE - 1; i++)
+		ASSERT_EQ(i, *(int *)PriorityQueue_Dequeue(priority_queue));
+	ASSERT_EQ(*data_ptr_modified, *(int *)PriorityQueue_Dequeue(priority_queue));
+}
+
