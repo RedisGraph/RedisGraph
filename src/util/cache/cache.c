3c7937a030bbd974c30ed6871f7f86f108527563
DvirDukhan <dvir@redislabs.com>
2020-05-03 14:58:41 +0300

    wip

--- src/util/cache/cache.c
+++ src/util/cache/cache.c
@@ -27,44 +27,43 @@ inline Cache *Cache_New(size_t cacheSize, cacheValueFreeFunc freeCB) {
 	Cache *cache = rm_malloc(sizeof(Cache));
 	// Members initialization.
 	cache->priorityQueue = PriorityQueue_New(cacheSize, CacheData, (QueueDataFreeFunc)_Cache_DataFree);
-	cache->rt = raxNew();
+	cache->lookup = raxNew();
 	cache->cacheValueFree = freeCB;
 	return cache;
 }
 
-inline void *Cache_GetValue(Cache *cache, const char *key, size_t keyLen) {
-	unsigned long long const hashKey = _Cache_HashKey(key, keyLen);
-	CacheData *cacheData = raxFind(cache->rt, (unsigned char *)&hashKey, HASH_KEY_LENGTH);
+inline void *Cache_GetValue(Cache *cache, const char *key) {
+	unsigned long long const hashKey = _Cache_HashKey(key, strlen(key));
+	CacheData *cacheData = raxFind(cache->lookup, (unsigned char *)&hashKey, HASH_KEY_LENGTH);
 	if(cacheData != raxNotFound) PriorityQueue_AggressivePromotion(cache->priorityQueue, cacheData);
 	return (cacheData && cacheData != raxNotFound) ? cacheData->value : NULL;
 }
 
-void Cache_SetValue(Cache *cache, const char *key, size_t keyLen, void *value) {
+void Cache_SetValue(Cache *cache, const char *key, void *value) {
 	CacheData cacheData;
-	cacheData.hashKey = _Cache_HashKey(key, keyLen);
+	cacheData.hashKey = _Cache_HashKey(key, strlen(key));
 	cacheData.value = value;
 	cacheData.freeFunc = cache->cacheValueFree;
 	// See if no one managed to cache it before.
-	assert(raxFind(cache->rt, (unsigned char *)&cacheData.hashKey, HASH_KEY_LENGTH) == raxNotFound);
+	assert(raxFind(cache->lookup, (unsigned char *)&cacheData.hashKey, HASH_KEY_LENGTH) == raxNotFound);
 	// Remove less recently used entry.
 	if(PriorityQueue_IsFull(cache->priorityQueue)) {
 		// Get cache data from queue.
 		CacheData *evictedCacheData = (CacheData *)PriorityQueue_Dequeue(cache->priorityQueue);
 		// Remove from storage.
-		raxRemove(cache->rt, (unsigned char *)&evictedCacheData->hashKey, HASH_KEY_LENGTH, NULL);
+		raxRemove(cache->lookup, (unsigned char *)&evictedCacheData->hashKey, HASH_KEY_LENGTH, NULL);
 	}
 	// Add to PriorityQueue.
 	CacheData *insertedCacheData = (CacheData *)PriorityQueue_Enqueue(cache->priorityQueue, &cacheData);
 	// Store in storage.
-	raxInsert(cache->rt, (unsigned char *)&insertedCacheData->hashKey, HASH_KEY_LENGTH,
+	raxInsert(cache->lookup, (unsigned char *)&insertedCacheData->hashKey, HASH_KEY_LENGTH,
 			  insertedCacheData, NULL);
-
 }
 
 inline void Cache_Free(Cache *cache) {
 	// Members destructors.
 	PriorityQueue_Free(cache->priorityQueue);
-	raxFree(cache->rt);
+	raxFree(cache->lookup);
 	// Memory release.
 	rm_free(cache);
 }
