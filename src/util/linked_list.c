aa98e04660a8d04af651b19c2679db207e7dda0d
DvirDukhan <dvir@redislabs.com>
2020-05-03 10:44:30 +0300

    fixed linked_list.c comments. wip

--- src/util/linked_list.c
+++ src/util/linked_list.c
@@ -8,26 +8,26 @@
 #include "rmalloc.h"
 
 inline void LinkedList_Init(LinkedList *list) {
-	list->front = NULL;
-	list->end = NULL;
+	list->head = NULL;
+	list->tail = NULL;
 }
 
 inline void LinkedList_AddNode(LinkedList *list, LinkedListNode *node) {
-	node->prev = list->end;
-	// If list is empty, both front and end are NULL. Set them to the new node.
-	if(list->front == NULL && list->end == NULL) list->front = list->end = node;
-	else list->end->next = node; 	// There is a end
-	// In any case, the new node is now the end.
-	list->end = node;
+	node->prev = list->tail;
+	// If list is empty, both head and tail are NULL. Set them to the new node.
+	if(list->head == NULL && list->tail == NULL) list->head = list->tail = node;
+	else list->tail->next = node; 	// There is a tail
+	// In any case, the new node is now the tail.
+	list->tail = node;
 	node->list = list;
 }
 
 inline void LinkedList_RemoveNode(LinkedList *list, LinkedListNode *node) {
 	if(node->list != list) return;
 	if(node->prev) node->prev->next = node->next;
-	if(node->next)node->next->prev = node->prev;
-	if(node == list->end) list->end = node->prev;
-	if(node == list->front) list->front = node->next;
+	if(node->next) node->next->prev = node->prev;
+	if(node == list->tail) list->tail = node->prev;
+	if(node == list->head) list->head = node->next;
 	// This node is disconnected.
 	node->next = NULL;
 	node->prev = NULL;
@@ -37,75 +37,65 @@ inline void LinkedList_RemoveNode(LinkedList *list, LinkedListNode *node) {
 void LinkedList_MoveForward(LinkedList *list, LinkedListNode *node) {
 	// The node is not belonged to the list.
 	if(node->list != list) return;
-	if(node != list->front) {
+	if(node != list->head) {
 		// Pull node out from its place (link its prev and next).
 		node->prev->next = node->next;
-		// Check if node is end (next == NULL)
+		// Check if node is tail (next == NULL)
 		if(node->next) node->next->prev = node->prev;
-		else list->end = node->prev; // In case node is end, move end to point at its prev.
+		else list->tail = node->prev; // In case node is tail, move tail to point at its prev.
 
 		node->next = node->prev;
 		// Originally node->prev->prev
 		node->prev = node->next->prev;
 		node->next->prev = node;
 		if(node->prev) node->prev->next = node;
-		else list->front = node;
+		else list->head = node;
 	}
 }
 
-void LinkedList_MoveToFront(LinkedList *list, LinkedListNode *node) {
+void LinkedList_MoveToHead(LinkedList *list, LinkedListNode *node) {
 	// The node is not belonged to the list.
 	if(node->list != list) return;
-	if(node != list->front) {
-		// Pull node out from its place (link its prev and next).
-		node->prev->next = node->next;
-		// Check if node is end (next == NULL)
-		if(node->next) node->next->prev = node->prev;
-		// In case node is end,  move end to point at its prev and nullify end's next.
-		if(node == list->end) {
-			list->end = node->prev;
-			list->end->next = NULL;
-		}
-		// Put node in the front and link with previous front.
-		node->next = list->front;
+	if(node != list->head) {
+		LinkedList_RemoveNode(list, node);
+		// Put node in the head and link with previous head.
+		node->next = list->head;
 		node->prev = NULL;
 		node->next->prev = node;
-		list->front = node;
+		list->head = node;
+		node->list = list;
 	}
 }
 
 void LinkedList_MoveBack(LinkedList *list, LinkedListNode *node) {
 	// The node is not belonged to the list.
 	if(node->list != list) return;
-	if(node != list->end) {
+	if(node != list->tail) {
 		// Pull node out from its place (link its prev and next).
 		node->next->prev = node->prev;
-		// Check if node is front (prev == NULL)
+		// Check if node is head (prev == NULL)
 		if(node->prev) node->prev->next = node->next;
-		else list->front = node->next; // In case node is front, move front to point at its next.
+		else list->head = node->next; // In case node is head, move head to point at its next.
 
 		node->prev = node->next;
 		// Originally node->next->next
 		node->next = node->prev->next;
 		node->prev->next = node;
 		if(node->next) node->next->prev = node;
-		else list->end = node;
+		else list->tail = node;
 	}
 }
 
-void LinkedList_MoveToEnd(LinkedList *list, LinkedListNode *node) {
+void LinkedList_MoveToTail(LinkedList *list, LinkedListNode *node) {
 	// The node is not belonged to the list.
 	if(node->list != list) return;
-	if(node != list->end) {
-		// Pull node out from its place (link its prev and next).
-		node->next->prev = node->prev;
-		// Check if node is front (prev == NULL)
-		if(node->prev) node->prev->next = node->next;
-		else list->front = node->next; // In case node is front, move front to point at its next.
-		// Put node in the end and link with previous end.
-		node->prev = list->end;
+	if(node != list->tail) {
+		LinkedList_RemoveNode(list, node);
+		// Put node in the tail and link with previous tail.
+		node->prev = list->tail;
 		node->next = NULL;
 		node->prev->next = node;
-		list->end = node;
+		list->tail = node;
+		node->list = list;
 	}
 }
