7ff4576ebc971b472573740c447f0a0978b50541
DvirDukhan <dvir@redislabs.com>
2020-06-25 10:14:37 +0300

    fixed PR comment

--- src/serializers/encoder/v7/encode_graph.c
+++ src/serializers/encoder/v7/encode_graph.c
@@ -95,6 +95,10 @@ static PayloadInfo *_RdbSaveKeySchema(RedisModuleIO *rdb, GraphContext *gc) {
 			payloads = array_append(payloads, _StatePayloadInfo(gc, state, 0, VKEY_ENTITY_COUNT_UNLIMITED));
 		}
 	} else {
+		// Check if this is the last key
+		bool last_key = GraphEncodeContext_GetProcessedKeyCount(gc->encoding_context) ==
+						(GraphEncodeContext_GetKeyCount(gc->encoding_context) - 1);
+		if(last_key) remaining_entities = VKEY_ENTITY_COUNT_UNLIMITED;
 		// Get the current state encoded entities count.
 		uint64_t offset = GraphEncodeContext_GetProcessedEntitiesOffset(gc->encoding_context);
 		// While there are still remaining entities to encode in this key and the state is valid.
@@ -103,20 +107,9 @@ static PayloadInfo *_RdbSaveKeySchema(RedisModuleIO *rdb, GraphContext *gc) {
 			PayloadInfo current_state_payload_info = _StatePayloadInfo(gc, current_state, offset,
 																	   remaining_entities);
 			payloads = array_append(payloads, current_state_payload_info);
-			remaining_entities -= current_state_payload_info.entities_count;
-			if(remaining_entities >= 0) {
+			if(!last_key) remaining_entities -= current_state_payload_info.entities_count;
+			if(remaining_entities > 0) {
 				offset = 0; // New state offset is 0.
-				current_state++; // Advance in the states.
-			}
-		}
-		// Check if this is the last key
-		if(GraphEncodeContext_GetProcessedKeyCount(gc->encoding_context) ==
-		   GraphEncodeContext_GetKeyCount(gc->encoding_context) - 1) {
-			// This is the last key, no more entities to encode. Encode "empty" states + schema state.
-			while(current_state < ENCODE_STATE_FINAL) {
-				PayloadInfo current_state_payload_info = _StatePayloadInfo(gc, current_state, offset,
-																		   remaining_entities);
-				payloads = array_append(payloads, current_state_payload_info);
 				current_state++; // Advance in the states.
 			}
 		}
