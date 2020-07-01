8e4e9b2d50d801ec4934730c186fe8478a59d53c
DvirDukhan <dvir@redislabs.com>
2020-07-01 11:13:23 +0300

    wip

--- tests/flow/test_path.py
+++ tests/flow/test_path.py
@@ -28,6 +28,7 @@ class testPath(FlowTestsBase):
         self.env.flush()
     
     def test_simple_path(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -54,6 +55,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
 
     def test_variable_length_path(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -81,6 +83,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
 
     def test_bi_directional_path(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -117,6 +120,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
 
     def test_bi_directional_path_functions(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -149,6 +153,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
 
     def test_zero_length_path(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1}
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L2")
         edge01 = Edge(node0, "R1", node1, edge_id=0, properties={'value': 1})
@@ -171,6 +176,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
 
     def test_fixed_size_single_hop_traverse(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -202,6 +208,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
     
     def test_fixed_size_multi_hop_traverse(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -232,6 +239,7 @@ class testPath(FlowTestsBase):
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
     
     def test_path_comparison(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
         node0 = Node(node_id=0, label="L1")
         node1 = Node(node_id=1, label="L1")
         node2 = Node(node_id=2, label="L1")
@@ -264,3 +272,61 @@ class testPath(FlowTestsBase):
 
         query_info = QueryInfo(query=query, description="Test path inequality", expected_result=expected_results)
         self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
+
+    def test_var_len_path_comparison(self):
+        # Build graph (:L1 {node_id:0})-[:R1 {value:1}]->(:L1 {node_id:1})-[:R1 {value:2}]->(:L1 {node_id:2})
+        node0 = Node(node_id=0, label="L1")
+        node1 = Node(node_id=1, label="L1")
+        node2 = Node(node_id=2, label="L1")
+        edge01 = Edge(node0, "R1", node1, edge_id=0, properties={'value': 1})
+        edge12 = Edge(node1, "R1", node2, edge_id=1, properties={'value': 2})
+
+        redis_graph.add_node(node0)
+        redis_graph.add_node(node1)
+        redis_graph.add_node(node2)
+        redis_graph.add_edge(edge01)
+        redis_graph.add_edge(edge12)
+
+        redis_graph.flush()
+
+        path01 = Path.new_empty_path().add_node(node0).add_edge(edge01).add_node(node1)
+        path12 = Path.new_empty_path().add_node(node1).add_edge(edge12).add_node(node2)
+        path02 = Path.new_empty_path().add_node(node0).add_edge(edge01).add_node(node1).add_edge(edge12).add_node(node2)
+
+        # Test a path equality filter
+        query = "MATCH p1 = (:L1)-[:R1*]->(:L1) MATCH p2 = (:L1)-[:R1*]->(:L1) WHERE p1 = p2 RETURN p1"
+        expected_results = [[path01],
+                            [path12],
+                            [path02]]
+
+        query_info = QueryInfo(query=query, description="Test var len path equality", expected_result=expected_results)
+        self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
+
+        # Test a path inequality filter
+        query = "MATCH p1 = (:L1)-[:R1*]->(:L1) MATCH p2 = (:L1)-[:R1*]->(:L1) WHERE p1 <> p2 RETURN DISTINCT p1, p2"
+        expected_results = [[path01, path12],
+                            [path12, path01],
+                            [path01, path02],
+                            [path02, path01],
+                            [path12, path02],
+                            [path02, path12]]
+
+        query_info = QueryInfo(query=query, description="Test var len path inequality", expected_result=expected_results)
+        self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
+
+
+    def test_null_path_from_optional_match(self):
+        # Taken from TCK
+        query = "WITH null AS a OPTIONAL MATCH p = (a)-[r]->() RETURN length(nodes(p)), type(r), nodes(p), relationships(p)"
+        expected_results = [[None, None, None, None]]
+        query_info = QueryInfo(query=query, description="Test non existing path in optional match", expected_result=expected_results)
+        self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
+    
+    def test_null_var_len_path_from_optional_match(self):
+        # Taken from TCK
+        query = "WITH null AS a OPTIONAL MATCH p = (a)-[*]->() RETURN length(nodes(p)), nodes(p), relationships(p)"
+        expected_results = [[None, None, None]]
+        query_info = QueryInfo(query=query, description="Test non existing var len path in optional match", expected_result=expected_results)
+        self._assert_resultset_and_expected_mutually_included(redis_graph.query(query), query_info)
+
+        
