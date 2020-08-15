e23b4bd750eb9bfd2f3ce5438bcfe90180cbd385
Roi Lipman <swilly22@users.noreply.github.com>
2020-08-15 10:19:37 +0300

    Update optimizer.c

--- src/execution_plan/optimizations/optimizer.c
+++ src/execution_plan/optimizations/optimizer.c
@@ -17,33 +17,32 @@ void optimizePlan(ExecutionPlan *plan) {
 	 * 2. Try to use the indices. Given a label scan and an indexed property, apply index scan.
 	 * 3. Given a filter which checks id condition, and full or label scan, reduce it to id scan or label with id scan.
 	 *    Note: Due to the scan optimization order, label scan will be replaced with index scan when possible, so the id filter remains. */
-	/* Remove redundant SCAN operations. */
+	
+	// Remove redundant SCAN operations.
 	reduceScans(plan);
 
-	/* When possible, replace label scan and filter ops
-	 * with index scans. */
+	// When possible, replace label scan and filter ops with index scans.
 	utilizeIndices(plan);
 
 	// Try to reduce SCAN + FILTER to a node seek operation.
 	seekByID(plan);
 
-	/* Try to optimize cartesian product */
+	// Try to optimize cartesian product.
 	reduceCartesianProductStreamCount(plan);
 
-	/* Try to match disjoint entities by applying a join */
+	// Try to match disjoint entities by applying a join.
 	applyJoin(plan);
 
-	/* Try to reduce a number of filters into a single filter op. */
+	// Try to reduce a number of filters into a single filter op.
 	reduceFilters(plan);
 
-	/* reduce traversals where both src and dest nodes are already resolved
-	 * into an expand into operation. */
+	// Reduce traversals where both src and dest nodes are already resolved into an expand into operation.
 	reduceTraversal(plan);
 
-	/* Try to reduce distinct if it follows aggregation. */
+	// Try to reduce distinct if it follows aggregation.
 	reduceDistinct(plan);
 
-	/* Try to reduce execution plan incase it perform node or edge counting. */
+	// Try to reduce execution plan incase it perform node or edge counting.
 	reduceCount(plan);
 }
 
