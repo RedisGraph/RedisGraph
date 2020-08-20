4374068193797c4aa02ff04d72c57c8452020870
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2020-08-20 10:45:59 -0400

    Parse full argv array on main thread

--- src/commands/cmd_dispatcher.c
+++ src/commands/cmd_dispatcher.c
@@ -6,11 +6,54 @@
 
 #include "commands.h"
 #include "cmd_context.h"
+#include "../RG.h"
 #include <assert.h>
 #include <strings.h>
 
 // Command handler function pointer.
 typedef void(*Command_Handler)(void *args);
+
+// Read configuration flags, returning REDIS_MODULE_ERR if flag parsing failed.
+static int _read_flags(RedisModuleString **argv, int argc, bool *compact, long long *timeout,
+					   char **errmsg) {
+	ASSERT(compact);
+	ASSERT(timeout);
+
+	// set defaults
+	*timeout = 0;      // no timeout
+	*compact = false;  // verbose
+
+	// GRAPH.QUERY <GRAPH_KEY> <QUERY>
+	// make sure we've got more than 3 arguments
+	if(argc <= 3) return REDISMODULE_OK;
+
+	// scan arguments
+	for(int i = 3; i < argc; i++) {
+		const char *arg = RedisModule_StringPtrLen(argv[i], NULL);
+
+		// compact result-set
+		if(!strcasecmp(arg, "--compact")) {
+			*compact = true;
+			continue;
+		}
+
+		// query timeout
+		if(!strcasecmp(arg, "timeout")) {
+			int err = REDISMODULE_ERR;
+			if(i < argc - 1) {
+				i++; // Set the current argument to the timeout value.
+				err = RedisModule_StringToLongLong(argv[i], timeout);
+			}
+
+			// Emit error on missing, negative, or non-numeric timeout values.
+			if(err != REDISMODULE_OK || *timeout < 0) {
+				asprintf(errmsg, "Failed to parse query timeout value");
+				return REDISMODULE_ERR;
+			}
+		}
+	}
+	return REDISMODULE_OK;
+}
 
 // Return true if the command has a valid number of arguments.
 static inline bool _validate_command_arity(GRAPH_Commands cmd, int arity) {
@@ -18,7 +61,7 @@ static inline bool _validate_command_arity(GRAPH_Commands cmd, int arity) {
 	case CMD_QUERY:
 	case CMD_EXPLAIN:
 	case CMD_PROFILE:
-		// Expect a command, graph name, a query, and optionally a "--compact" flag.
+		// Expect a command, graph name, a query, and optional config flags.
 		return arity >= 3 && arity <= 6;
 	case CMD_SLOWLOG:
 		// Expect just a command and graph name.
@@ -63,6 +106,19 @@ int CommandDispatch(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 	RedisModuleString *query = (argc > 2) ? argv[2] : NULL;
 	const char *command_name = RedisModule_StringPtrLen(argv[0], NULL);
 	GRAPH_Commands cmd = determine_command(command_name);
+	// Parse additional query arguments.
+	char *errmsg;
+	bool compact;
+	long long timeout;
+	int res = _read_flags(argv, argc, &compact, &timeout, &errmsg);
+	if(res == REDISMODULE_ERR) {
+		// Emit error and exit if argument parsing failed.
+		RedisModule_ReplyWithError(ctx, errmsg);
+		free(errmsg);
+		// The API reference dictates that registered functions should always return OK.
+		return REDISMODULE_OK;
+	}
+
 	if(_validate_command_arity(cmd, argc) == false) return RedisModule_WrongArity(ctx);
 	Command_Handler handler = get_command_handler(cmd);
 	GraphContext *gc = GraphContext_Retrieve(ctx, graph_name, true, true);
@@ -79,12 +135,12 @@ int CommandDispatch(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 											REDISMODULE_CTX_FLAGS_LOADING));
 	if(execute_on_main_thread) {
 		// Run query on Redis main thread.
-		context = CommandCtx_New(ctx, NULL, argv[0], query, argc, argv, gc, is_replicated);
+		context = CommandCtx_New(ctx, NULL, argv[0], query, gc, is_replicated, compact, timeout);
 		handler(context);
 	} else {
 		// Run query on a dedicated thread.
 		RedisModuleBlockedClient *bc = RedisModule_BlockClient(ctx, NULL, NULL, NULL, 0);
-		context = CommandCtx_New(NULL, bc, argv[0], query, argc, argv, gc, is_replicated);
+		context = CommandCtx_New(NULL, bc, argv[0], query, gc, is_replicated, compact, timeout);
 		thpool_add_work(_thpool, handler, context);
 	}
 
