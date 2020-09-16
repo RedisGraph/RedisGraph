89eed6c87c8929f6f1203467dd022ae7f79fd52d
swilly22 <roi@redislabs.com>
2020-09-16 16:14:46 +0300

    pagerank, optional arguments

--- src/procedures/proc_pagerank.c
+++ src/procedures/proc_pagerank.c
@@ -13,7 +13,9 @@
 #include "../graph/graphcontext.h"
 #include "../algorithms/pagerank.h"
 
-// CALL algo.pageRank('Page', 'LINKS', {iterations: 20, dampingFactor: 0.85}) YIELD node, score
+// CALL algo.pageRank(NULL, NULL)      YIELD node, score
+// CALL algo.pageRank('Page', NULL)    YIELD node, score
+// CALL algo.pageRank(NULL, 'LINKS')   YIELD node, score
 // CALL algo.pageRank('Page', 'LINKS') YIELD node, score
 
 typedef struct {
@@ -27,21 +29,34 @@ typedef struct {
 } PagerankContext;
 
 ProcedureResult Proc_PagerankInvoke(ProcedureCtx *ctx, const SIValue *args) {
+	// Expecting 2 arguments.
 	if(array_len((SIValue *)args) != 2) return PROCEDURE_ERR;
-	if(!(SI_TYPE(args[0]) & SI_TYPE(args[1]) & T_STRING)) return PROCEDURE_ERR;
+	// arg0 and arg1 can be either String or NULL
+	SIType arg0_t = SI_TYPE(args[0]);
+	SIType arg1_t = SI_TYPE(args[1]);
+	if(!(arg0_t & (T_STRING | T_NULL))) return PROCEDURE_ERR;
+	if(!(arg1_t & (T_STRING | T_NULL))) return PROCEDURE_ERR;
 
-	const char *label = args[0].stringval;
-	const char *relation = args[1].stringval;
+	// Read arguments.
+	const char *label = NULL;    // Node filter.
+	const char *relation = NULL; // Edge filter.
+	if(arg0_t == T_STRING) label = args[0].stringval;
+	if(arg1_t == T_STRING) relation = args[1].stringval;
+
+	// Pagerank config arguments
+	int iters;         // iterations performed
+	const double tol = 1e-4; // tolerance
+	const int itermax = 100; // max iterations
 
 	GrB_Info info;
 	GrB_Index n = 0;               // Node count
-	GrB_Index nvals;               // Number of entries in 'reduced'
+	GrB_Index nvals;               // Number of entries in 'r'
 	GrB_Index nrows;               // Relation matrix row count
 	Schema *s = NULL;
+	bool free_r = false;           // Should 'r' be freed
 	GrB_Matrix l = NULL;           // Label matrix
 	GrB_Matrix r = NULL;           // Relation matrix
 	GrB_Index *mapping = NULL;     // Mapping, array for returning row indices of tuples
-	GrB_Matrix reduced = GrB_NULL; // Relation matrix reduced to only 'l' rows
 	Graph *g = QueryCtx_GetGraph();
 	LAGraph_PageRank *ranking = NULL;
 	GraphContext *gc = QueryCtx_GetGraphCtx();
@@ -62,63 +77,89 @@ ProcedureResult Proc_PagerankInvoke(ProcedureCtx *ctx, const SIValue *args) {
 	ctx->privateData = pdata;
 
 	// Get label matrix.
-	s = GraphContext_GetSchema(gc, label, SCHEMA_NODE);
-	if(!s) return PROCEDURE_OK;
-	l = Graph_GetLabelMatrix(g, s->id);
-
-	// Get relation matrix.
-	s = GraphContext_GetSchema(gc, relation, SCHEMA_EDGE);
-	if(!s) return PROCEDURE_OK;
-	r = Graph_GetRelationMatrix(g, s->id);
-
-	//--------------------------------------------------------------------------
-	// Create a NxN matrix, one row for each labeled entity
-	//--------------------------------------------------------------------------
-	info = GrB_Matrix_nvals(&n, l);
-	ASSERT(info == GrB_SUCCESS);
-
-	info = GrB_Matrix_nrows(&nrows, r);
-	ASSERT(info == GrB_SUCCESS);
-
-	info = GrB_Matrix_new(&reduced, GrB_BOOL, n, n);
-	ASSERT(info == GrB_SUCCESS);
-
-	// Discard rows of 'r' associated with nodes of a different type than 'l'
-	if(n != nrows) {
-		mapping = rm_malloc(sizeof(GrB_Index) * n);
-		// Extract row indecies from 'l', coresponding to node IDs.
-		info = GrB_Matrix_extractTuples_BOOL(mapping, GrB_NULL, GrB_NULL, &n, l);
-		ASSERT(info == GrB_SUCCESS);
-
-		info = GrB_Matrix_extract(reduced, GrB_NULL, GrB_NULL, r, mapping, n,
-			mapping, n, GrB_NULL);
-		ASSERT(info == GrB_SUCCESS);
-	} else {
-		/* There no need to perform extraction as `r` dimension NxN
-		 * is the same as the number of entries in `l` which means
-		 * all connections described in `r` connect nodes of type `l`
-		 * Unfortunately we still need to type cast `r` into a boolean matrix. */
-		GrB_Descriptor desc;
-		GrB_Descriptor_new(&desc);
-		GrB_Descriptor_set(desc, GrB_INP0, GrB_TRAN);
-		info = GrB_transpose(reduced, GrB_NULL, GrB_NULL, r, desc);
-		ASSERT(info	== GrB_SUCCESS);
-		GrB_free(&desc);
+	if(label) {
+		s = GraphContext_GetSchema(gc, label, SCHEMA_NODE);
+		// Unknown label, quickly return.
+		if(!s) return PROCEDURE_OK;
+		l = Graph_GetLabelMatrix(g, s->id);
 	}
 
-	// Invoke Pagerank only if 'reduced' contains entries.
-	info = GrB_Matrix_nvals(&nvals, reduced);
+	// Get relation matrix.
+	if(relation) {
+		s = GraphContext_GetSchema(gc, relation, SCHEMA_EDGE);
+		// Unknown relation, quickly return.
+		if(!s) return PROCEDURE_OK;
+		r = Graph_GetRelationMatrix(g, s->id);
+	} else {
+		// Relation isn't specified, 'r' is the adjacency matrix.
+		r = Graph_GetAdjacencyMatrix(g);
+	}
+
+	info = GrB_Matrix_nrows(&nrows, r);
+	UNUSED(info);
+	ASSERT(info == GrB_SUCCESS);
+	n = nrows;
+
+	/* Incase label or relation is specified
+	 * if label is specified:
+	 * filter 'r' to contain only rows and columns associated with
+	 * nodes of type 'l'
+	 *
+	 * if relation is specified:
+	 * cast 'r' to a boolean matrix */
+	if(label != NULL || relation != NULL) {
+		//----------------------------------------------------------------------
+		// Create a NxN matrix, one row for each labeled entity
+		//----------------------------------------------------------------------
+		if(label) {
+			info = GrB_Matrix_nvals(&n, l);
+			ASSERT(info == GrB_SUCCESS);
+		}
+
+		GrB_Matrix reduced; // Relation matrix reduced to only 'l' rows/cols
+		info = GrB_Matrix_new(&reduced, GrB_BOOL, n, n);
+		ASSERT(info == GrB_SUCCESS);
+
+		// Discard rows of 'r' associated with nodes of a different type than 'l'
+		// this will also perform casting to boolean.
+		if(n != nrows) {
+			mapping = rm_malloc(sizeof(GrB_Index) * n);
+			// Extract row indecies from 'l', coresponding to node IDs.
+			info = GrB_Matrix_extractTuples_BOOL(mapping, GrB_NULL, GrB_NULL, &n, l);
+			ASSERT(info == GrB_SUCCESS);
+
+			info = GrB_Matrix_extract(reduced, GrB_NULL, GrB_NULL, r, mapping, n,
+					mapping, n, GrB_NULL);
+			ASSERT(info == GrB_SUCCESS);
+		} else {
+			/* There no need to perform extraction as either 'l' isn't specified
+			 * if if 'l' is given, 'r' dimension is NxN the same as
+			 * the number of entries in 'l' which means all connections
+			 * described in 'r' connect nodes of type 'l'
+			 * Unfortunately we still need to type cast 'r' to boolean */
+			GrB_Descriptor desc;
+			GrB_Descriptor_new(&desc);
+			GrB_Descriptor_set(desc, GrB_INP0, GrB_TRAN);
+			info = GrB_transpose(reduced, GrB_NULL, GrB_NULL, r, desc);
+			ASSERT(info == GrB_SUCCESS);
+			GrB_free(&desc);
+		}
+
+		r = reduced;
+		free_r = true;
+	}
+
+	// Invoke Pagerank only if 'r' contains entries.
+	info = GrB_Matrix_nvals(&nvals, r);
 	ASSERT(info == GrB_SUCCESS);
 
 	if(nvals > 0) {
-		double tol = 1e-4;
-		int iters, itermax = 100;
-		info = Pagerank(&ranking, reduced, itermax, tol, &iters);
+		info = Pagerank(&ranking, r, itermax, tol, &iters);
 		ASSERT(info == GrB_SUCCESS);
 	}
 
 	// Clean up.
-	GrB_free(&reduced);
+	if(free_r) GrB_free(&r);
 
 	// Update context.
 	pdata->n = n;
