3d022b0b3fd4e5454b991412d15dd1630fb35c83
alonre24 <alonreshef24@gmail.com>
2020-10-07 17:24:58 +0300

    indentations fix

--- src/procedures/procedure.c
+++ src/procedures/procedure.c
@@ -77,19 +77,19 @@ ProcedureCtx *Proc_Get(const char *proc_name) {
 }
 
 ProcedureResult Proc_Invoke(ProcedureCtx *proc, const SIValue *args, const char **yield) {
-    ASSERT(proc != NULL);
+	ASSERT(proc != NULL);
 
-    // Procedure is expected to be in the `PROCEDURE_NOT_INIT` state.
-    if (proc->state != PROCEDURE_NOT_INIT) {
-    	proc->state = PROCEDURE_ERROR;
-    	return PROCEDURE_ERR;
-    }
-    if (proc->argc != PROCEDURE_VARIABLE_ARG_COUNT) assert(proc->argc == array_len((SIValue *) args));
+	// Procedure is expected to be in the `PROCEDURE_NOT_INIT` state.
+	if (proc->state != PROCEDURE_NOT_INIT) {
+		proc->state = PROCEDURE_ERROR;
+		return PROCEDURE_ERR;
+	}
+	if (proc->argc != PROCEDURE_VARIABLE_ARG_COUNT) assert(proc->argc == array_len((SIValue *) args));
 
-    ProcedureResult res = proc->Invoke(proc, args, yield);
-    // Set state to initialized.
-    if (res == PROCEDURE_OK) proc->state = PROCEDURE_INIT;
-    return res;
+	ProcedureResult res = proc->Invoke(proc, args, yield);
+	// Set state to initialized.
+	if (res == PROCEDURE_OK) proc->state = PROCEDURE_INIT;
+	return res;
 }
 
 SIValue *Proc_Step(ProcedureCtx *proc) {
@@ -98,10 +98,10 @@ SIValue *Proc_Step(ProcedureCtx *proc) {
 	if (proc->state != PROCEDURE_INIT) return NULL;
 
 	SIValue *val = proc->Step(proc);
-    /* Set procedure state to depleted if NULL is returned.
-     * NOTE: we might have errored. */
-    if (val == NULL) proc->state = PROCEDURE_DEPLETED;
-    return val;
+	/* Set procedure state to depleted if NULL is returned.
+	* NOTE: we might have errored. */
+	if (val == NULL) proc->state = PROCEDURE_DEPLETED;
+	return val;
 }
 
 uint Procedure_Argc(const ProcedureCtx *proc) {
@@ -115,39 +115,39 @@ uint Procedure_OutputCount(const ProcedureCtx *proc) {
 }
 
 const char *Procedure_GetOutput(const ProcedureCtx *proc, uint output_idx) {
-    ASSERT(proc != NULL);
-    ASSERT(output_idx < Procedure_OutputCount(proc));
-    return proc->output[output_idx].name;
+	ASSERT(proc != NULL);
+	ASSERT(output_idx < Procedure_OutputCount(proc));
+	return proc->output[output_idx].name;
 }
 
 bool Procedure_ContainsOutput(const ProcedureCtx *proc, const char *output) {
-    ASSERT(proc != NULL);
-    ASSERT(output != NULL);
-    uint output_count = array_len(proc->output);
-    for (uint i = 0; i < output_count; i++) {
-    	if (strcmp(proc->output[i].name, output) == 0) return true;
-    }
-    return false;
+	ASSERT(proc != NULL);
+	ASSERT(output != NULL);
+	uint output_count = array_len(proc->output);
+	for (uint i = 0; i < output_count; i++) {
+		if (strcmp(proc->output[i].name, output) == 0) return true;
+	}
+	return false;
 }
 
 bool Proc_ReadOnly(const char *proc_name) {
-    assert(__procedures);
-    ProcGenerator gen = raxFind(__procedures, (unsigned char *) proc_name, strlen(proc_name));
-    if (gen == raxNotFound) return false; // Invalid procedure specified, handled elsewhere.
-    /* TODO It would be preferable to be able to determine whether a procedure is read-only
-     * without creating its entire context; this is wasteful. */
-    ProcedureCtx *ctx = gen(NULL, NULL);
-    bool read_only = ctx->readOnly;
-    Proc_Free(ctx);
-    return read_only;
+	assert(__procedures);
+	ProcGenerator gen = raxFind(__procedures, (unsigned char *) proc_name, strlen(proc_name));
+	if (gen == raxNotFound) return false; // Invalid procedure specified, handled elsewhere.
+	/* TODO It would be preferable to be able to determine whether a procedure is read-only
+	 * without creating its entire context; this is wasteful. */
+	ProcedureCtx *ctx = gen(NULL, NULL);
+	bool read_only = ctx->readOnly;
+	Proc_Free(ctx);
+	return read_only;
 }
 
 void Proc_Free(ProcedureCtx *proc) {
-    if (!proc) return;
-    proc->Free(proc);
+	if (!proc) return;
+	proc->Free(proc);
 
-    if (proc->output) array_free(proc->output);
+	if (proc->output) array_free(proc->output);
 
-    rm_free(proc);
+	rm_free(proc);
 }
 
