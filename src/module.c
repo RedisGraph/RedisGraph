7560330e9758379732d6dbefef31e3773c03e6b6
Guy Korland <gkorland@gmail.com>
2021-01-21 15:13:59 +0200

    Update module.c

--- src/module.c
+++ src/module.c
@@ -156,7 +156,7 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
 		return REDISMODULE_ERR;
 	}
 
-	if(RedisModule_CreateCommand(ctx, "graph.EXPLAIN", CommandDispatch, "readonly", 1, 1,
+	if(RedisModule_CreateCommand(ctx, "graph.EXPLAIN", CommandDispatch, "write deny-oom", 1, 1,
 								 1) == REDISMODULE_ERR) {
 		return REDISMODULE_ERR;
 	}
