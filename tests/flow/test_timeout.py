0ec6806ebe23db45ef5cac86c333e11a13438047
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2021-02-24 04:43:54 -0500

    Always report run-time errors as the sole reply (#1590)
    * Always report run-time errors as the sole reply
    
    * Update test_timeout.py
    
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>
    (cherry picked from commit c9ba776c42662b212069a634613febfc0d83938b)

--- tests/flow/test_timeout.py
+++ tests/flow/test_timeout.py
@@ -17,13 +17,19 @@ class testQueryTimeout(FlowTestsBase):
 
     def test_read_query_timeout(self):
         query = "UNWIND range(0,100000) AS x WITH x AS x WHERE x = 10000 RETURN x"
-        response = redis_con.execute_command("GRAPH.QUERY", "g", query, "timeout", 1)
-        error = response[-1]
-        self.env.assertTrue(isinstance(error, ResponseError))
-        self.env.assertContains("Query timed out", str(error))
+        try:
+            # The query is expected to time out
+            redis_con.execute_command("GRAPH.QUERY", "g", query, "timeout", 1)
+            assert(False)
+        except ResponseError as error:
+            self.env.assertTrue(isinstance(error, ResponseError))
+            self.env.assertContains("Query timed out", str(error))
 
-        response = redis_con.execute_command("GRAPH.QUERY", "g", query, "timeout", 100)
-        self.env.assertFalse(isinstance(response[-1], ResponseError))
+        try:
+            # The query is expected to succeed
+            redis_con.execute_command("GRAPH.QUERY", "g", query, "timeout", 100)
+        except:
+            assert(False)
 
     def test_write_query_timeout(self):
         query = "create ()"
@@ -33,4 +39,3 @@ class testQueryTimeout(FlowTestsBase):
         except:
             # Expecting an error.
             pass
-
