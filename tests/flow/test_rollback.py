c24573d9e842c1f96247905e3e1ccaac14ba300c
Ofir Moskovich <ofir.moskovich@redislabs.com>
2021-05-19 12:02:49 +0300

    Added rollback test, can be squash to prev commit.

---
+++ tests/flow/test_rollback.py
@@ -0,0 +1,45 @@
+import sys
+from RLTest import Env
+from base import FlowTestsBase
+from redis import ResponseError
+from redisgraph import Graph, Node
+
+redis_con = None
+redis_graph = None
+redis_graph = None
+people = ["Roi", "Alon", "Ailon", "Boaz", "Tal", "Omri", "Ori"]
+
+class testRollback(FlowTestsBase):
+    def __init__(self):
+        self.env = Env(decodeResponses=True)
+        global redis_graph
+        redis_con = self.env.getConnection()
+        redis_graph = Graph("G", redis_con)
+        self.populate_graph()
+
+    def populate_graph(self):
+        global redis_graph
+        
+        nodes = {}
+         # Create entities        
+        for p in people:
+            node = Node(label="person", properties={"v": 2})
+            redis_graph.add_node(node)
+            nodes[p] = node
+
+        redis_graph.commit()
+
+    def test01_update_rollback(self):
+        query = "MATCH (n) SET n.v = 1 WITH n AS n SET n.v = 1 * ‘str’"
+        try:
+            # The query is expected to fail
+            redis_graph.query(query)
+            assert(False)
+        except ResponseError as error:
+            self.env.assertContains("Invalid input", str(error))
+
+        # Verify that query was completely rollbacked
+        query2 = "MATCH (n) RETURN n.v"
+        actual_result = redis_graph.query(query2)
+        v = actual_result.result_set[0][0]
+        self.env.assertEquals(v, 2)
