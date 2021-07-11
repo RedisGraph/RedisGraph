fdc0d17f209a59edfd5217cc3b8e6b8971377054
Ofir Moskovich <ofir.moskovich@redislabs.com>
2021-07-11 12:18:20 +0300

    GraphBLAS: added iterator

--- deps/GraphBLAS/Include/GraphBLAS.h
+++ deps/GraphBLAS/Include/GraphBLAS.h
@@ -8040,6 +8040,79 @@ GrB_Info GxB_Vector_resize      // change the size of a vector (historical)
     (arg1, __VA_ARGS__)
 #endif
 
+//------------------------------------------------------------------------------
+// GxB_MatrixTupleIter:  Iterates over all none zero values of a matrix
+//------------------------------------------------------------------------------
+
+// TuplesIter maintains information required
+// to iterate over a matrix
+typedef struct
+{
+        GrB_Matrix A ;          // Matrix being iterated
+        GrB_Index nvals ;       // Number of none zero values in matrix
+        GrB_Index nnz_idx ;     // Index of current none zero value
+        int64_t p ;             // Number of none zero values in current column
+        int64_t row_idx ;       // Index of current row
+        GrB_Index nrows ;       // Total number of rows in matrix
+} GxB_MatrixTupleIter ;
+
+// Create a new matrix iterator
+GrB_Info GxB_MatrixTupleIter_new
+(
+        GxB_MatrixTupleIter **iter, // iterator to create
+        GrB_Matrix A                // matrix being iterated
+) ;
+
+// Iterate over specific row
+GrB_Info GxB_MatrixTupleIter_iterate_row
+(
+        GxB_MatrixTupleIter *iter,  // iterator to use
+        GrB_Index rowIdx            // row index to iterate over
+) ;
+
+// Move iterator to a specific row
+GrB_Info GxB_MatrixTupleIter_jump_to_row
+(
+        GxB_MatrixTupleIter *iter,  // iterator to use
+        GrB_Index rowIdx            // row index to move iterator to
+) ;
+
+// Move iterator over specific rows range
+GrB_Info GxB_MatrixTupleIter_iterate_range
+(
+        GxB_MatrixTupleIter *iter,  // iterator to use
+        GrB_Index startRowIdx,      // row index to start with
+        GrB_Index endRowIdx         // row index to finish with
+) ;
+
+// Advance iterator to the next none zero value
+GrB_Info GxB_MatrixTupleIter_next
+(
+        GxB_MatrixTupleIter *iter,      // iterator to consume
+        GrB_Index *row,                 // optional row index of current NNZ
+        GrB_Index *col,                 // optional column index of current NNZ
+        bool *depleted                  // indicate if iterator depleted
+) ;
+
+// Reset iterator
+GrB_Info GxB_MatrixTupleIter_reset
+(
+        GxB_MatrixTupleIter *iter        // iterator to reset
+) ;
+
+// Reuse iterator to scan given matrix
+GrB_Info GxB_MatrixTupleIter_reuse
+(
+        GxB_MatrixTupleIter *iter,  // iterator to update
+        GrB_Matrix A                            // matrix to scan
+) ;
+
+// Release every resource consumed by iterator
+GrB_Info GxB_MatrixTupleIter_free
+(
+        GxB_MatrixTupleIter *iter   // iterator to free
+) ;
+
 //==============================================================================
 // GxB_fprint and GxB_print: print the contents of a GraphBLAS object
 //==============================================================================
---
+++ deps/GraphBLAS/Source/GxB_Matrix_tuple_iter.c
@@ -0,0 +1,225 @@
+//------------------------------------------------------------------------------
+// GxB_MatrixTupleIter: Iterates over matrix none zero values
+//------------------------------------------------------------------------------
+
+#include "GB.h"
+
+// Sets iterator as depleted.
+static inline void _EmptyIterator
+(
+	GxB_MatrixTupleIter *iter   // Iterator to deplete.
+) {
+	iter->nvals = 0;
+	iter->nnz_idx = 0;
+}
+
+// Create a new iterator
+GrB_Info GxB_MatrixTupleIter_new
+(
+	GxB_MatrixTupleIter **iter,     // iterator to create
+	GrB_Matrix A                    // matrix to iterate over
+) {
+	GB_WHERE(A, "GxB_MatrixTupleIter_new (A)") ;
+	GB_RETURN_IF_NULL_OR_FAULTY(A) ;
+
+	// make sure matrix is not bitmap or full
+	GxB_set(A, GxB_SPARSITY_CONTROL, GxB_SPARSE) ;
+
+	GrB_Index nrows ;
+	GrB_Matrix_nrows(&nrows, A) ;
+
+	*iter = GB_MALLOC(1, GxB_MatrixTupleIter) ;
+	GrB_Matrix_nvals(&((*iter)->nvals), A) ;
+
+	(*iter)->A        =  A        ;
+	(*iter)->nnz_idx  =  0        ;
+	(*iter)->row_idx  =  0        ;
+	(*iter)->nrows    =  nrows    ;
+	(*iter)->p        =  A->p[0]  ;
+
+	return (GrB_SUCCESS) ;
+}
+
+GrB_Info GxB_MatrixTupleIter_iterate_row
+(
+	GxB_MatrixTupleIter *iter,
+	GrB_Index rowIdx
+) {
+	GB_WHERE1("GxB_MatrixTupleIter_iterate_row (iter, rowIdx)") ;
+	GB_RETURN_IF_NULL(iter) ;
+
+	// Deplete iterator, should caller ignore returned error.
+	_EmptyIterator(iter) ;
+
+	if(rowIdx < 0 || rowIdx >= iter->nrows) {
+		GB_ERROR(GrB_INVALID_INDEX,
+				 "Row index " GBu " out of range; must be < " GBu,
+				 rowIdx, iter->nrows) ;
+	}
+
+	iter->p        =  0 ;
+	iter->nvals    =  iter->A->p[rowIdx + 1] ;
+	iter->nnz_idx  =  iter->A->p[rowIdx] ;
+	iter->row_idx  =  rowIdx ;
+	return (GrB_SUCCESS) ;
+}
+
+GrB_Info GxB_MatrixTupleIter_jump_to_row
+(
+	GxB_MatrixTupleIter *iter,
+	GrB_Index rowIdx
+) {
+	GB_WHERE1("GxB_MatrixTupleIter_jump_to_row (iter, rowIdx)") ;
+	GB_RETURN_IF_NULL(iter) ;
+
+	// Deplete iterator, should caller ignore returned error.
+	_EmptyIterator(iter) ;
+
+	if(rowIdx < 0 || rowIdx >= iter->nrows) {
+		GB_ERROR(GrB_INVALID_INDEX,
+				 "Row index " GBu " out of range; must be < " GBu,
+				 rowIdx, iter->nrows) ;
+	}
+
+	GrB_Matrix_nvals(&(iter->nvals), iter->A) ;
+
+	iter->p        =  0                   ;
+	iter->nnz_idx  =  iter->A->p[rowIdx]  ;
+	iter->row_idx  =  rowIdx              ;
+
+	return (GrB_SUCCESS) ;
+}
+
+GrB_Info GxB_MatrixTupleIter_iterate_range
+(
+	GxB_MatrixTupleIter *iter,  // iterator to use
+	GrB_Index startRowIdx,      // row index to start with
+	GrB_Index endRowIdx         // row index to finish with
+) {
+	GB_WHERE1("GxB_MatrixTupleIter_iterate_range (iter, startRowIdx, endRowIdx)") ;
+	GB_RETURN_IF_NULL(iter) ;
+
+	// Deplete iterator, should caller ignore returned error.
+	_EmptyIterator(iter) ;
+
+	if(startRowIdx < 0 || startRowIdx >= iter->nrows) {
+		GB_ERROR(GrB_INVALID_INDEX,
+				 "row index " GBu " out of range; must be < " GBu,
+				 startRowIdx, iter->nrows) ;
+	}
+
+	if(startRowIdx > endRowIdx) {
+		GB_ERROR(GrB_INVALID_INDEX,
+				 "row index " GBu " must be > " GBu,
+				 startRowIdx, endRowIdx) ;
+	}
+
+	iter->p        =  0                        ;
+	iter->nnz_idx  =  iter->A->p[startRowIdx]  ;
+	iter->row_idx  =  startRowIdx              ;
+	if(endRowIdx < iter->nrows) iter->nvals = iter->A->p[endRowIdx + 1] ;
+	else GrB_Matrix_nvals(&(iter->nvals), iter->A) ;
+
+	return (GrB_SUCCESS) ;
+}
+
+// Advance iterator
+GrB_Info GxB_MatrixTupleIter_next
+(
+	GxB_MatrixTupleIter *iter,      // iterator to consume
+	GrB_Index *row,                 // optional output row index
+	GrB_Index *col,                 // optional output column index
+	bool *depleted                  // indicate if iterator depleted
+) {
+	GB_WHERE1("GxB_MatrixTupleIter_next (iter, row, col, depleted)") ;
+	GB_RETURN_IF_NULL(iter) ;
+	GB_RETURN_IF_NULL(depleted) ;
+	GrB_Index nnz_idx = iter->nnz_idx ;
+
+	if(nnz_idx >= iter->nvals) {
+		*depleted = true ;
+		return (GrB_SUCCESS) ;
+	}
+
+	GrB_Matrix A = iter->A ;
+
+	//--------------------------------------------------------------------------
+	// extract the column indices
+	//--------------------------------------------------------------------------
+
+	if(col)
+		*col = A->i[nnz_idx] ;
+
+	//--------------------------------------------------------------------------
+	// extract the row indices
+	//--------------------------------------------------------------------------
+
+	const int64_t *Ap = A->p ;
+	int64_t i = iter->row_idx ;
+
+	for(; i < iter->nrows; i++) {
+		int64_t p = iter->p + Ap[i] ;
+		if(p < Ap[i + 1]) {
+			iter->p++ ;
+			if(row)
+				*row = i ;
+			break ;
+		}
+		iter->p = 0 ;
+	}
+
+	iter->row_idx = i ;
+
+	iter->nnz_idx++ ;
+
+	*depleted = false ;
+	return (GrB_SUCCESS) ;
+}
+
+// Reset iterator
+GrB_Info GxB_MatrixTupleIter_reset
+(
+	GxB_MatrixTupleIter *iter       // iterator to reset
+) {
+	GB_WHERE1("GxB_MatrixTupleIter_reset (iter)") ;
+	GB_RETURN_IF_NULL(iter) ;
+	iter->nnz_idx  =  0              ;
+	iter->row_idx  =  0              ;
+	iter->p        =  iter->A->p[0]  ;
+	return (GrB_SUCCESS) ;
+}
+
+// Update iterator to scan given matrix
+GrB_Info GxB_MatrixTupleIter_reuse
+(
+	GxB_MatrixTupleIter *iter,      // iterator to update
+	GrB_Matrix A                    // matrix to scan
+) {
+	GB_WHERE(A, "GxB_MatrixTupleIter_reuse (iter, A)") ;
+	GB_RETURN_IF_NULL(iter) ;
+	GB_RETURN_IF_NULL_OR_FAULTY(A) ;
+
+	// make sure matrix is not bitmap or full
+	GxB_set(A, GxB_SPARSITY_CONTROL, GxB_SPARSE) ;
+
+	GrB_Index nrows ;
+	GrB_Matrix_nrows(&nrows, A) ;
+
+	iter->A = A ;
+	iter->nrows = nrows ;
+	GrB_Matrix_nvals(&iter->nvals, A) ;
+	GxB_MatrixTupleIter_reset(iter) ;
+	return (GrB_SUCCESS) ;
+}
+
+// Release iterator
+GrB_Info GxB_MatrixTupleIter_free
+(
+	GxB_MatrixTupleIter *iter       // iterator to free
+) {
+	GB_WHERE1("GxB_MatrixTupleIter_free (iter)") ;
+	GB_RETURN_IF_NULL(iter) ;
+	GB_FREE(iter) ;
+	return (GrB_SUCCESS) ;
+}
+
