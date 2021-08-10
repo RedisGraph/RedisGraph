58c6c336c0756fd114851ca1d930a2886f5b2fb8
Avi Avni <avi.avni@gmail.com>
2021-08-10 09:58:19 +0300

    make test readable

--- tests/flow/test_multi_label.py
+++ tests/flow/test_multi_label.py
@@ -17,7 +17,7 @@ class testMultiLabel(FlowTestsBase):
     def populate_graph(self):
         # Construct a graph with the form:
         # (v1:L0:L1)-[:E]->(v2:L1)-[:E]->(v3:L1:L2)
-        q = """CREATE (:L0:L1 {val:1})-[:E]->(:L1 {val:2})-[:E]->(:L1:L2 {val:3})"""
+        q = """CREATE (:L0:L1 {val:'L0L1'})-[:E]->(:L1 {val:'L1'})-[:E]->(:L1:L2 {val:'L1L2'})"""
         redis_graph.query(q)
 
     # Validate basic multi-label scans.
@@ -25,15 +25,15 @@ class testMultiLabel(FlowTestsBase):
         # Issue a query that matches the single (:L0:L1) node.
         query = """MATCH (a:L0:L1) RETURN a.val"""
         query_result = redis_graph.query(query)
-        expected_result = [[1]]
+        expected_result = [['L0L1']]
         self.env.assertEquals(query_result.result_set, expected_result)
 
         # Issue a query that matches all 3 nodes with the label :L1.
         query = """MATCH (a:L1) RETURN a.val ORDER BY a.val"""
         query_result = redis_graph.query(query)
-        expected_result = [[1],
-                           [2],
-                           [3]]
+        expected_result = [['L0L1'],
+                           ['L1'],
+                           ['L1L2']]
         self.env.assertEquals(query_result.result_set, expected_result)
 
         # Issue a query that matches no nodes, as the (:L0:L2) label disjunction is not present.
@@ -44,7 +44,7 @@ class testMultiLabel(FlowTestsBase):
 
     # Validate basic multi-label scans.
     def test02_multilabel_traversal(self):
-        expected_result = [[1, 2]]
+        expected_result = [['L0L1', 'L1']]
         query = """MATCH (a:L0:L1)-[]->(b) RETURN a.val, b.val"""
         query_result = redis_graph.query(query)
         self.env.assertEquals(query_result.result_set, expected_result)
