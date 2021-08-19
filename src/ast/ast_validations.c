1976ed1d1ffb447e2836ff78eca53b802214bd8d
Avi Avni <avi.avni@gmail.com>
2021-08-19 22:15:32 +0300

    implement quantifier (#1880)
    * implement quantifier
    
    * update doc
    
    * address review
    
    * allow NULL value in compact
    
    * fix comment
    
    * fix comment
    
    * address review
    
    * Update commands.md
    
    * fix disable test for coverage
    
    * exlude only test02_clean_shutdown from coverage
    
    * Update test_concurrent_query.py
    
    * Update test_pending_queries_limit.py
    
    * Update test_query_mem_limit.py
    
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>

--- src/ast/ast_validations.c
+++ src/ast/ast_validations.c
@@ -75,7 +75,11 @@ static void _AST_GetIdentifiers(const cypher_astnode_t *node, rax *identifiers) 
 		_AST_GetIdentifiers(child, identifiers);
 	}
 
-	if(type == CYPHER_AST_LIST_COMPREHENSION || type == CYPHER_AST_ANY || type == CYPHER_AST_ALL) {
+	if(type == CYPHER_AST_LIST_COMPREHENSION || 
+	   type == CYPHER_AST_ANY || 
+	   type == CYPHER_AST_ALL || 
+	   type == CYPHER_AST_SINGLE || 
+	   type == CYPHER_AST_NONE) {
 		// A list comprehension has a local variable that should only be accessed within its scope;
 		// do not leave it in the identifiers map.
 		const cypher_astnode_t *variable_node = cypher_ast_list_comprehension_get_identifier(node);
