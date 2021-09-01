fce5899061e7d2bdcc946717c82f3c694c81c718
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2021-09-01 03:16:53 -0400

    Allow reconfiguring query timeout to 0 (#1904)
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>

--- tests/flow/test_config.py
+++ tests/flow/test_config.py
@@ -105,3 +105,29 @@ class testConfig(FlowTestsBase):
             assert("Unknown subcommand for GRAPH.CONFIG" in str(e))
             pass
 
+    def test08_config_restore_timeout_to_default(self):
+        # Revert memory limit to default
+        response = redis_con.execute_command("GRAPH.CONFIG SET QUERY_MEM_CAPACITY 0")
+        self.env.assertEqual(response, "OK")
+
+        # Change timeout value from default
+        response = redis_con.execute_command("GRAPH.CONFIG SET TIMEOUT 10")
+        self.env.assertEqual(response, "OK")
+
+        # Make sure config been updated.
+        response = redis_con.execute_command("GRAPH.CONFIG GET TIMEOUT")
+        expected_response = ["TIMEOUT", 10]
+        self.env.assertEqual(response, expected_response)
+
+        # Revert timeout to unlimited
+        response = redis_con.execute_command("GRAPH.CONFIG SET TIMEOUT 0")
+        self.env.assertEqual(response, "OK")
+
+        # Make sure config been updated.
+        response = redis_con.execute_command("GRAPH.CONFIG GET TIMEOUT")
+        expected_response = ["TIMEOUT", 0]
+        self.env.assertEqual(response, expected_response)
+
+        # Issue long-running query to validate the reconfiguration
+        result = redis_graph.query("UNWIND range(1,1000000) AS v RETURN COUNT(v)")
+        self.env.assertEqual(result.result_set[0][0], 1000000)
