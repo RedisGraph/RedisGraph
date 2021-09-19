713457c454431733563f7709ce5613e04b6c772b
swilly22 <roi@redislabs.com>
2021-09-19 11:36:12 +0300

    introduce edge by index scan op

--- src/execution_plan/ops/op_edge_by_index_scan.c
+++ src/execution_plan/ops/op_edge_by_index_scan.c
@@ -4,92 +4,145 @@
 * This file is available under the Redis Labs Source Available License Agreement
 */
 
-#include "op_node_by_index_scan.h"
+#include "op_edge_by_index_scan.h"
 #include "../../query_ctx.h"
 #include "shared/print_functions.h"
 #include "../../filter_tree/ft_to_rsq.h"
 
 // forward declarations
-static OpResult IndexScanInit(OpBase *opBase);
-static Record IndexScanConsume(OpBase *opBase);
-static Record IndexScanConsumeFromChild(OpBase *opBase);
-static OpResult IndexScanReset(OpBase *opBase);
-static void IndexScanFree(OpBase *opBase);
+static OpResult EdgeIndexScanInit(OpBase *opBase);
+static Record EdgeIndexScanConsume(OpBase *opBase);
+static Record EdgeIndexScanConsumeFromChild(OpBase *opBase);
+static OpResult EdgeIndexScanReset(OpBase *opBase);
+static void EdgeIndexScanFree(OpBase *opBase);
 
-static void IndexScanToString(const OpBase *ctx, sds *buf) {
-	IndexScan *op = (IndexScan *)ctx;
-	return ScanToString(ctx, buf, op->n.alias, op->n.label);
+static void EdgeIndexScanToString
+(
+	const OpBase *ctx,
+	sds *buf
+) {
+	ASSERT(ctx != NULL);
+	ASSERT(buf != NULL);
+
+	OpEdgeIndexScan *op = (OpEdgeIndexScan *)ctx;
+
+	*buf = sdscatprintf(*buf, "%s | ", ctx->name);
+	QGEdge_ToString(op->edge, buf);
 }
 
-OpBase *NewIndexScanOp(const ExecutionPlan *plan, Graph *g, NodeScanCtx n,
-		RSIndex *idx, FT_FilterNode *filter) {
+OpBase *NewEdgeIndexScanOp
+(
+	const ExecutionPlan *plan,
+	Graph *g,
+	QGEdge *e,
+	RSIndex *idx,
+	FT_FilterNode *filter
+) {
 	// validate inputs
-	ASSERT(g      != NULL);
-	ASSERT(idx    != NULL);
-	ASSERT(plan   != NULL);
-	ASSERT(filter != NULL);
+	ASSERT(g                       != NULL);
+	ASSERT(e                       != NULL);
+	ASSERT(idx                     != NULL);
+	ASSERT(plan                    != NULL);
+	ASSERT(filter                  != NULL);
+	ASSERT(QGEdge_Alias(e)         != NULL);
+	ASSERT(QGEdge_RelationCount(e) == 1);
 
-	IndexScan *op = rm_malloc(sizeof(IndexScan));
+	OpEdgeIndexScan *op      =  rm_malloc(sizeof(OpEdgeIndexScan));
 	op->g                    =  g;
-	op->n                    =  n;
 	op->idx                  =  idx;
+	op->edge                 =  e;
 	op->iter                 =  NULL;
 	op->filter               =  filter;
 	op->child_record         =  NULL;
 	op->unresolved_filters   =  NULL;
 	op->rebuild_index_query  =  false;
 
-	// Set our Op operations
-	OpBase_Init((OpBase *)op, OPType_NODE_BY_INDEX_SCAN, "Node By Index Scan", IndexScanInit, IndexScanConsume,
-				IndexScanReset, IndexScanToString, NULL, IndexScanFree, false, plan);
+	// set our Op operations
+	OpBase_Init(
+			(OpBase *)op,
+			OPType_EDGE_BY_INDEX_SCAN,
+			"Edge By Index Scan",
+			EdgeIndexScanInit,
+			EdgeIndexScanConsume,
+			EdgeIndexScanReset,
+			EdgeIndexScanToString,
+			NULL,
+			EdgeIndexScanFree,
+			false,
+			plan);
 
-	op->nodeRecIdx = OpBase_Modifies((OpBase *)op, n.alias);
+	// TODO: src/dest node might already be resolved
+	op->edgeRecIdx = OpBase_Modifies((OpBase *)op, QGEdge_Alias(e));
+	op->edgeRecIdx = OpBase_Modifies((OpBase *)op, QGNode_Alias(QGEdge_Src(e)));
+	op->edgeRecIdx = OpBase_Modifies((OpBase *)op, QGNode_Alias(QGEdge_Dest(e)));
+
 	return (OpBase *)op;
 }
 
-static OpResult IndexScanInit(OpBase *opBase) {
-	IndexScan *op = (IndexScan *)opBase;
+static OpResult EdgeIndexScanInit
+(
+	OpBase *opBase
+) {
+	OpEdgeIndexScan *op = (OpEdgeIndexScan *)opBase;
 
 	if(opBase->childCount > 0) {
 		// find out how many different entities are refered to 
 		// within the filter tree, if number of entities equals 1
-		// (current node being scanned) there's no need to re-build the index
+		// (current edge being scanned) there's no need to re-build the index
 		// query for every input record
 		rax *entities = FilterTree_CollectModified(op->filter);
-		op->rebuild_index_query = raxSize(entities) > 1; // this is us
+		op->rebuild_index_query = raxSize(entities) > 1;
 		raxFree(entities);
 
-		OpBase_UpdateConsume(opBase, IndexScanConsumeFromChild);
+		OpBase_UpdateConsume(opBase, EdgeIndexScanConsumeFromChild);
 	}
 
 	// resolve label ID now if it is still unknown
-	if(op->n.label_id == GRAPH_UNKNOWN_LABEL) {
+	if(QGEdge_RelationID(op->edge, 0) == GRAPH_UNKNOWN_RELATION) {
 		GraphContext *gc = QueryCtx_GetGraphCtx();
-		Schema *schema = GraphContext_GetSchema(gc, op->n.label, SCHEMA_NODE);
-		ASSERT(schema != NULL);
-		op->n.label_id = schema->id;
+		Schema *s = GraphContext_GetSchema(gc, QGEdge_Relation(op->edge, 0),
+				SCHEMA_EDGE);
+		ASSERT(s != NULL);
+
+		op->edge->reltypeIDs[0] = Schema_ID(s);
 	}
 
 	return OP_OK;
 }
 
-static inline void _UpdateRecord(IndexScan *op, Record r, EntityID node_id) {
-	// Populate the Record with the graph entity data.
-	Node n = GE_NEW_LABELED_NODE(op->n.label, op->n.label_id);
-	int res = Graph_GetNode(op->g, node_id, &n);
+static inline void _UpdateRecord
+(
+	OpEdgeIndexScan *op,
+	Record r,
+	EntityID edge_id
+) {
+	// populate Record with the graph entity data
+	// TODO: set edge relation, src and dest nodes
+	Edge e;
+
+	int res = Graph_GetEdge(op->g, edge_id, &e);
 	ASSERT(res != 0);
-	Record_AddNode(r, op->nodeRecIdx, n);
+
+	Record_AddEdge(r, op->edgeRecIdx, e);
 }
 
-static inline bool _PassUnresolvedFilters(const IndexScan *op, Record r) {
+static inline bool _PassUnresolvedFilters
+(
+	const OpEdgeIndexScan *op,
+	Record r
+) {
 	FT_FilterNode *unresolved_filters = op->unresolved_filters;
 	if(unresolved_filters == NULL) return true; // no filters
+
 	return FilterTree_applyFilters(unresolved_filters, r) == FILTER_PASS;
 }
 
-static Record IndexScanConsumeFromChild(OpBase *opBase) {
-	IndexScan *op = (IndexScan *)opBase;
-	const EntityID *nodeId = NULL;
+static Record EdgeIndexScanConsumeFromChild
+(
+	OpBase *opBase
+) {
+	OpEdgeIndexScan	*op = (OpEdgeIndexScan*)opBase;
+	const EntityID *edgeId = NULL;
 
 pull_index:
 	//--------------------------------------------------------------------------
@@ -97,10 +150,10 @@ pull_index:
 	//--------------------------------------------------------------------------
 
 	if(op->iter != NULL) {
-		while((nodeId = RediSearch_ResultsIteratorNext(op->iter, op->idx, NULL))
+		while((edgeId = RediSearch_ResultsIteratorNext(op->iter, op->idx, NULL))
 				!= NULL) {
-			// populate record with node
-			_UpdateRecord(op, op->child_record, *nodeId);
+			// populate record with edge
+			_UpdateRecord(op, op->child_record, *edgeId);
 			// apply unresolved filters
 			if(_PassUnresolvedFilters(op, op->child_record)) {
 				// clone the held Record, as it will be freed upstream
@@ -170,7 +223,8 @@ pull_index:
 		// reset it if already initialized
 		if(op->iter == NULL) {
 			// first call to consume, create query and iterator
-			RSQNode *rs_query_node = FilterTreeToQueryNode(&op->unresolved_filters, op->filter, op->idx);
+			RSQNode *rs_query_node = FilterTreeToQueryNode(
+					&op->unresolved_filters, op->filter, op->idx);
 			ASSERT(rs_query_node != NULL);
 			ASSERT(op->unresolved_filters == NULL);
 			op->iter = RediSearch_GetResultsIterator(rs_query_node, op->idx);
@@ -184,8 +238,11 @@ pull_index:
 	goto pull_index;
 }
 
-static Record IndexScanConsume(OpBase *opBase) {
-	IndexScan *op = (IndexScan *)opBase;
+static Record EdgeIndexScanConsume
+(
+	OpBase *opBase
+) {
+	OpEdgeIndexScan *op = (OpEdgeIndexScan *)opBase;
 
 	// create iterator on first call
 	if(op->iter == NULL) {
@@ -196,19 +253,19 @@ static Record IndexScanConsume(OpBase *opBase) {
 		op->iter = RediSearch_GetResultsIterator(rs_query_node, op->idx);
 	}
 
-	const EntityID *nodeId = RediSearch_ResultsIteratorNext(op->iter, op->idx,
+	const EntityID *edgeId = RediSearch_ResultsIteratorNext(op->iter, op->idx,
 			NULL);
-	if(!nodeId) return NULL;
+	if(!edgeId) return NULL;
 
-	// populate the Record with the actual node
+	// populate the Record with the actual edge
 	Record r = OpBase_CreateRecord((OpBase *)op);
-	_UpdateRecord(op, r, *nodeId);
+	_UpdateRecord(op, r, *edgeId);
 
 	return r;
 }
 
-static OpResult IndexScanReset(OpBase *opBase) {
-	IndexScan *op = (IndexScan *)opBase;
+static OpResult EdgeIndexScanReset(OpBase *opBase) {
+	OpEdgeIndexScan *op = (OpEdgeIndexScan *)opBase;
 
 	if(op->rebuild_index_query) {
 		RediSearch_ResultsIteratorFree(op->iter);
@@ -220,12 +277,12 @@ static OpResult IndexScanReset(OpBase *opBase) {
 	return OP_OK;
 }
 
-static void IndexScanFree(OpBase *opBase) {
-	IndexScan *op = (IndexScan *)opBase;
-	/* As long as this Index iterator is alive the index is
-	 * read locked, if this index scan operation is part of
-	 * a query which will modified this index we'll be stuck in
-	 * a dead lock, as we're unable to acquire index write lock. */
+static void EdgeIndexScanFree(OpBase *opBase) {
+	OpEdgeIndexScan *op = (OpEdgeIndexScan *)opBase;
+	// as long as this Index iterator is alive the index is
+	// read locked, if this index scan operation is part of
+	// a query which will modified this index we'll be stuck in
+	// a dead lock, as we're unable to acquire index write lock
 	if(op->iter) {
 		RediSearch_ResultsIteratorFree(op->iter);
 		op->iter = NULL;
---
+++ src/execution_plan/ops/op_edge_by_index_scan.h
@@ -0,0 +1,38 @@
+/*
+* Copyright 2018-2021 Redis Labs Ltd. and Contributors
+*
+* This file is available under the Redis Labs Source Available License Agreement
+*/
+
+#pragma once
+
+#include "op.h"
+#include "../execution_plan.h"
+#include "../../graph/graph.h"
+#include "redisearch_api.h"
+
+typedef struct {
+	OpBase op;
+	Graph *g;
+	bool rebuild_index_query;           // should we rebuild RediSearch index query for each input record
+	RSIndex *idx;                       // index to query
+	QGEdge *edge;                       // edge scanned
+	uint edgeRecIdx;                    // record index of source node
+	uint srcNodeRecIdx;                 // record index of destination node 
+	uint destNodeRecIdx;                // record index of edge
+	RSResultsIterator *iter;            // iterator over an index
+	FT_FilterNode *filter;              // index query
+	FT_FilterNode *unresolved_filters;  // subset of filter, contains filters that couldn't be resolved by index
+	Record child_record;                // input record in case op ins't a tap
+} OpEdgeIndexScan;
+
+// creates a new OpEdgeIndexScan operation
+OpBase *NewEdgeIndexScanOp
+(
+	const ExecutionPlan *plan,
+	Graph *g,
+	QGEdge *e,
+	RSIndex *idx,
+	FT_FilterNode *filter
+);
+
