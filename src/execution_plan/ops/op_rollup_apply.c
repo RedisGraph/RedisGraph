564753a756b330b7056b2b6b4e5fb626ec930b61
swilly22 <roi@redislabs.com>
2021-11-22 11:32:21 +0200

    remove while loop from rollup consume

--- src/execution_plan/ops/op_rollup_apply.c
+++ src/execution_plan/ops/op_rollup_apply.c
@@ -8,19 +8,27 @@
 #include "../../datatypes/array.h"
 #include "../execution_plan_build/execution_plan_modify.h"
 
-/* Forward declarations. */
+// forward declarations
 static OpResult RollUpApplyInit(OpBase *opBase);
 static Record RollUpApplyConsume(OpBase *opBase);
 static OpBase *RollUpApplyClone(const ExecutionPlan *plan, const OpBase *opBase);
 
-OpBase *NewRollUpApplyOp(const ExecutionPlan *plan, const char *alias) {
-	RollUpApply *op = rm_malloc(sizeof(RollUpApply));
-	op->op_arg = NULL;
-	op->bound_branch = NULL;
-	op->rhs_branch = NULL;
-	op->alias = alias;
+OpBase *NewRollUpApplyOp
+(
+	const ExecutionPlan *plan,
+	const char *alias
+) {
+	ASSERT(plan != NULL);
+	ASSERT(alias != NULL);
 
-	// Set our Op operations
+	RollUpApply *op = rm_malloc(sizeof(RollUpApply));
+
+	op->alias         =  alias;
+	op->op_arg        =  NULL;
+	op->rhs_branch    =  NULL;
+	op->bound_branch  =  NULL;
+
+	// set our Op callbacks
 	OpBase_Init((OpBase *)op, OPType_APPLY, "RollUpApply", RollUpApplyInit,
 				RollUpApplyConsume, NULL, NULL, RollUpApplyClone,
 				NULL, false, plan);
@@ -30,58 +38,67 @@ OpBase *NewRollUpApplyOp(const ExecutionPlan *plan, const char *alias) {
 	return (OpBase *)op;
 }
 
-static OpResult RollUpApplyInit(OpBase *opBase) {
+static OpResult RollUpApplyInit
+(
+	OpBase *opBase
+) {
 	ASSERT(opBase->childCount == 2);
 
 	RollUpApply *op = (RollUpApply *)opBase;
-	/* The op's bound branch and optional match branch have already been built as
-	 * the RollUpApply op's first and second child ops, respectively. */
-	op->bound_branch = opBase->children[0];
-	op->rhs_branch = opBase->children[1];
+	// the op's bound branch and optional match branch have already been built as
+	// the RollUpApply op's first and second child ops, respectively
+	op->bound_branch  =  opBase->children[0];
+	op->rhs_branch    =  opBase->children[1];
 
-	// Locate branch's Argument op tap.
-	op->op_arg = (Argument *)ExecutionPlan_LocateOp(op->rhs_branch, OPType_ARGUMENT);
-	ASSERT(op->op_arg);
+	// locate branch's Argument op tap
+	op->op_arg = (Argument *)ExecutionPlan_LocateOp(op->rhs_branch,
+			OPType_ARGUMENT);
+	ASSERT(op->op_arg != NULL);
 
 	return OP_OK;
 }
 
-static Record RollUpApplyConsume(OpBase *opBase) {
+static Record RollUpApplyConsume
+(
+	OpBase *opBase
+) {
 	RollUpApply *op = (RollUpApply *)opBase;
 
-	while(true) {
-		// Retrieve a Record from the bound branch.
-		Record r = OpBase_Consume(op->bound_branch);
-		if(r == NULL) return NULL; // Bound branch and this op are depleted.
+	// retrieve a Record from the bound branch
+	Record r = OpBase_Consume(op->bound_branch);
+	if(r == NULL) return NULL; // bound branch and this op are depleted
 
-		// Successfully pulled a new Record, propagate to the top of the RHS branch.
-		Argument_AddRecord(op->op_arg, OpBase_CloneRecord(r));
+	// successfully pulled a new Record
+	// propagate to the top of the RHS branch
+	Argument_AddRecord(op->op_arg, OpBase_CloneRecord(r));
 
-		Record rhs_record;
-		SIValue rolled_array = SI_Array(1);
-		// Pull and aggregate all records from the RHS branch.
-		while((rhs_record = OpBase_Consume(op->rhs_branch))) {
-			// Retrieve the value being aggregated from the RHS branch.
-			SIValue rollup_val = Record_Get(rhs_record, op->alias_idx);
-			// Add the value to the concatenation array.
-			SIArray_Append(&rolled_array, rollup_val);
-			// Release the record.
-			OpBase_DeleteRecord(rhs_record);
-		}
-
-		// Reset the RHS branch.
-		OpBase_PropagateReset(op->rhs_branch);
-
-		// Merge the concatenated array into the bound Record and return it..
-		Record_AddScalar(r, op->alias_idx, rolled_array);
-
-		return r;
+	Record rhs_record;
+	SIValue rolled_array = SI_Array(1);
+	// pull and aggregate all records from the RHS branch
+	while((rhs_record = OpBase_Consume(op->rhs_branch))) {
+		// retrieve the value being aggregated from the RHS branch
+		SIValue rollup_val = Record_Get(rhs_record, op->alias_idx);
+		// add the value to the concatenation array
+		SIArray_Append(&rolled_array, rollup_val);
+		// release the record
+		OpBase_DeleteRecord(rhs_record);
 	}
 
-	return NULL;
+	// reset the RHS branch
+	OpBase_PropagateReset(op->rhs_branch);
+
+	// merge the concatenated array into the bound Record and return it
+	Record_AddScalar(r, op->alias_idx, rolled_array);
+
+	return r;
 }
 
-static OpBase *RollUpApplyClone(const ExecutionPlan *plan, const OpBase *opBase) {
+static OpBase *RollUpApplyClone
+(
+	const ExecutionPlan *plan,
+	const OpBase *opBase
+) {
 	RollUpApply *op = (RollUpApply *)opBase;
 	return NewRollUpApplyOp(plan, op->alias);
 }
+
--- src/execution_plan/ops/op_rollup_apply.h
+++ src/execution_plan/ops/op_rollup_apply.h
@@ -10,22 +10,22 @@
 #include "op_argument.h"
 #include "../execution_plan.h"
 
-/* The RollUpApply op has a bound left-hand branch
- * and a right-hand branch that projects a single alias.
- * RollUpApply collects these expressions into an array
- * that is merged into the Record from the left-hand side.
- * This is unlike the standard Apply op in that for every record 
- * retrieved from the left-hand branch, the results of the right-hand
- * branch are concatenated into a list, rather than each record
- * from the right-hand branch being merged into the left-hand record
- * and passed upward once. */
+// the RollUpApply op has a bound left-hand branch
+// and a right-hand branch that projects a single alias
+// rollUpApply collects these expressions into an array
+// that is merged into the Record from the left-hand side
+// this is unlike the standard Apply op in that for every record
+// retrieved from the left-hand branch, the results of the right-hand
+// branch are concatenated into a list, rather than each record
+// from the right-hand branch being merged into the left-hand record
+// and passed upward once
 typedef struct {
 	OpBase op;
-	OpBase *bound_branch;           // Bound branch.
-	OpBase *rhs_branch;             // Right-hand branch.
-	Argument *op_arg;               // Right-hand branch tap.
-	uint alias_idx;                 // Index into Record of alias to be collected.
-	const char *alias;              // Alias to be collected from right-hand branch.
+	OpBase *bound_branch;  // bound branch
+	OpBase *rhs_branch;    // right-hand branch
+	Argument *op_arg;      // right-hand branch tap
+	uint alias_idx;        // index into Record of alias to be collected
+	const char *alias;     // alias to be collected from right-hand branch
 } RollUpApply;
 
 OpBase *NewRollUpApplyOp(const ExecutionPlan *plan, const char *alias);
