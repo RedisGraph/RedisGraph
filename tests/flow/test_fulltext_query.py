d98574a972a36b34a268ddb0a3f5be246b752c7e
Avi Avni <avi.avni@gmail.com>
2021-11-30 12:50:39 +0200

    add tests

--- tests/flow/test_fulltext_query.py
+++ tests/flow/test_fulltext_query.py
@@ -21,11 +21,26 @@ class testFulltextIndexQuery(FlowTestsBase):
     def populate_graph(self):
         graph.query("CALL db.idx.fulltext.createNodeIndex('L1', 'v')")
         graph.query("CALL db.idx.fulltext.createNodeIndex({ label: 'L2', stopwords: ['redis', 'world'] }, 'v')")
+        graph.query("CALL db.idx.fulltext.createNodeIndex('L3', { field: 'v1', weight: 1 }, { field: 'v2', weight: 1 })")
+        graph.query("CALL db.idx.fulltext.createNodeIndex('L4', { field: 'v1', weight: 1 }, { field: 'v2', weight: 2 })")
+        graph.query("CALL db.idx.fulltext.createNodeIndex('L5', { field: 'v', phonetic: 'dm:en' })")
+        graph.query("CALL db.idx.fulltext.createNodeIndex('L6', { field: 'v', nostem: true })")
 
         a = Node(label="L1", properties={"v": 'hello redis world'})
         graph.add_node(a)
 
         b = Node(label="L2", properties={"v": 'hello redis world'})
+        graph.add_node(b)
+
+        b = Node(label="L3", properties={"v1": 'hello world', "v2": 'hello redis'})
+        graph.add_node(b)
+        b = Node(label="L4", properties={"v1": 'hello world', "v2": 'hello redis'})
+        graph.add_node(b)
+
+        b = Node(label="L5", properties={"v": 'felix'})
+        graph.add_node(b)
+
+        b = Node(label="L6", properties={"v": 'there are seven words in this sentence'})
         graph.add_node(b)
 
         graph.flush()
@@ -57,4 +72,25 @@ class testFulltextIndexQuery(FlowTestsBase):
 
         # fulltext query L2 for world 
         result = graph.query("CALL db.idx.fulltext.queryNodes('L2', 'world')")
+        self.env.assertEquals(result.result_set, [])
+
+        # # fulltext query L1 and L3 for redis and compate score
+        # result1 = graph.query("CALL db.idx.fulltext.queryNodes('L3', 'redis')")
+        # result2 = graph.query("CALL db.idx.fulltext.queryNodes('L4', 'redis')")
+        # self.env.assertEquals(result1.result_set[0][1], result2.result_set[0][1])
+
+        expected_result = graph.query("MATCH (n:L5) RETURN n")
+
+        # fulltext query L5 for phelix
+        result = graph.query("CALL db.idx.fulltext.queryNodes('L5', 'phelix')")
+        self.env.assertEquals(result.result_set[0][0], expected_result.result_set[0][0])
+
+        expected_result = graph.query("MATCH (n:L6) RETURN n")
+
+        # fulltext query L6 for words
+        result = graph.query("CALL db.idx.fulltext.queryNodes('L6', 'words')")
+        self.env.assertEquals(result.result_set[0][0], expected_result.result_set[0][0])
+
+        # fulltext query L5 for word
+        result = graph.query("CALL db.idx.fulltext.queryNodes('L6', 'word')")
         self.env.assertEquals(result.result_set, [])
