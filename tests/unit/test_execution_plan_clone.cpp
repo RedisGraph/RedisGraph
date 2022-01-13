76964c164f757be511292bbac5273d41512b4e87
Avi Avni <avi.avni@gmail.com>
2022-01-13 13:06:38 +0200

    fix test

--- tests/unit/test_execution_plan_clone.cpp
+++ tests/unit/test_execution_plan_clone.cpp
@@ -43,7 +43,7 @@ class ExecutionPlanCloneTest: public ::testing::Test {
 
 	static void build_ast_and_plan(const char *query, AST **ast, ExecutionPlan **plan) {
 		QueryCtx *ctx = QueryCtx_GetQueryCtx();
-		ctx->query_data.query = query;
+		ctx->query_data.query_no_params = query;
 		cypher_parse_result_t *parse_result = cypher_parse(query, NULL, NULL, CYPHER_PARSE_ONLY_STATEMENTS);
 		*ast = AST_Build(parse_result);
 		*plan = NewExecutionPlan();
