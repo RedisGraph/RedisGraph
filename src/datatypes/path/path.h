6b4b0a55dd05d2376ea4932ca9f2b7f4d1fda41d
Avi Avni <avi.avni@gmail.com>
2022-01-27 13:57:00 +0200

    documentation

--- src/datatypes/path/path.h
+++ src/datatypes/path/path.h
@@ -14,129 +14,60 @@ typedef struct {
 	Edge *edges;    // Edges in path.
 } Path;
 
-/**
- * @brief  Creates a new Path with given capacity.
- * @param  len: length parameter.
- * @retval A pointer to an empty path with given capacity.
- */
+// creates a new Path with given capacity
 Path *Path_New(size_t len);
 
+// ensure the nodes and edge array in a specific len
 void Path_EnsureLen(Path *p, size_t len);
 
-/**
- * @brief  Appends a node to the path.
- * @param  *p: Path address.
- * @param  n: Given Node.
- */
+// appends a node to the path
 void Path_AppendNode(Path *p, Node n);
 
-/**
- * @brief  Appends a relationship to the path.
- * @param  *p: Path address.
- * @param  e: Given edge.
- */
+// appends a relationship to the path
 void Path_AppendEdge(Path *p, Edge e);
 
+// set node in a position
 void Path_SetNode(Path *p, uint i, Node n);
 
+// set edge in a position
 void Path_SetEdge(Path *p, uint i, Edge e);
 
-/**
- * @brief  Returns a refernce to a node in the specific index.
- * @note   Index refers to the node's position in the path, regarding the nodes order left-to-right.
- *         Assertion error will be thrown if index is out of bounds.
- * @param  p: Path.
- * @param  index: Node position.
- * @retval Node refernce.
- */
+// returns a refernce to a node in the specific index
 Node *Path_GetNode(const Path *p, int index);
 
-/**
- * @brief  Returns a refernce to an edge in the specific index.
- * @note   Index refers to the edge's position in the path, regarding the edges order left-to-right.
- *         Assertion error will be thrown if index is out of bounds.
- * @param  p: Path.
- * @param  index: Edge position.
- * @retval Edge reference.
- */
+// returns a refernce to an edge in the specific index
 Edge *Path_GetEdge(const Path *p, int index);
 
-/**
- * @brief  Removes the last node from the path.
- * @note   This action cause an inconsistency between the last edge and the new last node.
- *         Those actions should execute consecutively.
- * @param  p: Path.
- * @retval The removed node.
- */
+// removes the last node from the path
 Node Path_PopNode(Path *p);
 
-/**
- * @brief  Removes the last edge from the path.
- * @note   This action cause an inconsistency between the last node and the new last edge.
- *         Those actions should execute consecutively.
- * @param  p: Path.
- * @retval The removed edge.
- */
+// removes the last edge from the path
 Edge Path_PopEdge(Path *p);
 
-/**
- * @brief  Returns the last node in the path.
- * @note   This function does not check for path boundaries. Do not use in case of empty path.
- * @param  p: Path.
- * @retval Last node in the path.
- */
+// returns the last node in the path
 Node Path_Head(Path *p);
 
-/**
- * @brief  Returns the amount of nodes in the path.
- * @param  p: path.
- * @retval Amount of nodes in the path.
- */
+// returns the amount of nodes in the path
 size_t Path_NodeCount(const Path *p);
 
-/**
- * @brief  Returns the amount of edges in the path.
- * @param  p: path.
- * @retval Amount of edges in the path.
- */
+// returns the amount of edges in the path
 size_t Path_EdgeCount(const Path *p);
 
-/**
- * @brief  Returns the path length - amount of edges.
- * @note   For path with one node, the length is zero. For n nodes, the length is n-1.
- * @param  p: path.
- * @retval Path length.
- */
+// returns the path length - amount of edges
 size_t Path_Len(const Path *p);
 
-/**
- * @brief  Returns if a path contains a node.
- * @param  p: Path
- * @param  *n: Node
- * @retval True if the node is in the path, false otherwise.
- */
+// returns if a path contains a node
 bool Path_ContainsNode(const Path *p, Node *n);
 
-/**
- * @brief  Clones a path.
- * @param  p: Origin path.
- * @retval A pointer to a path struct with newly allocated array copies.
- */
+// clones a path
 Path *Path_Clone(const Path *p);
 
-/**
- * @brief  Reverse the order of the path.
- * @param  p: Path.
- * @retval None
- */
+// reverse the order of the path
 void Path_Reverse(Path *p);
 
+// clear the path
 void Path_Clear(Path *p);
 
-/**
- * @brief  Deletes the path nodes and edges arrays.
- * @note   Do not delete path allocation itself.
- * @param  p: Path pointer.
- */
+// deletes the path nodes and edges arrays
 void Path_Free(Path *p);
 
