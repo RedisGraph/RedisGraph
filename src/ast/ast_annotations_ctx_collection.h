3d94af39934617fefeb08f6f62f91bf12ab11f72
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2022-02-13 10:55:38 -0500

    Test ast star replacement (#2147)
    * WIP
    
    * Add logic for building new clauses
    
    * TEMP use branch of parser
    
    * Remove logic for star projection annotations
    
    * Add tests
    
    * Rewrite query before validation
    
    * manual construction of RETURN clause AST node
    
    * Support WITH and CREATE replacements with all variables
    
    * Update parser commit
    
    * Fix memory leaks
    
    * mostly indentations
    
    * Address PR comments
    
    * removed unused import
    
    * Allow projection of other expressions alongside stars
    
    * Update libcypher-parser upstream
    
    * refactor
    
    * Fix up comments
    
    * Add test for uniquing star-projected columns
    
    * refactor use rax instead
    
    * add tests
    
    * removed unused function
    
    Co-authored-by: swilly22 <roi@redislabs.com>
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>
    Co-authored-by: Avi Avni <avi.avni@gmail.com>

--- src/ast/ast_annotations_ctx_collection.h
+++ src/ast/ast_annotations_ctx_collection.h
@@ -13,7 +13,6 @@ typedef cypher_ast_annotation_context_t AnnotationCtx;
  * specific AST nodes annotation use case. For each context there is a dedicated inlined
  * getter and setter. */
 typedef struct {
-	AnnotationCtx *project_all_ctx; // Context containing aliases for WITH/RETURN * projections.
 	AnnotationCtx *named_paths_ctx; // Annotation context for named paths projections.
 	AnnotationCtx *to_string_ctx;   // Annotation context for AST_ToString of astnode.
 	uint anon_count;                // Counter of anonymous entities already created.
@@ -23,12 +22,8 @@ AST_AnnotationCtxCollection *AST_AnnotationCtxCollection_New();
 
 AnnotationCtx *AST_AnnotationCtxCollection_GetNamedPathsCtx(const AST_AnnotationCtxCollection *anot_ctx_collection);
 
-AnnotationCtx *AST_AnnotationCtxCollection_GetProjectAllCtx(const AST_AnnotationCtxCollection *anot_ctx_collection);
-
 AnnotationCtx *AST_AnnotationCtxCollection_GetToStringCtx(const AST_AnnotationCtxCollection *anot_ctx_collection);
 
 void AST_AnnotationCtxCollection_SetNamedPathsCtx(AST_AnnotationCtxCollection *anot_ctx_collection, AnnotationCtx *named_paths_ctx);
-
-void AST_AnnotationCtxCollection_SetProjectAllCtx(AST_AnnotationCtxCollection *anot_ctx_collection, AnnotationCtx *project_all_ctx);
 
 void AST_AnnotationCtxCollection_Free(AST_AnnotationCtxCollection *anot_ctx_collection);
