5127e218637adf78bbf1e4b826236041dda4dd1a
Roi Lipman <swilly22@users.noreply.github.com>
2022-03-06 16:07:15 +0200

    aggregation should return a default values when aggregation is keyles… (#2151)
    * aggregation should return a default values when aggregation is keyless and no data was processed
    
    * update graph deletion tests
    
    * address PR comments
    
    * use child record to perform forced aggregation
    
    * update construction logic
    
    * WIP
    
    * fix build
    
    * fixs for test
    
    * wip
    
    * fix
    
    * fix
    
    * fix
    
    * fix tck
    
    * reviews
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * refactor
    
    * address review comments
    
    * fix leak
    
    Co-authored-by: Avi Avni <avi.avni@gmail.com>

--- src/execution_plan/ops/op_aggregate.h
+++ src/execution_plan/ops/op_aggregate.h
@@ -13,16 +13,16 @@
 
 typedef struct {
 	OpBase op;
-	uint *record_offsets;               /* Record IDs for key and aggregate exps. */
-	AR_ExpNode **key_exps;              /* Array of expressions used to calculate the group key. */
-	AR_ExpNode **aggregate_exps;        /* Array of expressions that aggregate data for each key. */
-	rax *groups;                        /* Map of all groups built by this operation. */
-	Group *group;                       /* Last accessed group. */
-	SIValue *group_keys;                /* Array of values that represent a key associated with a Group of aggregations. */
-	CacheGroupIterator *group_iter;     /* Iterator for walking all groups. */
-	uint key_count;                     /* Number of key expressions. */
-	uint aggregate_count;               /* Number of aggregating expressions. */
-	bool should_cache_records;          /* Records should be cached if we're sorting after aggregation. */
+	uint *record_offsets;               // record IDs for key and aggregate exps
+	AR_ExpNode **key_exps;              // array of expressions used to calculate the group key
+	AR_ExpNode **aggregate_exps;        // array of expressions that aggregate data for each key
+	rax *groups;                        // map of all groups built by this operation
+	Group *group;                       // last accessed group
+	SIValue *group_keys;                // array of values that represent a key associated with a Group of aggregations
+	CacheGroupIterator *group_iter;     // iterator for walking all groups
+	uint key_count;                     // number of key expressions
+	uint aggregate_count;               // number of aggregating expressions
+	bool should_cache_records;          // records should be cached if we're sorting after aggregation
 } OpAggregate;
 
 OpBase *NewAggregateOp(const ExecutionPlan *plan, AR_ExpNode **exps, bool should_cache_records);
