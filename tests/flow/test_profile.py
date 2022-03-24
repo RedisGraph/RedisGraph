27cba2f23597e02f63b028395ee5dda2d7c469ae
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2022-03-24 04:13:25 -0400

    Fix crash on profiling traversals after op free (#2242)
    Co-authored-by: Avi Avni <avi.avni@gmail.com>

--- tests/flow/test_profile.py
+++ tests/flow/test_profile.py
@@ -19,7 +19,7 @@ class testProfile(FlowTestsBase):
         redis_con = self.env.getConnection()
         redis_graph = Graph(GRAPH_ID, redis_con)
 
-    def test_profile(self):
+    def test01_profile(self):
         q = """UNWIND range(1, 3) AS x CREATE (p:Person {v:x})"""
         profile = redis_con.execute_command("GRAPH.PROFILE", GRAPH_ID, q)
         profile = [x[0:x.index(',')].strip() for x in profile]
@@ -35,3 +35,12 @@ class testProfile(FlowTestsBase):
         self.env.assertIn("Project | Records produced: 2", profile)
         self.env.assertIn("Filter | Records produced: 2", profile)
         self.env.assertIn("Node By Label Scan | (p:Person) | Records produced: 3", profile)
+
+    def test02_profile_after_op_free(self):
+        # validate that profile works properly on freed operations
+        q = """MATCH (a:L)-[*]->() SET a.v = 5"""
+        profile = redis_con.execute_command("GRAPH.PROFILE", GRAPH_ID, q)
+        profile = [x[0:x.index(',')].strip() for x in profile]
+        self.env.assertIn("Update | Records produced: 0", profile)
+        self.env.assertIn("Conditional Variable Length Traverse | Records produced: 0", profile)
+        self.env.assertIn("Node By Label Scan | (a:L) | Records produced: 0", profile)
