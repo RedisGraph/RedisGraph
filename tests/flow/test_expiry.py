4c352e344ee71d58906e28ab176b6df4aa02c22b
Guy Korland <gkorland@gmail.com>
2022-03-27 10:22:58 +0300

    Add eviction_expiry to ramp.yml (#2245)
    * Update ramp.yml
    
    * Add expire test
    
    * refactor test
    
    Co-authored-by: swilly22 <roi@redislabs.com>

---
+++ tests/flow/test_expiry.py
@@ -0,0 +1,31 @@
+import time
+from RLTest import Env
+from redisgraph import Graph
+from redis import ResponseError
+
+GRAPH_ID = "expire"
+
+class testExpiry():
+    def test01_expire_graph(self):
+        # create a redisgraph object
+        env = Env(decodeResponses=True)
+        redis_con = env.getConnection()
+        redis_graph = Graph(GRAPH_ID, redis_con)
+
+        # create a single node
+        redis_graph.query("create ()")
+
+        # expire key in 100ms
+        expire_in_ms = 100
+        env.assertTrue(redis_con.pexpire(GRAPH_ID, expire_in_ms))
+
+        # wait for key to expire
+        time.sleep((expire_in_ms * 3) / 1000) # convert from sec to ms
+
+        # key should have been evicted by now
+        env.assertFalse(redis_con.exists(GRAPH_ID))
+        try:
+            slowlog = redis_con.execute_command("GRAPH.SLOWLOG", GRAPH_ID)
+        except ResponseError as e:
+            env.assertIn("Invalid graph operation on empty key", str(e))
+
