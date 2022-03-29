60627bfc431f8254a5d5afc9e1a1d976d9b388a6
Avi Avni <avi.avni@gmail.com>
2022-03-29 10:01:34 +0300

    remove redundant node label matrix population (#2253)

--- src/serializers/decoders/prev/v10/decode_graph.c
+++ src/serializers/decoders/prev/v10/decode_graph.c
@@ -189,6 +189,9 @@ GraphContext *RdbLoadGraphContext_v10(RedisModuleIO *rdb) {
 	if(GraphDecodeContext_Finished(gc->decoding_context)) {
 		Graph *g = gc->g;
 
+		// set the node label matrix
+		Serializer_Graph_SetNodeLabels(g);
+
 		// revert to default synchronization behavior
 		Graph_SetMatrixPolicy(g, SYNC_POLICY_FLUSH_RESIZE);
 		Graph_ApplyAllPending(g, true);
