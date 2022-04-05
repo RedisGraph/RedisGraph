dbed71cd2e62b5eba11856ec5dd3673aec0a43b8
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2022-04-05 17:37:58 -0400

    Add tests

--- tests/flow/test_filters.py
+++ tests/flow/test_filters.py
@@ -1,5 +1,6 @@
 from RLTest import Env
 from redisgraph import Graph
+from redis import ResponseError
 
 class testFilters():
     def __init__(self):
@@ -37,4 +38,20 @@ class testFilters():
         # test negation of xor operation
         expected = [[i, j] for i in range(1, 6) for j in range(1, 6) if not (i % 2 != j % 2)]
         result = g.query("MATCH (n:N), (m:N) WHERE NOT (n.b XOR m.b) RETURN n.v, m.v ORDER BY n.v, m.v")
+        self.env.assertEqual(result.result_set,  expected)
+
+    def test02_filter_unhandled_operations(self):
+        g = Graph("g", self.env.getConnection())
+
+        try:
+            g.query("MATCH (a) WHERE [()-[]-() | 0] AND NULL ^ a OR -3 RETURN a")
+            self.env.assertTrue(False)
+        except ResponseError as e:
+            self.env.assertContains("Encountered unhandled unary operator", str(e))
+
+    def test03_filter_null_with_boolean(self):
+        g = Graph("g", self.env.getConnection())
+
+        result = g.query("WITH 5 AS a WHERE NOT (NULL AND FALSE) RETURN a")
+        expected = [[5]]
         self.env.assertEqual(result.result_set,  expected)
