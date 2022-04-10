0b47149d3bd96da9ce7468693662001ea1e9ec75
rafie <rafi@redislabs.com>
2022-04-10 09:57:41 +0300

    fixes 32

--- tests/flow/test_ro_query.py
+++ tests/flow/test_ro_query.py
@@ -4,8 +4,8 @@ import time
 slave_con = None
 master_con = None
 
-def checkSlaveSynced(env, slaveConn, graph_name, time_out=5):
-    time.sleep(time_out)
+def checkSlaveSynced(env, masterConn, slaveConn, graph_name):
+    masterConn.execute_command("WAIT", "1", "0")
     res = slaveConn.execute_command("keys", graph_name)
     env.assertEqual(res, [graph_name])
 
@@ -72,14 +72,12 @@ class test_read_only_query(FlowTestsBase):
             pass
 
     def test03_test_replica_read_only(self):
-        if OS == 'macos':
-            self.env.skip()
         # This test checks that only RO_QUERY is valid on replicas.
         graph_name = "Test_RO_QUERY_command_on_replica"
         graph = Graph(master_con, graph_name)
         graph.query("UNWIND range(0,20) as i CREATE ()")
         slave_con.execute_command("REPLICAOF", "127.0.0.1", self.env.envRunner.port)
-        checkSlaveSynced(self.env, slave_con, graph_name)
+        checkSlaveSynced(self.env, master_con, slave_con, graph_name)
         raw_result_set = slave_con.execute_command("GRAPH.RO_QUERY", graph_name, "MATCH (n) RETURN COUNT(n)", "--compact")
         result_set = query_result.QueryResult(graph, raw_result_set).result_set
         self.env.assertEqual(21, result_set[0][0])
