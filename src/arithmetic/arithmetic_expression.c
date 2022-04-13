ecd668f7fe137c1d5fbce09cd673aa2c4bc817bd
alon <alonreshef24@gmail.com>
2022-04-13 11:36:36 +0300

    Fix stackoverflow bug in evaluating arithmetic expression which are large lists

--- src/arithmetic/arithmetic_expression.c
+++ src/arithmetic/arithmetic_expression.c
@@ -30,6 +30,9 @@
 // return child at position 'idx' of 'n'
 #define NODE_CHILD(n, idx) (n)->op.children[(idx)]
 
+// maximum size for which an array of SIValue will be stack-allocated, otherwise it will be heap-allocated.
+#define MAX_ARRAY_SIZE_ON_STACK 32
+
 //------------------------------------------------------------------------------
 // Forward declarations
 //------------------------------------------------------------------------------
@@ -408,6 +411,10 @@ static inline void _AR_EXP_FreeResultsArray(SIValue *results, int count) {
 	for(int i = 0; i < count; i ++) {
 		SIValue_Free(results[i]);
 	}
+    // Large arrays are heap-allocated, so here is where we free it.
+    if (count > MAX_ARRAY_SIZE_ON_STACK) {
+        rm_free(results);
+    }
 }
 
 static AR_EXP_Result _AR_EXP_EvaluateFunctionCall
@@ -421,8 +428,14 @@ static AR_EXP_Result _AR_EXP_EvaluateFunctionCall
 	int child_count = node->op.child_count;
 
 	// evaluate each child before evaluating current node
-	SIValue sub_trees[child_count];
-
+    SIValue *sub_trees;
+    // if array size is above the threshold, we allocate it on the heap (otherwise on stack)
+    SIValue sub_trees_on_stack[MAX_ARRAY_SIZE_ON_STACK];
+    if (child_count > MAX_ARRAY_SIZE_ON_STACK) {
+        sub_trees = rm_malloc(child_count * sizeof(SIValue));
+    } else {
+        sub_trees = sub_trees_on_stack;
+    }
 	bool param_found = false;
 	for(int child_idx = 0; child_idx < NODE_CHILD_COUNT(node); child_idx++) {
 		SIValue v;
