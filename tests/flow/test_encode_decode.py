c7f0f0cc6d8b89df000bcf54e240b2ae7385e320
Avi Avni <avi.avni@gmail.com>
2022-06-12 17:27:42 +0300

    Test random graph in encode/decode (#2392)
    * wip work on random graphs
    
    * address review
    
    * lower labels number
    
    * fix validation

--- tests/flow/test_encode_decode.py
+++ tests/flow/test_encode_decode.py
@@ -1,12 +1,24 @@
 from common import *
+from random_graph import create_random_schema, create_random_graph, run_random_graph_ops, ALL_OPS
 import re
 
 redis_con = None
 
 
+def compare_nodes_result_set(env, result_set_a, result_set_b):
+    env.assertEquals(len(result_set_a), len(result_set_b))
+    for i in range(0, len(result_set_a)):
+        env.assertEquals(result_set_a[i][0].id, result_set_b[i][0].id)
+        env.assertEquals(set(result_set_a[i][0].labels), set(
+            result_set_b[i][0].labels))
+        env.assertEquals(result_set_a[i][0].properties,
+                         result_set_b[i][0].properties)
+
+
 class test_encode_decode(FlowTestsBase):
     def __init__(self):
-        self.env = Env(decodeResponses=True, moduleArgs='VKEY_MAX_ENTITY_COUNT 10')
+        self.env = Env(decodeResponses=True,
+                       moduleArgs='VKEY_MAX_ENTITY_COUNT 10 NODE_CREATION_BUFFER 100')
         global redis_con
         redis_con = self.env.getConnection()
 
@@ -41,18 +53,21 @@ class test_encode_decode(FlowTestsBase):
 
         actual = redis_graph.query(query)
         # Validate no compaction, all IDs are the same
-        self.env.assertEquals(expected_nodes_id_after_delete.result_set, actual.result_set)
+        self.env.assertEquals(
+            expected_nodes_id_after_delete.result_set, actual.result_set)
 
         # Validate reuse of node ids - create 3 nodes.
         redis_graph.query("UNWIND range (0, 2) as i CREATE (:Node)")
         actual = redis_graph.query(query)
-        self.env.assertEquals(expected_full_graph_nodes_id.result_set, actual.result_set)
+        self.env.assertEquals(
+            expected_full_graph_nodes_id.result_set, actual.result_set)
 
     def test03_edges_over_multiple_keys(self):
         graph_name = "edges_over_multiple_keys"
         redis_graph = Graph(redis_con, graph_name)
         # Create 3 edges meta keys
-        redis_graph.query("UNWIND range(0,20) as i CREATE (:Src)-[:R {val:i}]->(:Dest)")
+        redis_graph.query(
+            "UNWIND range(0,20) as i CREATE (:Src)-[:R {val:i}]->(:Dest)")
         # Return all the edges, before and after saving & loading the RDB, and check equality
         query = "MATCH (:Src)-[e:R]->(:Dest) return e"
         expected = redis_graph.query(query)
@@ -65,28 +80,34 @@ class test_encode_decode(FlowTestsBase):
         graph_name = "no_compaction_on_edges_delete"
         redis_graph = Graph(redis_con, graph_name)
         # Create 3 nodes meta keys
-        redis_graph.query("UNWIND range(0,20) as i CREATE (:Src)-[:R]->(:Dest)")
+        redis_graph.query(
+            "UNWIND range(0,20) as i CREATE (:Src)-[:R]->(:Dest)")
         # Return all the edges, before and after saving & loading the RDB, and check equality
         query = "MATCH (:Src)-[e:R]->(:Dest) WITH e ORDER by id(e) return COLLECT(id(e))"
         expected_full_graph_nodes_id = redis_graph.query(query)
         # Delete 3 edges.
-        redis_graph.query("MATCH (:Src)-[e:R]->(:Dest) WHERE id(e) IN [7,14,20] DELETE e")
+        redis_graph.query(
+            "MATCH (:Src)-[e:R]->(:Dest) WHERE id(e) IN [7,14,20] DELETE e")
         expected_nodes_id_after_delete = redis_graph.query(query)
         # Save RDB & Load from RDB
         redis_con.execute_command("DEBUG", "RELOAD")
         actual = redis_graph.query(query)
         # Validate no compaction, all IDs are the same
-        self.env.assertEquals(expected_nodes_id_after_delete.result_set, actual.result_set)
+        self.env.assertEquals(
+            expected_nodes_id_after_delete.result_set, actual.result_set)
         # Validate reuse of edges ids - create 3 edges.
-        redis_graph.query("UNWIND range (0,2) as i CREATE (:Src)-[:R]->(:Dest)")
+        redis_graph.query(
+            "UNWIND range (0,2) as i CREATE (:Src)-[:R]->(:Dest)")
         actual = redis_graph.query(query)
-        self.env.assertEquals(expected_full_graph_nodes_id.result_set, actual.result_set)
+        self.env.assertEquals(
+            expected_full_graph_nodes_id.result_set, actual.result_set)
 
     def test05_multiple_edges_over_multiple_keys(self):
         graph_name = "multiple_edges_over_multiple_keys"
         redis_graph = Graph(redis_con, graph_name)
         # Create 3 edges meta keys
-        redis_graph.query("CREATE (n1:Src {val:1}), (n2:Dest {val:2}) WITH n1, n2 UNWIND range(0,20) as i CREATE (n1)-[:R {val:i}]->(n2)")
+        redis_graph.query(
+            "CREATE (n1:Src {val:1}), (n2:Dest {val:2}) WITH n1, n2 UNWIND range(0,20) as i CREATE (n1)-[:R {val:i}]->(n2)")
         # Return all the edges, before and after saving & loading the RDB, and check equality
         query = "MATCH (:Src)-[e:R]->(:Dest) return e"
         expected = redis_graph.query(query)
@@ -99,22 +120,27 @@ class test_encode_decode(FlowTestsBase):
         graph_name = "no_compaction_on_multiple_edges_delete"
         redis_graph = Graph(redis_con, graph_name)
         # Create 3 nodes meta keys
-        redis_graph.query("CREATE (n1:Src {val:1}), (n2:Dest {val:2}) WITH n1, n2 UNWIND range(0,20) as i CREATE (n1)-[:R]->(n2)")
+        redis_graph.query(
+            "CREATE (n1:Src {val:1}), (n2:Dest {val:2}) WITH n1, n2 UNWIND range(0,20) as i CREATE (n1)-[:R]->(n2)")
         # Return all the edges, before and after saving & loading the RDB, and check equality
         query = "MATCH (:Src)-[e:R]->(:Dest) WITH e ORDER by id(e) return COLLECT(id(e))"
         expected_full_graph_nodes_id = redis_graph.query(query)
         # Delete 3 edges.
-        redis_graph.query("MATCH (:Src)-[e:R]->(:Dest) WHERE id(e) IN [7,14,20] DELETE e")
+        redis_graph.query(
+            "MATCH (:Src)-[e:R]->(:Dest) WHERE id(e) IN [7,14,20] DELETE e")
         expected_nodes_id_after_delete = redis_graph.query(query)
         # Save RDB & Load from RDB
         redis_con.execute_command("DEBUG", "RELOAD")
         actual = redis_graph.query(query)
         # Validate no compaction, all IDs are the same
-        self.env.assertEquals(expected_nodes_id_after_delete.result_set, actual.result_set)
+        self.env.assertEquals(
+            expected_nodes_id_after_delete.result_set, actual.result_set)
         # Validate reuse of edges ids - create 3 edges.
-        redis_graph.query("MATCH (n1:Src {val:1}), (n2:Dest {val:2}) WITH n1, n2 UNWIND range (0,2) as i CREATE (n1)-[:R]->(n2)")
+        redis_graph.query(
+            "MATCH (n1:Src {val:1}), (n2:Dest {val:2}) WITH n1, n2 UNWIND range (0,2) as i CREATE (n1)-[:R]->(n2)")
         actual = redis_graph.query(query)
-        self.env.assertEquals(expected_full_graph_nodes_id.result_set, actual.result_set)
+        self.env.assertEquals(
+            expected_full_graph_nodes_id.result_set, actual.result_set)
 
     def test07_index_after_encode_decode_in_v7(self):
         graph_name = "index_after_encode_decode_in_v7"
@@ -135,7 +161,8 @@ class test_encode_decode(FlowTestsBase):
         # Create a multi-key graph.
         graph1_name = "v7_graph_1"
         graph1 = Graph(redis_con, graph1_name)
-        graph1.query("UNWIND range(0,21) AS i CREATE (a:L {v: i})-[:E]->(b:L2 {v: i})")
+        graph1.query(
+            "UNWIND range(0,21) AS i CREATE (a:L {v: i})-[:E]->(b:L2 {v: i})")
 
         # Create a single-key graph.
         graph2_name = "v7_graph_2"
@@ -162,9 +189,12 @@ class test_encode_decode(FlowTestsBase):
         # Create 10 nodes
         redis_graph.query("UNWIND range(0,10) as v CREATE (:L {v: v})")
         # Create 3 edges of different relation types connecting 6 different nodes
-        redis_graph.query("MATCH (a:L {v: 1}), (b:L {v: 2}) CREATE (a)-[:R1]->(b)")
-        redis_graph.query("MATCH (a:L {v: 3}), (b:L {v: 4}) CREATE (a)-[:R2]->(b)")
-        redis_graph.query("MATCH (a:L {v: 5}), (b:L {v: 6}) CREATE (a)-[:R3]->(b)")
+        redis_graph.query(
+            "MATCH (a:L {v: 1}), (b:L {v: 2}) CREATE (a)-[:R1]->(b)")
+        redis_graph.query(
+            "MATCH (a:L {v: 3}), (b:L {v: 4}) CREATE (a)-[:R2]->(b)")
+        redis_graph.query(
+            "MATCH (a:L {v: 5}), (b:L {v: 6}) CREATE (a)-[:R3]->(b)")
 
         # Retrieve all the edges before and after saving & loading the RDB to check equality
         query = "MATCH (:L)-[e]->(:L) RETURN ID(e), type(e) ORDER BY ID(e)"
@@ -186,32 +216,36 @@ class test_encode_decode(FlowTestsBase):
         log = logfile.read()
 
         # Set configuration
-        response = redis_con.execute_command("GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 10")
+        response = redis_con.execute_command(
+            "GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 10")
         self.env.assertEqual(response, "OK")
 
         graph_name = "vkey_max_entity_count"
         redis_graph = Graph(redis_con, graph_name)
-        
+
         # Create 30 nodes
         redis_graph.query("UNWIND range(0, 30) as v CREATE (:L {v: v})")
-        
+
         # Save RDB & Load from RDB
         redis_con.save()
 
         # Set configuration
-        response = redis_con.execute_command("GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 5")
+        response = redis_con.execute_command(
+            "GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 5")
         self.env.assertEqual(response, "OK")
 
         # Save RDB & Load from RDB
         redis_con.save()
-        
+
         log = logfile.read()
 
-        matches = re.findall("Created (.) virtual keys for graph vkey_max_entity_count", log)
+        matches = re.findall(
+            "Created (.) virtual keys for graph vkey_max_entity_count", log)
 
         self.env.assertEqual(matches, ['3', '6'])
 
-        matches = re.findall("Deleted (.) virtual keys for graph vkey_max_entity_count", log)
+        matches = re.findall(
+            "Deleted (.) virtual keys for graph vkey_max_entity_count", log)
 
         self.env.assertEqual(matches, ['3', '6'])
 
@@ -219,48 +253,89 @@ class test_encode_decode(FlowTestsBase):
         redis_con.flushall()
 
         # Set configuration
-        response = redis_con.execute_command("GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 20000")
+        response = redis_con.execute_command(
+            "GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 20000")
         self.env.assertEqual(response, "OK")
 
         graph_name = "large_deleted_nodes"
         redis_graph = Graph(redis_con, graph_name)
-        
+
         # Create 60000 nodes and 30000 edges
-        redis_graph.query("UNWIND range(0, 30000) as v CREATE (:L {v: v})-[:R]->(:M {v: v})")
+        redis_graph.query(
+            "UNWIND range(0, 30000) as v CREATE (:L {v: v})-[:R]->(:M {v: v})")
 
         # Delete 20000 nodes and 10000 edges
-        redis_graph.query("MATCH (n:L)-[:R]->(m:M) WHERE id(n) <= 20000 DELETE n, m")
-        
-        res_before = redis_graph.query("MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
+        redis_graph.query(
+            "MATCH (n:L)-[:R]->(m:M) WHERE id(n) <= 20000 DELETE n, m")
+
+        res_before = redis_graph.query(
+            "MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
 
         # Save RDB & Load from RDB
         redis_con.execute_command("DEBUG", "RELOAD")
 
         # Validate all data lodaed correctly
-        res_after = redis_graph.query("MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
+        res_after = redis_graph.query(
+            "MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
         self.env.assertEquals(res_before.result_set, res_after.result_set)
 
     def test11_decode_multi_edge_relation_with_deleted_nodes(self):
         redis_con.flushall()
 
         # Set configuration
-        response = redis_con.execute_command("GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 20000")
+        response = redis_con.execute_command(
+            "GRAPH.CONFIG SET VKEY_MAX_ENTITY_COUNT 20000")
         self.env.assertEqual(response, "OK")
 
         graph_name = "large_deleted_nodes"
         redis_graph = Graph(redis_con, graph_name)
-        
+
         # Create 60000 nodes and 60000 edges
-        redis_graph.query("UNWIND range(0, 30000) as v CREATE (n:L {v: v}), (m:M {v: v}) WITH n, m CREATE (n)-[:R]->(m), (n)-[:R]->(m)")
+        redis_graph.query(
+            "UNWIND range(0, 30000) as v CREATE (n:L {v: v}), (m:M {v: v}) WITH n, m CREATE (n)-[:R]->(m), (n)-[:R]->(m)")
 
         # Delete 20000 nodes and 40000 edges
-        redis_graph.query("MATCH (n:L)-[:R]->(m:M) WHERE id(n) <= 20000 DELETE n, m")
-        
-        res_before = redis_graph.query("MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
+        redis_graph.query(
+            "MATCH (n:L)-[:R]->(m:M) WHERE id(n) <= 20000 DELETE n, m")
+
+        res_before = redis_graph.query(
+            "MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
 
         # Save RDB & Load from RDB
         redis_con.execute_command("DEBUG", "RELOAD")
 
         # Validate all data lodaed correctly
-        res_after = redis_graph.query("MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
+        res_after = redis_graph.query(
+            "MATCH (n:L)-[r:R]->(m:M) RETURN id(n), id(r), id(m)")
         self.env.assertEquals(res_before.result_set, res_after.result_set)
+
+    def test12_random_graph(self):
+        graph_name = "random_graph"
+        redis_graph = Graph(redis_con, graph_name)
+
+        nodes, edges = create_random_schema()
+        res = create_random_graph(redis_graph, nodes, edges)
+
+        nodes_before = redis_graph.query("MATCH (n) RETURN n")
+        edges_before = redis_graph.query("MATCH ()-[e]->() RETURN e")
+
+        redis_con.execute_command("DEBUG", "RELOAD")
+
+        nodes_after = redis_graph.query("MATCH (n) RETURN n")
+        edges_after = redis_graph.query("MATCH ()-[e]->() RETURN e")
+
+        compare_nodes_result_set(self.env, nodes_before.result_set, nodes_after.result_set)
+        self.env.assertEquals(edges_before.result_set, edges_after.result_set)
+
+        res = run_random_graph_ops(redis_graph, nodes, edges, ALL_OPS)
+
+        nodes_before = redis_graph.query("MATCH (n) RETURN n")
+        edges_before = redis_graph.query("MATCH ()-[e]->() RETURN e")
+
+        redis_con.execute_command("DEBUG", "RELOAD")
+
+        nodes_after = redis_graph.query("MATCH (n) RETURN n")
+        edges_after = redis_graph.query("MATCH ()-[e]->() RETURN e")
+
+        compare_nodes_result_set(self.env, nodes_before.result_set, nodes_after.result_set)
+        self.env.assertEquals(edges_before.result_set, edges_after.result_set)
