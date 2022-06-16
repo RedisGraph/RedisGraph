4b1c771b4bdebb3a6a41eb0ba52aa68303669cdf
Avi Avni <avi.avni@gmail.com>
2022-06-16 20:12:29 +0300

    fix crash in double to string (#2410)
    * fix crash in double to string
    
    * address review

--- src/value.c
+++ src/value.c
@@ -259,7 +259,6 @@ const char *SIType_ToString(SIType t) {
 
 void SIValue_ToString(SIValue v, char **buf, size_t *bufferLen, size_t *bytesWritten) {
 	// uint64 max and int64 min string representation requires 21 bytes
-	// float defaults to print 6 digit after the decimal-point
 	// checkt for enough space
 	if(*bufferLen - *bytesWritten < 64) {
 		*bufferLen += 64;
@@ -277,8 +276,20 @@ void SIValue_ToString(SIValue v, char **buf, size_t *bufferLen, size_t *bytesWri
 		*bytesWritten += snprintf(*buf + *bytesWritten, *bufferLen, "%s", v.longval ? "true" : "false");
 		break;
 	case T_DOUBLE:
-		*bytesWritten += snprintf(*buf + *bytesWritten, *bufferLen, "%f", v.doubleval);
+	{
+		size_t n = snprintf(*buf + *bytesWritten, *bufferLen - *bytesWritten, "%f", v.doubleval);
+		// check if there was enough space in the buffer
+		if(*bytesWritten + n > *bufferLen) {
+			// realloc the buffer
+			*bufferLen = *bytesWritten + n + 1;
+			*buf = rm_realloc(*buf, sizeof(char) * *bufferLen);
+
+			// write it again
+			snprintf(*buf + *bytesWritten, *bufferLen - *bytesWritten, "%f", v.doubleval);
+		}
+		*bytesWritten += n;
 		break;
+	}
 	case T_NODE:
 		Node_ToString(v.ptrval, buf, bufferLen, bytesWritten, ENTITY_ID);
 		break;
@@ -301,6 +312,8 @@ void SIValue_ToString(SIValue v, char **buf, size_t *bufferLen, size_t *bytesWri
 		*bytesWritten += snprintf(*buf + *bytesWritten, *bufferLen, "POINTER");
 		break;
 	case T_POINT:
+		// max string length is 32 chars of string + 10 * 2 chars for the floats
+		// = 52 bytes that already checked in the header of the function
 		*bytesWritten += snprintf(*buf + *bytesWritten, *bufferLen, "point({latitude: %f, longitude: %f})", Point_lat(v), Point_lon(v));
 		break;
 	default:
