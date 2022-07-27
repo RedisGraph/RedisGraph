bdbf6e0063023b36077da71a782b67eee7747df3
DvirDukhan <dvir@redis.com>
2022-07-27 15:58:54 +0300

    revert attribute_set.c

--- src/graph/entities/attribute_set.c
+++ src/graph/entities/attribute_set.c
@@ -215,9 +215,9 @@ bool AttributeSet_Update
 	SIValue *current = AttributeSet_Get(*set, attr_id);
 	ASSERT(current != ATTRIBUTE_NOTFOUND);
 
-	// compare current value to new value, only update if current != new, otherwise preform shallow update.
+	// compare current value to new value, only update if current != new
 	if(unlikely(SIValue_Compare(*current, value, NULL) == 0)) {
-		return true;
+		return false;
 	}
 
 	// value != current, update entity
