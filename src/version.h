01db05c1298d49b8c45fad0c3397f4dc4ea31b4d
Avi Avni <avi.avni@gmail.com>
2022-08-01 11:36:41 +0300

    bump version 2.10.1

--- src/version.h
+++ src/version.h
@@ -8,7 +8,7 @@
 
 #define REDISGRAPH_VERSION_MAJOR 2
 #define REDISGRAPH_VERSION_MINOR 10
-#define REDISGRAPH_VERSION_PATCH 0
+#define REDISGRAPH_VERSION_PATCH 1
 
 #define REDISGRAPH_SEMANTIC_VERSION(major, minor, patch) \
   (major * 10000 + minor * 100 + patch)
