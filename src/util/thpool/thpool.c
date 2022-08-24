7362d1ab5df80e0ee18013ef88eb747f73b932dc
Avi Avni <avi.avni@gmail.com>
2022-08-24 11:48:17 +0300

    return wating for alive threads

--- src/util/thpool/thpool.c
+++ src/util/thpool/thpool.c
@@ -219,10 +219,10 @@ void thpool_destroy(thpool_* thpool_p) {
 
 	/* Poll remaining threads */
 	// do not wait forever for threads to complete their work
-	//while(thpool_p->num_threads_alive) {
-	//	bsem_post_all(thpool_p->jobqueue.has_jobs);
-	//	sleep(1);
-	//}
+	while(thpool_p->num_threads_alive) {
+		bsem_post_all(thpool_p->jobqueue.has_jobs);
+		sleep(1);
+	}
 
 	/* Job queue cleanup */
 	jobqueue_destroy(&thpool_p->jobqueue);
