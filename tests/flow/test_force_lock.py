e15c28e09e0660caf492d3465f0f9ec8ec3ac64c
Avi Avni <avi.avni@gmail.com>
2022-09-20 09:54:54 +0300

    merge test

--- tests/flow/test_force_lock.py
+++ tests/flow/test_force_lock.py
@@ -11,59 +11,39 @@ class test_ForceUnlock():
         redis_con = self.env.getConnection()
         redis_graph = Graph(redis_con, GRAPH_ID)
 
-    # Check that when the last operation is MERGE
-    # that did not performed any modifications
+    # Check that when the last operation
+    # did not performed any modifications
     # the locks are released appropriately
-    def test01_merge_lock(self):
+    def test01_last_operation_lock(self):
         logfilename = self.env.envRunner._getFileName("master", ".log")
         logfile = open(f"{self.env.logDir}/{logfilename}")
 
         query = """MERGE (n:N) RETURN n"""
         redis_graph.query(query)
 
-        # query is expected to:
-        # 1. lock
-        # 2. create (l:L) and (m:M)
-        # 3. match (n:N)
-        # 4. unlock
-        query = """MERGE (l:L) MERGE (m:M) MERGE (n:N) RETURN l, m, n"""
-        redis_graph.query(query)
+        queries = [
+            # query is expected to:
+            # 1. lock
+            # 2. create (l:L) and (m:M)
+            # 3. match (n:N)
+            # 4. unlock
+            """MERGE (l:L) MERGE (m:M) MERGE (n:N) RETURN l, m, n""",
+            # query is expected to:
+            # 1. lock
+            # 2. create (n:N)
+            # 3. match (n)
+            # 4. unlock
+            """CREATE (n:N) WITH 1 AS x MATCH (n) WHERE n.v > 2 SET n.v = 2""",
+            # query is expected to:
+            # 1. lock
+            # 2. create (n:N)
+            # 3. match
+            # 4. unlock
+            """CREATE (n:N) WITH n MATCH (x) WHERE 1 > 2 CREATE  (m:M)"""
+        ]
 
-        log = logfile.read()
-        self.env.assertNotContains("forced unlocking commit", log)
-
-    # Check that when the last operation is UPDATE
-    # that did not performed any modifications
-    # the locks are released appropriately
-    def test02_update_lock(self):
-        logfilename = self.env.envRunner._getFileName("master", ".log")
-        logfile = open(f"{self.env.logDir}/{logfilename}")
-
-        # query is expected to:
-        # 1. lock
-        # 2. create (n:N)
-        # 3. match (n)
-        # 4. unlock
-        query = """CREATE (n:N) WITH 1 AS x MATCH (n) WHERE n.v > 2 SET n.v = 2"""
-        redis_graph.query(query)
-
-        log = logfile.read()
-        self.env.assertNotContains("forced unlocking commit", log)
-
-    # Check that when the last operation is CREATE
-    # that did not performed any modifications
-    # the locks are released appropriately
-    def test03_create_lock(self):
-        logfilename = self.env.envRunner._getFileName("master", ".log")
-        logfile = open(f"{self.env.logDir}/{logfilename}")
-
-        # query is expected to:
-        # 1. lock
-        # 2. create (n:N)
-        # 3. match
-        # 4. unlock
-        query = """CREATE (n:N) WITH n MATCH (x) WHERE 1 > 2 CREATE  (m:M)"""
-        redis_graph.query(query)
+        for query in queries:
+            redis_graph.query(query)
 
         log = logfile.read()
         self.env.assertNotContains("forced unlocking commit", log)
