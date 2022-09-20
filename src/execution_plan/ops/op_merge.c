0a14bd4c0b9ac60709b4bb2fece5063c3cea0ee5
Avi Avni <avi.avni@gmail.com>
2022-09-20 13:40:57 +0300

    fix force unlock (#2599)
    * fix force unlock
    
    * remove print
    
    * address review comments
    
    * Update test_force_lock.py
    
    * Update test_force_lock.py
    
    * Update create_functions.c
    
    * Update op_update.c
    
    * merge test
    
    * address review
    
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>

--- src/execution_plan/ops/op_merge.c
+++ src/execution_plan/ops/op_merge.c
@@ -415,14 +415,9 @@ static Record MergeConsume
 		}
 	}
 
-	// release lock
-	bool lock_acquired = must_create_records |
-		                 (array_len(op->node_pending_updates) > 0) |
-						 (array_len(op->edge_pending_updates) > 0);
-
-	if(lock_acquired) {
-		QueryCtx_UnlockCommit(&op->op);
-	}
+	// always try to releasing the lock, even though this merge operation might not made any changes
+	// this is required in the situation where this merge op is the last write operation within the execution-plan.
+	QueryCtx_UnlockCommit(&op->op);
 
 	//--------------------------------------------------------------------------
 	// free updates
