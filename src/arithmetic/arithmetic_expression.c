4e25c0a142f502ac1430e93a07724a27693a7627
nafraf <nafraf@gmail.com>
2022-10-03 22:34:17 -0500

    _AR_EXP_EvaluateFunctionCall: Validate arg count

--- src/arithmetic/arithmetic_expression.c
+++ src/arithmetic/arithmetic_expression.c
@@ -160,7 +160,7 @@ static AR_ExpNode *_AR_EXP_CloneOp(AR_ExpNode *exp) {
 	return clone;
 }
 
-static void _AR_EXP_ValidateArgsCount
+static bool _AR_EXP_ValidateArgsCount
 (
 	AR_FuncDesc *fdesc,
 	uint argc
@@ -170,12 +170,14 @@ static void _AR_EXP_ValidateArgsCount
 		// Set the query-level error.
 		ErrorCtx_SetError("Received %d arguments to function '%s', expected at least %d", argc,
 						  fdesc->name, fdesc->min_argc);
+		return false;
 	} else if(fdesc->max_argc < argc) {
 		// Set the query-level error.
 		ErrorCtx_SetError("Received %d arguments to function '%s', expected at most %d", argc,
 						  fdesc->name, fdesc->max_argc);
-		return;
+		return false;
 	}
+	return true;
 }
 
 AR_ExpNode *AR_EXP_NewOpNode
@@ -461,6 +463,12 @@ static AR_EXP_Result _AR_EXP_EvaluateFunctionCall
 	}
 
 	if(param_found) res = EVAL_FOUND_PARAM;
+
+	if(!_AR_EXP_ValidateArgsCount(node->op.f, child_count)) {
+		// the arg count failed its validations and set an error message
+		res = EVAL_ERR;
+		goto cleanup;
+	}
 
 	// validate before evaluation
 	if(!_AR_EXP_ValidateInvocation(node->op.f, sub_trees, child_count)) {
