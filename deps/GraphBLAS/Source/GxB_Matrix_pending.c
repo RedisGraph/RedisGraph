8a4bb07457be469e0ee907200acc88900032ff06
swilly22 <roi@redislabs.com>
2022-10-21 09:40:29 +0300

    wait due to hyperhash

--- deps/GraphBLAS/Source/GxB_Matrix_pending.c
+++ deps/GraphBLAS/Source/GxB_Matrix_pending.c
@@ -9,7 +9,7 @@ GrB_Info GxB_Matrix_Pending     // does matrix has pending operations
 (
 	GrB_Matrix A,           // matrix to query
 	bool *pending           // are there any pending operations
- ) {
+) {
 	GB_WHERE1 ("GxB_Matrix_Pending (A)") ;
 	//--------------------------------------------------------------------------
 	// check inputs
@@ -17,7 +17,7 @@ GrB_Info GxB_Matrix_Pending     // does matrix has pending operations
 	GB_RETURN_IF_NULL_OR_FAULTY(A) ;
 	GB_RETURN_IF_NULL(pending) ;
 
-	(*pending) = (A->nzombies > 0) || (GB_Pending_n(A) > 0) ;
+	(*pending) = (GB_ANY_PENDING_WORK (A) || GB_NEED_HYPER_HASH (A));
 
 	return (GrB_SUCCESS) ;
 }
