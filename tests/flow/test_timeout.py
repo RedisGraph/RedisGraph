1bf686bdff33088ce1d4efbd1651280c10983e46
Avi Avni <avi.avni@gmail.com>
2022-11-02 17:15:17 +0200

    fix flaky timeout test

--- tests/flow/test_timeout.py
+++ tests/flow/test_timeout.py
@@ -62,7 +62,7 @@ class testQueryTimeout():
         redis_con.execute_command("GRAPH.CONFIG SET timeout 0")
 
         # construct a graph and create multiple indices
-        query = """UNWIND range(0, 500000) AS x CREATE (p:Person {age: x%90, height: x%200, weight: x%80})"""
+        query = """UNWIND range(0, 20000) AS x CREATE (p:Person {age: x%90, height: x%200, weight: x%80})"""
         redis_graph.query(query)
 
         query = """CREATE INDEX ON :Person(age, height, weight)"""
@@ -106,9 +106,10 @@ class testQueryTimeout():
         for q in queries:
             q += " LIMIT 2"
             try:
-                redis_graph.query(q, timeout=20)
+                res = redis_graph.query(q, timeout=20)
             except:
                 print(q)
+                print(res.run_time_ms)
                 self.env.assertTrue(False)
 
 
