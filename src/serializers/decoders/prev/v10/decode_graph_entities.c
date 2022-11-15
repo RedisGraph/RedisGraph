79a6b5ec58d8bd89697ca4056e46a7a57469bb5b
Avi Avni <avi.avni@gmail.com>
2022-11-15 11:51:40 +0200

    license change (#2705) (#2706)
    * license change
    
    * update

--- src/serializers/decoders/prev/v10/decode_graph_entities.c
+++ src/serializers/decoders/prev/v10/decode_graph_entities.c
@@ -1,8 +1,8 @@
 /*
-* Copyright 2018-2022 Redis Labs Ltd. and Contributors
-*
-* This file is available under the Redis Labs Source Available License Agreement
-*/
+ * Copyright Redis Ltd. 2018 - present
+ * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
+ * the Server Side Public License v1 (SSPLv1).
+ */
 
 #include "decode_v10.h"
 
