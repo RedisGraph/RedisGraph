85926278f04754f1e266e5c292c54c66d9c3684b
Victor Polevoy <fx@thefx.co>
2022-11-30 16:02:08 +0100

    Move to threaded-collections.
    1. Changes the query info storage to store a query info per thread.
    This is valid since every single thread can only have on query assigned.
    2. Since the waiting queries collection can be accessed from multiple
    different threads, it is not made a per-thread collection and requires
    a synchronisation primitive to be used when accessing the collection.
    For this case, an rw-lock is used.

--- src/graph/info.h
+++ src/graph/info.h
@@ -3,15 +3,29 @@
  * Licensed under your choice of the Redis Source Available License 2.0 (RSALv2) or
  * the Server Side Public License v1 (SSPLv1).
  */
+/*
+ * This file contains all the necessary data structures for the "graph.info"
+ * command to operate. In order to show the statistics, it is required to store
+ * and accumulate the information about different queries, their stages and the
+ * time those spend in these stages. To tackle multithreading issues, either
+ * atomic variables are used, or read-write locks, or the knowledge of
+ * the thread ids is used to directly navigate to a certain element in linear
+ * time and in a lock-free manner.
+*/
 
 #pragma once
 
 #include <stdatomic.h>
 #include <stdbool.h>
-#include "util/simple_timer.h"
+#include <stdint.h>
+
+#include <pthread.h>
+
+#include "../util/simple_timer.h"
 
 typedef struct QueryCtx QueryCtx;
 
+// Holds the necessary per-query statistics.
 typedef struct {
     // The time it spent waiting.
     uint64_t waiting_time_milliseconds;
@@ -30,6 +44,10 @@ typedef struct {
 QueryInfo QueryInfo_New();
 // Assigns the query context to the query info.
 void QueryInfo_SetQueryContext(QueryInfo *, const QueryCtx *);
+// Returns the query context associated with the query info.
+const QueryCtx* QueryInfo_GetQueryContext(const QueryInfo *);
+// Returns true if the query info object is valid and can be worked with.
+bool QueryInfo_IsValid(const QueryInfo *);
 // Returns the total time spent by a query waiting, executing and reporting.
 uint64_t QueryInfo_GetTotalTimeSpent(const QueryInfo, bool *is_ok);
 // Returns the time the query spent waiting.
@@ -55,16 +73,27 @@ typedef struct {
     QueryInfo *queries;
 } QueryInfoStorage;
 
-// Creates a new query info storage.
+// Creates a new query info storage with the specified capacity.
+QueryInfoStorage QueryInfoStorage_NewWithCapacity(const uint64_t capacity);
+// Creates a new query info storage with default capacity.
 QueryInfoStorage QueryInfoStorage_New();
 // Clears the storage by removing all the queries stored.
 void QueryInfoStorage_Clear(QueryInfoStorage *);
 // Deallocates the storage.
 void QueryInfoStorage_Free(QueryInfoStorage *);
+// Returns the number of elements which are valid within the storage.
+uint32_t QueryInfoStorage_ValidLength(const QueryInfoStorage *);
 // Returns the current length of the storage (in elements).
 uint32_t QueryInfoStorage_Length(const QueryInfoStorage *);
 // Adds a query info object.
 void QueryInfoStorage_Add(QueryInfoStorage *, const QueryInfo);
+// Sets the value of an existing element within the array.
+bool QueryInfoStorage_Set(QueryInfoStorage *, const uint64_t, const QueryInfo);
+// Returns a pointer to an already existing value in the storage at provided
+// index.
+QueryInfo* QueryInfoStorage_Get(const QueryInfoStorage *, const uint64_t);
+// Resets the element value in the index.
+bool QueryInfoStorage_ResetElement(QueryInfoStorage *, const uint64_t);
 // Returns true if the element has successfully been removed.
 bool QueryInfoStorage_Remove(QueryInfoStorage *, const QueryInfo *);
 // Returns true if the element has successfully been removed.
@@ -73,21 +102,28 @@ bool QueryInfoStorage_RemoveByContext(QueryInfoStorage *, const QueryCtx *);
 typedef struct {
     // Storage for query information for waiting queries.
     QueryInfoStorage waiting_queries;
+    // Synchronisation primitive to use when doing anything with the
+    // waiting_queries collection, as it is supposed to be touched concurrently.
+    pthread_rwlock_t waiting_queries_rwlock;
     // Storage for query information for queries being executed.
-    QueryInfoStorage executing_queries;
+    QueryInfoStorage executing_queries_per_thread;
     // Storage for query information for reporting currently queries.
-    QueryInfoStorage reporting_queries;
+    QueryInfoStorage reporting_queries_per_thread;
+    // Global synchronisation primitive, locking all the collecitons. Required
+    // when the read operation is performed over all the collections. Example:
+    // "GRAPH.INFO QUERIES" iterates over each of the collections.
+    pthread_rwlock_t global_collection_lock;
     // Maximum registered time a query was spent waiting, executing and
     // reporting the results.
     atomic_uint_fast64_t max_query_pipeline_time;
 } Info;
 
-// Create a new info structure.
-Info Info_New();
+// Create a new info structure. Returns true on successfull creation.
+bool Info_New(Info *);
 // Reset an already existing info structure.
 void Info_Reset(Info *);
-// Free the info structure's contents.
-void Info_Free(const Info);
+// Free the info structure's contents. Returns true on successfull free.
+bool Info_Free(Info *);
 // Insert a query information into the info structure. The query is supposed
 // to be added right after being successfully parsed and known to the module,
 // and before it starts being executed.
@@ -118,8 +154,6 @@ void Info_IndicateQueryFinishedReporting
     Info *,
     const QueryCtx *
 );
-// Find a QueryInfo object by the provided context.
-QueryInfo* Info_FindQueryInfo(Info *, const QueryCtx *);
 // Return the total number of queries currently queued or being executed.
 uint64_t Info_GetTotalQueriesCount(const Info *);
 // Return the number of queries currently waiting to be executed.
