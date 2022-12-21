24c54fa33c08d12aca9819102ac130f735cd2e00
Victor Polevoy <fx@thefx.co>
2022-12-21 11:42:53 +0100

    Implement the GRAPH.INFO QUERIES PREV <n>.
    1. Changes the code so that the compact and non-compact reply modes
    are abstracted out for the info command.
    2. Implements the PREV argument of the QUERIES subcommand of the
    GRAPH.INFO which prints out the finished queries.
    3. Adds tests for the changes in the circular buffer.
    4. Adds tests for the PREV argument handling implementation.

--- src/util/circular_buffer.h
+++ src/util/circular_buffer.h
@@ -10,24 +10,24 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-// forward declaration
 typedef struct _CircularBuffer _CircularBuffer;
 typedef _CircularBuffer* CircularBuffer;
 
-typedef void (*CircularBuffer_ViewAllCallback)(void *user_data, const void *item);
+// Should return true if the read has to be stopped right after the invocation.
+typedef bool (*CircularBuffer_ReadAllCallback)(void *user_data, const void *item);
 typedef void (*CircularBuffer_ElementFree)(void *user_data, void *item);
 
 CircularBuffer CircularBuffer_New
 (
-	const uint64_t item_size,
-	const uint64_t capacity
+    const uint64_t item_size,
+    const uint64_t capacity
 );
 
 void CircularBuffer_SetDeleter
 (
-	CircularBuffer cb,
-	CircularBuffer_ElementFree deleter,
-	void *user_data
+    CircularBuffer cb,
+    CircularBuffer_ElementFree deleter,
+    void *user_data
 );
 
 uint64_t CircularBuffer_GetCapacity(const CircularBuffer circular_buffer);
@@ -40,52 +40,64 @@ bool CircularBuffer_SetCapacity(CircularBuffer *cb_ptr, const uint64_t new_capac
 // a full circle, the read offset as well.
 void CircularBuffer_Add
 (
-	CircularBuffer circular_buffer,
-	const void *item
+    CircularBuffer circular_buffer,
+    const void *item
 );
 
 // Removes the oldest item from buffer. If there was nothing to read, and so,
 // remove, the item pointer is not changed.
 void CircularBuffer_Read
 (
-	CircularBuffer cb,  // buffer to remove item from
-	void *item          // [output] pointer populated with removed item
+    CircularBuffer cb,  // buffer to remove item from
+    void *item          // [output] pointer populated with removed item
 );
 
-// Iterates over the entire unread contents and invoke the callback.
+// Iterates over the entire unread contents and invokes the callback.
 // Does it without making copies (memcpy) but rather passing a view pointer to
-// the callback function.
+// the callback function, suitable for a reinterpret cast.
+// This function adjusts the read offset.
+void CircularBuffer_ReadAll
+(
+    CircularBuffer buffer,
+    CircularBuffer_ReadAllCallback callback,
+    void *user_data
+);
+
+// Iterates over the entire unread contents and invokes the callback.
+// Does it without making copies (memcpy) but rather passing a view pointer to
+// the callback function, suitable for a reinterpret cast.
+// This doesn't adjust the read offset.
 void CircularBuffer_ViewAll
 (
-	CircularBuffer buffer,
-	CircularBuffer_ViewAllCallback callback,
-	void *user_data
+    CircularBuffer buffer,
+    CircularBuffer_ReadAllCallback callback,
+    void *user_data
 );
 
 // Returns a pointer to the current item without adjusting the read offset.
 void *CircularBuffer_ViewCurrent
 (
-	const CircularBuffer cb
+    const CircularBuffer cb
 );
 
 // Returns number of items in buffer.
 uint64_t CircularBuffer_ItemCount
 (
-	const CircularBuffer cb  // buffer to inspect
+    const CircularBuffer cb  // buffer to inspect
 );
 
 uint64_t CircularBuffer_ItemSize
 (
-	const CircularBuffer cb
+    const CircularBuffer cb
 );
 
 // Returns true if buffer there is nothing to read.
 bool CircularBuffer_IsEmpty
 (
-	const CircularBuffer cb  // buffer to inspect
+    const CircularBuffer cb  // buffer to inspect
 );
 
 void CircularBuffer_Free
 (
-	CircularBuffer cb
+    CircularBuffer cb
 );
