c5fc105ecc87300e833a228efa3067a5bc061a6f
Victor Polevoy <fx@thefx.co>
2022-12-21 15:11:42 +0100

    Unify "GRAPH.INFO QUERIES [CURRENT] [PREV]".
    1. Makes the output to follow the same pattern and the queries to be
    printed within a single array, first the historical records and then
    the current queries.
    2. Changes some titles of the output fields.
    3. Allows both the flags to appear at the same time.

--- tests/flow/test_graph_info.py
+++ tests/flow/test_graph_info.py
@@ -9,9 +9,16 @@ import time
 
 GRAPH_ID = "GRAPH_INFO_TEST"
 INFO_QUERIES_CURRENT_COMMAND = 'GRAPH.INFO QUERIES CURRENT'
-INFO_QUERIES_PREV_COMMAND = 'GRAPH.INFO QUERIES PREV 100'
+INFO_QUERIES_PREV_COMMAND = 'GRAPH.INFO QUERIES PREV 100 --compact'
+INFO_QUERIES_CURRENT_PREV_COMMAND = 'GRAPH.INFO QUERIES CURRENT PREV 100'
 INFO_GET_COMMAND_TEMPLATE = 'GRAPH.INFO GET %s'
 INFO_RESET_ALL_COMMAND = 'GRAPH.INFO RESET *'
+
+# Keys
+CURRENT_MAXIMUM_WAIT_DURATION_KEY_NAME = 'Current maximum query wait duration'
+GLOBAL_INFO_KEY_NAME = 'Global info'
+QUERY_STAGE_EXECUTING = 1
+QUERY_STAGE_FINISHED = 3
 
 
 def thread_execute_command(cmd_and_args, _args):
@@ -88,31 +95,31 @@ class testGraphInfoFlow(FlowTestsBase):
         # No other queries has finished yet.
         # No queries are waiting to be executed as the only query we
         # launched must have already started being executed.
-        self.env.assertEquals(info['Global info']['Total waiting queries count'], 0, depth=1)
+        self.env.assertEquals(info[GLOBAL_INFO_KEY_NAME]['Total waiting queries count'], 0, depth=1)
         # The current query is being executed.
         executing_queries_count = len(queries)
-        self.env.assertEquals(info['Global info']['Total executing queries count'], executing_queries_count, depth=1)
+        self.env.assertEquals(info[GLOBAL_INFO_KEY_NAME]['Total executing queries count'], executing_queries_count, depth=1)
         # As there are no other queries, no query is reporting.
-        self.env.assertEquals(info['Global info']['Total reporting queries count'], 0, depth=1)
+        self.env.assertEquals(info[GLOBAL_INFO_KEY_NAME]['Total reporting queries count'], 0, depth=1)
         max_wait_time = 0
         # Check that the only query being currently executed is ours.
         for i in range(0, executing_queries_count):
             executing_query = list_to_dict(info['Queries'][i])
             if assert_receive_time is not None:
-                self.env.assertGreaterEqual(executing_query['Receive timestamp (milliseconds)'], assert_receive_time, depth=1)
-            self.env.assertLessEqual(executing_query['Receive timestamp (milliseconds)'], assert_timestamp, depth=1)
+                self.env.assertGreaterEqual(executing_query['Received at'], assert_receive_time, depth=1)
+            self.env.assertLessEqual(executing_query['Received at'], assert_timestamp, depth=1)
             self.env.assertEquals(executing_query['Graph name'], GRAPH_ID, depth=1)
             self.env.assertEquals(executing_query['Stage'], 1, depth=1)
             self.env.assertEquals(executing_query['Query'], queries[i], depth=1)
-            self.env.assertGreaterEqual(executing_query['Current total duration (milliseconds)'], 0, depth=1)
-            wait_time = executing_query['Current wait duration (milliseconds)']
+            self.env.assertGreaterEqual(executing_query['Total duration'], 0, depth=1)
+            wait_time = executing_query['Wait duration']
             max_wait_time = max(max_wait_time, wait_time)
             self.env.assertGreaterEqual(wait_time, 0, depth=1)
-            self.env.assertGreaterEqual(executing_query['Current execution duration (milliseconds)'], 0, depth=1)
-            self.env.assertEqual(executing_query['Current reporting duration (milliseconds)'], 0, depth=1)
+            self.env.assertGreaterEqual(executing_query['Execution duration'], 0, depth=1)
+            self.env.assertEqual(executing_query['Report duration'], 0, depth=1)
         # It has already accounted for the graph creation query, which might
         # have taken longer than a millisecond.
-        self.env.assertGreaterEqual(info['Global info']['Max query wait duration (milliseconds)'], max_wait_time, depth=1)
+        self.env.assertGreaterEqual(info[GLOBAL_INFO_KEY_NAME][CURRENT_MAXIMUM_WAIT_DURATION_KEY_NAME], max_wait_time, depth=1)
 
     def _assert_one_executing_query(self, info, query, assert_receive_time=None):
         queries = []
@@ -131,11 +138,14 @@ class testGraphInfoFlow(FlowTestsBase):
                 return False
         return True
 
-    def _wait_till_queries_start_being_executed(self, query_count=1, timeout=2):
+    def _wait_till_queries_start_being_executed(self, query_count=1, timeout=2, prev=False):
         wait_step = 0.1
         waited_time = 0
         while True:
-            info = self.conn.execute_command(INFO_QUERIES_CURRENT_COMMAND)
+            cmd = INFO_QUERIES_CURRENT_COMMAND
+            if prev == True:
+                cmd = INFO_QUERIES_CURRENT_PREV_COMMAND
+            info = self.conn.execute_command(cmd)
             count = get_total_executing_queries_from_info_cmd_result(info)
             if count >= query_count:
                 # Found the query being executed.
@@ -180,13 +190,13 @@ class testGraphInfoFlow(FlowTestsBase):
         self.env.assertEquals(results.result_set[0][0], 90000, depth=1)
 
         info = list_to_dict(self.conn.execute_command(INFO_QUERIES_CURRENT_COMMAND))
-        self.env.assertGreaterEqual(info['Global info']['Max query wait duration (milliseconds)'], 0, depth=1)
+        self.env.assertGreaterEqual(info[GLOBAL_INFO_KEY_NAME][CURRENT_MAXIMUM_WAIT_DURATION_KEY_NAME], 0, depth=1)
 
         reset = self.conn.execute_command(INFO_RESET_ALL_COMMAND)
         self.env.assertTrue(reset, depth=1)
 
         info = list_to_dict(self.conn.execute_command(INFO_QUERIES_CURRENT_COMMAND))
-        self.env.assertEqual(info['Global info']['Max query wait duration (milliseconds)'], 0, depth=1)
+        self.env.assertEqual(info[GLOBAL_INFO_KEY_NAME][CURRENT_MAXIMUM_WAIT_DURATION_KEY_NAME], 0, depth=1)
 
     def _assert_info_get_result(self, result,
         nodes=0, relationships=0,
@@ -220,17 +230,59 @@ class testGraphInfoFlow(FlowTestsBase):
         query = """CYPHER end=100 RETURN reduce(sum = 0, n IN range(1, $end) | sum ^ n)"""
         graph = Graph(self.conn, GRAPH_ID)
         query_issue_timestamp_ms = get_unix_timestamp_milliseconds()
+
         results = graph.query(query)
         self.env.assertEquals(results.result_set[0][0], 0, depth=1)
 
-        prev = []
         results = self.conn.execute_command(INFO_QUERIES_PREV_COMMAND)
-        for r in results:
-            prev.append(list_to_dict(r))
-        last_query = prev[-1]
-        self.env.assertGreaterEqual(last_query['Receive timestamp (milliseconds)'], query_issue_timestamp_ms, depth=1)
-        self.env.assertEqual(last_query['Graph name'], GRAPH_ID, depth=1)
-        self.env.assertEqual(last_query['Query'], query, depth=1)
+        last_query = results[1][-1]
+        self.env.assertGreaterEqual(last_query[0], query_issue_timestamp_ms, depth=1)
+        self.env.assertEqual(last_query[1], QUERY_STAGE_FINISHED, depth=1)
+        self.env.assertEqual(last_query[2], GRAPH_ID, depth=1)
+        self.env.assertEqual(last_query[3], query, depth=1)
+
+    def test05_graph_info_queries_prev_and_current(self):
+        prev_query_string = """CYPHER end=100 RETURN reduce(sum = 0, n IN range(1, $end) | sum ^ n)"""
+        graph = Graph(self.conn, GRAPH_ID)
+
+        results = graph.query(prev_query_string)
+        self.env.assertEquals(results.result_set[0][0], 0, depth=1)
+
+        query_issue_timestamp_ms = get_unix_timestamp_milliseconds()
+        query = """UNWIND (range(0, 10000000)) AS x WITH x AS x WHERE (x / 90000) = 1 RETURN x"""
+        waiter = run_concurrently((query), thread_run_query)
+
+        # Wait until the concurrent query (UNWIND) starts execution: until
+        # another redis client with the query above issues a query and it
+        # starts being executed.
+        self.env.assertTrue(self._wait_for_number_of_clients(2), depth=1)
+        # Got two clients connected.
+
+        # Wait until the query starts execution and get the statistics.
+        info = self._wait_till_queries_start_being_executed(prev=True)
+        self.env.assertIsNotNone(info, depth=1)
+
+        waiter.wait()
+        results = waiter.get().result_set
+        # Validate the GRAPH.QUERY result.
+        self.env.assertEquals(results[0][0], 90000)
+        # The very last query in the combined output should the query
+        # which is currently being executed.
+        current_query = list_to_dict(list_to_dict(info)['Queries'][-1])
+        self.env.assertGreaterEqual(current_query['Received at'], query_issue_timestamp_ms, depth=1)
+        self.env.assertEqual(current_query['Stage'], QUERY_STAGE_EXECUTING, depth=1)
+        self.env.assertEqual(current_query['Graph name'], GRAPH_ID, depth=1)
+        self.env.assertEqual(current_query['Query'], query, depth=1)
+
+        # The query right before the current should be the last executed
+        # (finished) query which we know as we issued it right before the
+        # current one.
+        prev_query = list_to_dict(list_to_dict(info)['Queries'][-2])
+        self.env.assertLessEqual(prev_query['Received at'], query_issue_timestamp_ms, depth=1)
+        self.env.assertEqual(prev_query['Stage'], QUERY_STAGE_FINISHED, depth=1)
+        self.env.assertEqual(prev_query['Graph name'], GRAPH_ID, depth=1)
+        self.env.assertEqual(prev_query['Query'], prev_query_string, depth=1)
+
 
     # TODO
     # 1) Reset some graphs, not all, check the output.
