1766c8e7762a6de05dada15831ec95583506af40
Ofir Moskovich <ofir.moskovich@redislabs.com>
2023-01-08 10:50:12 +0200

    fixes in rdb load of index

--- src/serializers/decoders/current/v13/decode_graph_schema.c
+++ src/serializers/decoders/current/v13/decode_graph_schema.c
@@ -53,6 +53,7 @@ static void _RdbLoadFullTextIndex
 
 	if(!already_loaded) {
 		ASSERT(idx != NULL);
+		Index_DropInternalIndex(idx);
 		Index_SetLanguage(idx, language);
 		Index_SetStopwords(idx, stopwords);
 		Index_ConstructStructure(idx);
@@ -87,11 +88,6 @@ static void _RdbLoadExactMatchIndex
 			Schema_AddIndex(&idx, s, &field, IDX_EXACT_MATCH, false);
 		}
 		RedisModule_Free(field_name);
-	}
-
-	// construct index structure
-	if(!already_loaded) {
-		Index_ConstructStructure(idx);
 	}
 }
 
