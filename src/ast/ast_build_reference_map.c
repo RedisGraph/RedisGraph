58fa4296179ea37c1e0516a772607a77d152d5e0
razmon <razmonsonego2@gmail.com>
2023-01-10 15:37:35 +0200

    build reference map

--- src/ast/ast_build_reference_map.c
+++ src/ast/ast_build_reference_map.c
@@ -322,6 +322,24 @@ static void _AST_MapUnwindClauseReferences
 	_AST_MapExpression(ast, expr);
 }
 
+// maps entities in a FOREACH clause
+static void _AST_MapForeachClauseReferences
+(
+	AST *ast,
+	const cypher_astnode_t *foreach_clause
+) {
+	ASSERT(ast != NULL);
+	ASSERT(foreach_clause != NULL);
+
+	const cypher_astnode_t *exp = cypher_ast_foreach_get_expression(foreach_clause);
+	_AST_MapExpression(ast, exp);
+
+	for(uint i = 0; i < cypher_ast_foreach_nclauses(foreach_clause); i++) {
+		exp = cypher_ast_foreach_get_clause(foreach_clause, i);
+		_AST_MapExpression(ast, exp);
+	}
+}
+
 // maps entities in DELETE clause
 static void _AST_MapDeleteClauseReferences
 (
@@ -444,6 +462,9 @@ static void _ASTClause_BuildReferenceMap(AST *ast, const cypher_astnode_t *claus
 	} else if(type == CYPHER_AST_UNWIND) {
 		// add referenced aliases for UNWIND clause
 		_AST_MapUnwindClauseReferences(ast, clause);
+	} else if(type == CYPHER_AST_FOREACH) {
+		// add referenced aliases for a FOREACH clause
+		_AST_MapForeachClauseReferences(ast, clause);
 	}
 }
 
