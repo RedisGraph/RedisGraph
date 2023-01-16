76eccb9a77b9fdd11fa4c314981c1d36124b2363
Avi Avni <avi.avni@gmail.com>
2023-01-16 19:40:45 +0200

    fix mac ci issue

--- src/execution_plan/ops/op_node_by_label_scan.c
+++ src/execution_plan/ops/op_node_by_label_scan.c
@@ -200,7 +200,9 @@ static OpResult NodeByLabelScanReset(OpBase *ctx) {
 		OpBase_DeleteRecord(op->child_record); // Free old record.
 		op->child_record = NULL;
 	}
-	_ResetIterator(op);
+	if(op->n.label_id == GRAPH_UNKNOWN_LABEL) {
+		_ResetIterator(op);
+	}
 	return OP_OK;
 }
 
