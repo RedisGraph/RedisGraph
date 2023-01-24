57f7c4928e832aece09a1bbf32a6e0f96496b9f6
Victor Polevoy <fx@thefx.co>
2023-01-24 09:38:12 +0100

    Rewrite the tests for acutest

--- tests/unit/test_num.c
+++ tests/unit/test_num.c
@@ -4,29 +4,28 @@
  * the Server Side Public License v1 (SSPLv1).
  */
 
-#include "gtest.h"
+#include "src/util/num.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include "../../src/util/num.h"
-#ifdef __cplusplus
-}
-#endif
+#include "acutest.h"
 
 // There is no need in parametrisation as the code is the same
 // for all the other (wider) types.
-TEST(NumOverflowTest, TestCheckedAddU8) {
+void test_NumOverflow_CheckedAddU8(void) {
     uint8_t out = 0;
     bool is_ok = checked_add_u8(254, 1, &out);
-    ASSERT_EQ(out, 255);
-    ASSERT_EQ(is_ok, true);
+    TEST_ASSERT(out == 255);
+    TEST_ASSERT(is_ok == true);
     is_ok = checked_add_u8(254, 2, &out);
     // The value isn't changed when it is not ok.
-    ASSERT_EQ(out, 0);
-    ASSERT_EQ(is_ok, false);
+    TEST_ASSERT(out == 0);
+    TEST_ASSERT(is_ok == false);
     is_ok = checked_add_u8(254, 100, &out);
     // The value isn't changed when it is not ok.
-    ASSERT_EQ(out, 98);
-    ASSERT_EQ(is_ok, false);
+    TEST_ASSERT(out == 98);
+    TEST_ASSERT(is_ok == false);
 }
+
+TEST_LIST = {
+    {"NumOverflow_CheckedAddU8", test_NumOverflow_CheckedAddU8},
+    {NULL, NULL}
+};
