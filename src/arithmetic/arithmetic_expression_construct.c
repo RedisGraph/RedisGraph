c902f91cc5604141d738951a00caad7f228a9338
nafraf <nafraf@gmail.com>
2023-02-21 21:19:37 -0500

    Remove additional space

--- src/arithmetic/arithmetic_expression_construct.c
+++ src/arithmetic/arithmetic_expression_construct.c
@@ -666,7 +666,7 @@ static AR_ExpNode *_AR_ExpNodeFromComprehensionFunction
 	if(predicate_node) {
 		AST_ConvertFilters(&ctx->ft, predicate_node);
 		// in case of list comprehension, validate that aggregation function is not used in predicate
-		if( func_name == "LIST_COMPREHENSION" && !FilterTree_Valid(ctx->ft, CYPHER_AST_LIST_COMPREHENSION)) {
+		if(func_name == "LIST_COMPREHENSION" && !FilterTree_Valid(ctx->ft, CYPHER_AST_LIST_COMPREHENSION)) {
 			rm_free(ctx);
 			return AR_EXP_NewConstOperandNode(SI_NullVal());
 		}
