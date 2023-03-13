d6366b0bbf0805d3f7c1f18184562fc24f2703af
Avi Avni <avi.avni@gmail.com>
2023-03-13 09:20:07 +0200

    fix indentation

--- src/constraint/unique_constraint.c
+++ src/constraint/unique_constraint.c
@@ -19,10 +19,10 @@ struct _UniqueConstraint {
 	ConstraintType t;              // constraint type
 	EnforcementCB enforce;         // enforcement function
 	int schema_id;                 // enforced schema ID
-    Attribute_ID *attrs;           // enforced attributes
+	Attribute_ID *attrs;           // enforced attributes
 	const char **attr_names;       // enforced attribute names
-    ConstraintStatus status;       // constraint status
-    uint _Atomic pending_changes;  // number of pending changes
+	ConstraintStatus status;       // constraint status
+	uint _Atomic pending_changes;  // number of pending changes
 	GraphEntityType et;            // entity type
 	Index idx;                     // supporting index
 };
@@ -60,25 +60,25 @@ static bool Constraint_EnforceUniqueEntity
 	// TODO: prefer to have the RediSearch query "template" constructed
 	// once and reused for each entity
 
-    SIType t;
-    SIValue *v;
+	SIType t;
+	SIValue *v;
 	uint8_t i     = 0;
-    RSQNode *node = NULL;  // RediSearch query node
-    RSQNode *root = NULL;  // root of RediSearch query tree
+	RSQNode *node = NULL;  // RediSearch query node
+	RSQNode *root = NULL;  // root of RediSearch query tree
 	RSQNode *nodes[_c->n_attr];
-    RSResultsIterator *iter = NULL;
+	RSResultsIterator *iter = NULL;
 	const AttributeSet attributes = GraphEntity_GetAttributes(e);
 
 	//--------------------------------------------------------------------------
 	// create a RediSearch query
 	//--------------------------------------------------------------------------
 
-    for(i = 0; i < _c->n_attr; i++) {
+	for(i = 0; i < _c->n_attr; i++) {
 		Attribute_ID attr_id = _c->attrs[i];
 		const char *field = _c->attr_names[i];
 
 		// get current attribute from entity
-        v = AttributeSet_Get(attributes, attr_id);
+		v = AttributeSet_Get(attributes, attr_id);
 		if(v == ATTRIBUTE_NOTFOUND) {
 			// entity satisfies constraint in a vacuous truth manner
 			holds = true;
@@ -86,25 +86,25 @@ static bool Constraint_EnforceUniqueEntity
 		}
 
 		// create RediSearch query node according to entity attr type
-        t = SI_TYPE(*v);
+		t = SI_TYPE(*v);
 
-	    if(!(t & SI_INDEXABLE)) {
+		if(!(t & SI_INDEXABLE)) {
 			// TODO: see RediSearch MULTI-VALUE index.
 			holds = true;
 			goto cleanup;
-	    } else if(t == T_STRING) {
-            node = RediSearch_CreateTagNode(rs_idx, field);
-            RSQNode *child = RediSearch_CreateTagTokenNode(rs_idx, v->stringval);
-	    	RediSearch_QueryNodeAddChild(node, child);
-        } else {
-            ASSERT(t & SI_NUMERIC || t == T_BOOL);
-		    double d = SI_GET_NUMERIC((*v));
-            node = RediSearch_CreateNumericNode(rs_idx, field, d, d, true, true);
-        }
+		} else if(t == T_STRING) {
+			node = RediSearch_CreateTagNode(rs_idx, field);
+			RSQNode *child = RediSearch_CreateTagTokenNode(rs_idx, v->stringval);
+			RediSearch_QueryNodeAddChild(node, child);
+		} else {
+			ASSERT(t & SI_NUMERIC || t == T_BOOL);
+			double d = SI_GET_NUMERIC((*v));
+			node = RediSearch_CreateNumericNode(rs_idx, field, d, d, true, true);
+		}
 
-        ASSERT(node != NULL);
+		ASSERT(node != NULL);
 		nodes[i] = node;
-    }
+	}
 
 	//--------------------------------------------------------------------------
 	// cancat filters
@@ -124,7 +124,7 @@ static bool Constraint_EnforceUniqueEntity
 	//--------------------------------------------------------------------------
 
 	// constraint holds if there are no duplicates, a single index match
-    iter = RediSearch_GetResultsIterator(root, rs_idx);
+	iter = RediSearch_GetResultsIterator(root, rs_idx);
 	if(Constraint_GetEntityType(c) == GETYPE_NODE) {
 		const EntityID *id =
 			(EntityID*)RediSearch_ResultsIteratorNext(iter, rs_idx, NULL);
@@ -156,7 +156,7 @@ cleanup:
 		}
 	}
 
-    return holds;
+	return holds;
 }
 
 Constraint Constraint_UniqueNew
@@ -168,14 +168,14 @@ Constraint Constraint_UniqueNew
 	GraphEntityType et,       // entity type
 	Index idx                 // index
 ) {
-    UniqueConstraint c = rm_malloc(sizeof(struct _UniqueConstraint));
+	UniqueConstraint c = rm_malloc(sizeof(struct _UniqueConstraint));
 
 	// introduce constraint attributes
 	c->attrs = rm_malloc(sizeof(Attribute_ID) * n_fields);
-    memcpy(c->attrs, fields, sizeof(Attribute_ID) * n_fields);
+	memcpy(c->attrs, fields, sizeof(Attribute_ID) * n_fields);
 
 	c->attr_names = rm_malloc(sizeof(char*) * n_fields);
-    memcpy(c->attr_names, attr_names, sizeof(char*) * n_fields);
+	memcpy(c->attr_names, attr_names, sizeof(char*) * n_fields);
 
 	// initialize constraint
 	c->t               = CT_UNIQUE;
