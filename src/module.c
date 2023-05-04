040fcc0b8fb34357187283b749d12c3d9948621e
swilly22 <roi@redislabs.com>
2023-05-04 10:34:45 +0300

    at the moment graph.info does not expects a key

--- src/module.c
+++ src/module.c
@@ -201,8 +201,8 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
 		return REDISMODULE_ERR;
 	}
 
-	if(RedisModule_CreateCommand(ctx, "graph.INFO", Graph_Info, "readonly", 1, 1,
-								1) == REDISMODULE_ERR) {
+	if(RedisModule_CreateCommand(ctx, "graph.INFO", Graph_Info, "readonly", 0, 0,
+								0) == REDISMODULE_ERR) {
 		return REDISMODULE_ERR;
 	}
 
