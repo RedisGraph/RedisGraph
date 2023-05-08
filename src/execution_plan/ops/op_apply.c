720f16cab4356e82c908eaf24e03bcb9fe4c46ff
swilly22 <roi@redislabs.com>
2023-05-08 19:34:49 +0300

    apply collects all LHS records

--- src/execution_plan/ops/op_apply.c
+++ src/execution_plan/ops/op_apply.c
@@ -16,14 +16,16 @@ static void ApplyFree(OpBase *opBase);
 
 OpBase *NewApplyOp(const ExecutionPlan *plan) {
 	Apply *op = rm_malloc(sizeof(Apply));
-	op->r = NULL;
-	op->op_arg = NULL;
+
+	op->r            = NULL;
+	op->op_arg       = NULL;
+	op->records      = NULL;
+	op->rhs_branch   = NULL;
 	op->bound_branch = NULL;
-	op->rhs_branch = NULL;
 
 	// Set our Op operations
-	OpBase_Init((OpBase *)op, OPType_APPLY, "Apply", ApplyInit, ApplyConsume, ApplyReset, NULL,
-				ApplyClone, ApplyFree, false, plan);
+	OpBase_Init((OpBase *)op, OPType_APPLY, "Apply", ApplyInit, ApplyConsume,
+			ApplyReset, NULL, ApplyClone, ApplyFree, false, plan);
 
 	return (OpBase *)op;
 }
@@ -32,13 +34,15 @@ static OpResult ApplyInit(OpBase *opBase) {
 	ASSERT(opBase->childCount == 2);
 
 	Apply *op = (Apply *)opBase;
-	/* The op's bound branch and optional match branch have already been built as
-	 * the Apply op's first and second child ops, respectively. */
+	// the op's bound branch and optional match branch have already been
+	// built as the Apply op's first and second child ops, respectively
+	op->records      = array_new(Record, 1);
+	op->rhs_branch   = opBase->children[1];
 	op->bound_branch = opBase->children[0];
-	op->rhs_branch = opBase->children[1];
 
-	// Locate branch's Argument op tap.
-	op->op_arg = (Argument *)ExecutionPlan_LocateOp(op->rhs_branch, OPType_ARGUMENT);
+	// locate branch's Argument op tap
+	op->op_arg = (Argument *)ExecutionPlan_LocateOp(op->rhs_branch,
+			OPType_ARGUMENT);
 
 	return OP_OK;
 }
@@ -48,9 +52,14 @@ static Record ApplyConsume(OpBase *opBase) {
 
 	while(true) {
 		if(op->r == NULL) {
-			// Retrieve a Record from the bound branch.
+			// retrieve a Record from the bound branch
 			op->r = OpBase_Consume(op->bound_branch);
-			if(!op->r) return NULL; // Bound branch and this op are depleted.
+			if(op->r == NULL) {
+				return NULL; // Bound branch and this op are depleted.
+			}
+
+			// collect record for future freeing
+			array_append(op->records, op->r);
 
 			// Successfully pulled a new Record, propagate to the top of the RHS branch.
 			if(op->op_arg) {
@@ -58,23 +67,22 @@ static Record ApplyConsume(OpBase *opBase) {
 			}
 		}
 
-		// Pull a Record from the RHS branch.
+		// pull a Record from the RHS branch
 		Record rhs_record = OpBase_Consume(op->rhs_branch);
 
 		if(rhs_record == NULL) {
-			/* RHS branch depleted for the current bound Record;
-			 * free it and loop back to retrieve a new one. */
-			OpBase_DeleteRecord(op->r);
+			// RHS branch depleted for the current bound Record;
+			// free it and loop back to retrieve a new one
 			op->r = NULL;
-			// Reset the RHS branch.
+			// reset the RHS branch
 			OpBase_PropagateReset(op->rhs_branch);
 			continue;
 		}
 
-		// Clone the bound Record and merge the RHS Record into it.
-		Record r = OpBase_DeepCloneRecord(op->r);
+		// clone the bound Record and merge the RHS Record into it
+		Record r = OpBase_CloneRecord(op->r);
 		Record_Merge(r, rhs_record);
-		// Delete the RHS record, as it has been merged into r.
+		// delete the RHS record, as it has been merged into r
 		OpBase_DeleteRecord(rhs_record);
 
 		return r;
@@ -85,10 +93,15 @@ static Record ApplyConsume(OpBase *opBase) {
 
 static OpResult ApplyReset(OpBase *opBase) {
 	Apply *op = (Apply *)opBase;
-	if(op->r) {
-		OpBase_DeleteRecord(op->r);
-		op->r = NULL;
+	op->r = NULL;
+
+	// free collected records
+	uint32_t n = array_len(op->records);
+	for(uint32_t i = 0; i < n; i++) {
+		OpBase_DeleteRecord(op->records[i]);
 	}
+	array_clear(op->records);
+
 	return OP_OK;
 }
 
@@ -98,9 +111,18 @@ static OpBase *ApplyClone(const ExecutionPlan *plan, const OpBase *opBase) {
 
 static void ApplyFree(OpBase *opBase) {
 	Apply *op = (Apply *)opBase;
-	if(op->r) {
-		OpBase_DeleteRecord(op->r);
-		op->r = NULL;
+
+	// free collected records
+	if(op->records != NULL) {
+		uint32_t n = array_len(op->records);
+		for(uint32_t i = 0; i < n; i++) {
+			OpBase_DeleteRecord(op->records[i]);
+		}
+
+		array_free(op->records);
+		op->records = NULL;
 	}
+
+	op->r = NULL;
 }
 
