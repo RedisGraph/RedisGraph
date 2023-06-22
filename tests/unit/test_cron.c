0eb28d5c9686cf7d85260bb73185e88e308541b4
nafraf <nafraf@gmail.com>
2023-06-22 12:26:41 -0500

    WIP: Print time taken in test_AbortRunningTask()

--- tests/unit/test_cron.c
+++ tests/unit/test_cron.c
@@ -387,7 +387,8 @@ static void test_AbortRunningTask() {
 	double time_taken_sec = ((double)t)/CLOCKS_PER_SEC;
 
 	// expecting Cron_AbortTask to return after task completed
-	TEST_ASSERT(time_taken_sec >= 0.1);
+	printf("time_taken_sec = %lf\n", time_taken_sec);
+	TEST_ASSERT(time_taken_sec > 0.099);
 
 	_AddTaskData_Free(data);
 }
