7e99b2d30969a1c2046f7138fd1469101493566f
Raz Monsonego <74051729+raz-mon@users.noreply.github.com>
2023-07-06 09:56:55 +0300

    fix propagate reset (#3129)
    * fix propogate_reset
    
    * fix
    
    * fix
    
    * fix
    
    * address review comments
    
    ---------
    
    Co-authored-by: Roi Lipman <swilly22@users.noreply.github.com>

--- tests/flow/test_index_delete.py
+++ tests/flow/test_index_delete.py
@@ -40,4 +40,96 @@ class testIndexDeletionFlow():
         result = redis_graph.query("CALL db.indexes()")
         self.env.assertEquals(len(result.result_set), 0)
 
-        
+    def test04_drop_fulltext_node_index(self):
+        # create an index over L:title
+        create_fulltext_index(g, 'L', 'title', sync=True)
+
+        # delete index
+        drop_fulltext_index(g, 'L')
+
+        # validate no indexes in graph
+        result = list_indicies(g)
+        self.env.assertEquals(len(result.result_set), 0)
+
+    def test05_drop_index_during_population(self):
+        # 1. populate a graph
+        # 2. create an index and wait for it to be sync
+        # 3. constantly update indexed entities
+        # 4. drop index
+        # 5. validate execution-plan + indexes report
+
+        # populate a graph
+        q = "UNWIND range(0, 1000) AS x CREATE (:N{v:x})"
+        g.query(q)
+
+        # create an index and wait for it to be sync
+        create_node_exact_match_index(g, 'N', 'v', sync=True)
+
+        # constantly update indexed entities
+        # drop index
+        # validate execution-plan + indexes report
+        start_idx = 0
+        end_idx = 100
+        for i in range(start_idx, end_idx):
+            # update indexed entities
+            q = f"MATCH (n:N) WHERE n.v = {i} SET n.v = -n.v"
+            g.query(q)
+
+            if i < end_idx / 2:
+                # validate execution-plan + indexes report
+                plan = g.execution_plan(q)
+                self.env.assertIn("Index", plan)
+
+                indicies = list_indicies(g).result_set
+                self.env.assertEquals(len(indicies), 1)
+
+            elif i == end_idx / 2:
+                # drop index
+                drop_exact_match_index(g, 'N', 'v')
+
+            else:
+                # validate execution-plan + indexes report
+                plan = g.execution_plan(q)
+                self.env.assertNotIn("Index", plan)
+
+                indicies = list_indicies(g).result_set
+                self.env.assertEquals(len(indicies), 0)
+
+    def test06_reset_order(self):
+        """Tests that the reset order is correct, i.e., that the reading ops are
+        reset before the writing ops (otherwise we write while a read-lock is
+        held)."""
+
+        # clear the db
+        self.env.flush()
+        g = Graph(self.env.getConnection(), GRAPH_ID)
+
+        # create data
+        g.query(
+            """
+            WITH 1 AS x
+            CREATE (:X {uid: toString(x)})-[:R]->(y:Y {v: x})
+            """
+        )
+
+        # create an index
+        create_node_exact_match_index(g, 'X', 'uid', sync=True)
+        create_node_exact_match_index(g, 'Y', 'v', sync=True)
+
+        # utilize the index for a scan, followed by a deletion of the indexed
+        # entity and setting of a property on the other entity
+        res = g.query(
+            """
+            MATCH (x:X {uid: '1'})-[:R]->(y:Y)
+            DELETE y
+            SET x.uid = '10'
+            RETURN x
+            """
+        )
+
+        # validate results
+        self.env.assertEquals(res.nodes_deleted, 1)
+        self.env.assertEquals(res.relationships_deleted, 1)
+        self.env.assertEquals(len(res.result_set), 1)
+        self.env.assertEquals(res.result_set[0][0],
+            Node(label='X', properties={'uid': '10'}))
