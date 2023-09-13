8fcf7eb7453d895c863172ce8c0ffac6072a1381
Roi Lipman <swilly22@users.noreply.github.com>
2023-09-13 09:00:11 +0300

    skip aux when there are no graphs in keyspace (#3185)

--- src/serializers/graphcontext_type.c
+++ src/serializers/graphcontext_type.c
@@ -56,8 +56,24 @@ static void _GraphContextType_RdbSave(RedisModuleIO *rdb, void *value) {
 }
 
 // save an unsigned placeholder before and after the keyspace encoding
-static void _GraphContextType_AuxSave(RedisModuleIO *rdb, int when) {
+static void _GraphContextType_AuxSave
+(
+	RedisModuleIO *rdb,
+	int when
+) {
 	RedisModule_SaveUnsigned(rdb, 0);
+}
+
+// save an unsigned placeholder before and after the keyspace encoding
+static void _GraphContextType_AuxSave2
+(
+	RedisModuleIO *rdb,
+	int when
+) {
+	// only write AUX field if there are graphs in the keyspace
+	if(Globals_GetGraphsCount() > 0) {
+		RedisModule_SaveUnsigned(rdb, 0);
+	}
 }
 
 // decode the unsigned placeholders saved before and after the keyspace values
@@ -84,6 +100,14 @@ int GraphContextType_Register(RedisModuleCtx *ctx) {
 	tm.aux_save           =  _GraphContextType_AuxSave;
 	tm.aux_load           =  _GraphContextType_AuxLoad;
 	tm.aux_save_triggers  =  REDISMODULE_AUX_BEFORE_RDB | REDISMODULE_AUX_AFTER_RDB;
+
+	// use aux_save2 if available
+	Redis_Version redis_version = RG_GetRedisVersion();
+	if(redis_version.major > 7 ||
+	  (redis_version.major = 7 && redis_version.minor >= 2)) {
+		tm.aux_save  = NULL;
+		tm.aux_save2 = _GraphContextType_AuxSave2;
+	}
 
 	GraphContextRedisModuleType = RedisModule_CreateDataType(ctx, "graphdata",
 			GRAPH_ENCODING_VERSION_LATEST, &tm);
