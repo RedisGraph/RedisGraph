e04efb45bed1326cb3aa92e156fff53a502cb5c1
nafraf <nafraf@gmail.com>
2023-11-14 16:54:40 -0500

    Revert "fix bulk insertion test"
    This reverts commit 862f9a365cc29b613df95539350d4aeffc20f481.

--- tests/flow/test_bulk_insertion.py
+++ tests/flow/test_bulk_insertion.py
@@ -13,7 +13,7 @@ redis_graph = None
 
 def run_bulk_loader(graphname, filename):
     runner = CliRunner()
-    runner.invoke(bulk_insert, ['--redis-url', f"redis://localhost:{port}",
+    runner.invoke(bulk_insert, ['--port', port,
                                 '--nodes', filename,
                                 graphname])
 
