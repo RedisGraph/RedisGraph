8a57d42db53ab92fc202131e634128bc563e41a2
razmon <razmonsonego2@gmail.com>
2024-05-02 20:09:22 +0300

    Cleanups

--- utils/migrate-graph/check-graph.py
+++ utils/migrate-graph/check-graph.py
@@ -1,90 +1,87 @@
-import re
-import sys
-import json
 import redis
-# from redis.commands.graph import Graph
-# from redis.commands.graph.node import Node
-# from redis.commands.graph.edge import Edge
-import hashlib
 import datetime
-from RLTest import Env
 
 ################################################################################
 # Auxiliary functions
 ################################################################################
-def printTimeStamp():
-    now = datetime.datetime.now()
-    print ("Current date and time : ", now.strftime("%Y-%m-%d %H:%M:%S"))
 
-def printTotalNodes(graph, graph_name):
-    res = graph.query("MATCH (n) RETURN count(n)", read_only=True)
-    print('Graph:', graph_name, ' total nodes:', "{:10,}".format(res.result_set[0][0]))
+def printTimeStamp(begin=True):
+    """Prints the current date and time."""
+
+    now = datetime.datetime.now()
+    print("Date and time", "of test START" if begin else "of test END", f": {now.strftime('%Y-%m-%d %H:%M:%S')}")
+
+def printGraphData(graph):
+    """Prints graph node and edge count, and returns that maximum node-id in the
+    graph."""
+
+    res = graph.query("MATCH (n) RETURN max(id(n)), count(n)", read_only=True)
+    max_node_id = res.result_set[0][0]
+    total_nodes_src = res.result_set[0][1]
+    print('Total nodes:', "{:,}".format(total_nodes_src), 'max_node_id:', "{:,}".format(max_node_id))
+
+    res = graph.query("MATCH ()-[r]->() RETURN max(id(r)), count(r)", read_only=True)
+    max_edge_id = res.result_set[0][0]
+    total_edges_src = res.result_set[0][1]
+    print('Total edges:', "{:,}".format(total_edges_src), 'max_edge_id:', "{:,}".format(max_edge_id))
+
+    return max_node_id
 
 def checkSrcAndDestNodes(src_graph):
+    print(f'Searching for corrupted entities in Graph: {src_graph.NAME}')
     printTimeStamp()
-    printTotalNodes(src_graph, src_graph.NAME)
-    batch_size = 50000
-    res = src_graph.query("MATCH (n) RETURN max(id(n))", read_only=True)
-    max_node_id = res.result_set[0][0]
+    max_node_id = printGraphData(src_graph)
 
-    res = src_graph.query("MATCH ()-[r]->() RETURN max(id(r))", read_only=True)
-    max_edge_id = res.result_set[0][0]
+    batch_size = 10000
+    errors = []
 
-    res = src_graph.query("MATCH ()-[r]->() RETURN count(r)", read_only=True)
-    total_edges_src = res.result_set[0][0]
-    print('SOURCE: total edges:', "{:,}".format(total_edges_src), 'max_edge_id:', max_edge_id)
+    # Queries
+    q1 = "MATCH (n)-[r]->(m) WHERE id(n) = $id_n RETURN id(m), id(r)"
+    q2 = "MATCH (n)<-[r]-(m) WHERE id(n) = $id_n RETURN id(m), id(r)"
 
-    src_node_count = 0
-    dest_node_count = 0
-    total_validations = 0
-    errors = 0
-    for src_node_id in range(0, max_node_id + 1):
-        res = src_graph.query(f"CYPHER id_n={src_node_id} MATCH (n)-[r1]->(m) WHERE id(n) = $id_n WITH n, r1, m MATCH (n)<-[r2]-(p) RETURN id(m), id(r1), id(p), id(r2)", read_only=True)
-        if len(res.result_set) == 0:
-            continue # no edges for this node. Is this an error?
-        
-        for row in res.result_set:
-            id_n = src_node_id
-            id_m = row[0]
-            id_r1 = row[1]
-            id_p = row[2]
-            id_r2 = row[3]
-            # check dest node
-            res = src_graph.query(f"CYPHER id_m = {id_m} MATCH (m) WHERE id(m) = $id_m RETURN 1", read_only=True)
-            if len(res.result_set) == 0:
-                errors += 1
-                print()
-                print(f'Error in path: (n)-[r1]->(m): id(n)={id_n} id(r1)={id_r1}, id(m)={id_m}')
-                print(f'Node with id {id_m} not found.')
-            dest_node_count += 1
-            total_validations += 1
-            # Check source node
-            res = src_graph.query(f"CYPHER id_p = {id_p} MATCH (p) WHERE id(p) = $id_p RETURN 1", read_only=True)
-            if len(res.result_set) == 0:
-                errors += 1
-                print()
-                print(f'Error in path: (n)<-[r2]-(p): id(n)={id_n} id(r2)={id_r2}, id(m)={id_p}')
-                print(f'Node with id {id_p} not found.')
-            src_node_count += 1
-            total_validations += 1
-            if total_validations % batch_size == 0:
-                print("{:10,}".format(total_validations), 'validations done,', 'current_node_id', "{:10,}".format(src_node_id))
+    # for node_id in range(0, max_node_id + 1):
+    for node_id in range(max_node_id + 1):
+        for q in [q1, q2]:
+            results = src_graph.query(f"CYPHER id_n={node_id} {q}", read_only=True).result_set
+            if len(results) == 0:
+                # no edges for this node.
+                continue
+            
+            for row in results:
+                id_n = node_id
+                id_m = row[0]
+                id_r = row[1]
+                # check other node
+                res = src_graph.query(f"CYPHER id_m = {id_m} MATCH (m) WHERE id(m) = $id_m RETURN 1", read_only=True)
+                if len(res.result_set) == 0:
+                    err_msg = 'Error in path: ' + (f'(n)-[r]->(m):' if q is q1 else f'(n)<-[r]-(m):') + f' id(n)={id_n} id(r)={id_r}, id(m)={id_m}: Node with id {id_m} not found.'
+                    errors.append(err_msg)
 
-    print("{:10,}".format(total_validations), 'validations done,', 'current_node_id', "{:10,}".format(src_node_id))
-    print('Destination and Source nodes validation finished')
-    print('Errors:', errors)
-    printTimeStamp()
+        if node_id % batch_size == 0:
+            print(f'Traversed {node_id} node ids...')
+
+    if node_id % batch_size != 0:
+        print(f'Traversed {node_id} node ids...')
+
+    print('Traversal finished!')
+    printTimeStamp(False)
+
+    print(f'Encountered {len(errors)} errors overall{":" if len(errors) > 0 else ""}')
+    for e in errors:
+        print(e)
 
 ################################################################################
 # Main
 ################################################################################
 
-HOST = "localhost" 
+HOST = "localhost"
 PORT = 6379
+# HOST = "redis-18763.raz-mon-aws-cluster-kabmc.env0.qa.redislabs.com"
+# PORT = 18763
 
-# src = redis.Redis(host=HOST, port=PORT, password="<>",ssl_cert_reqs="none",ssl=True,decode_responses=True)
-src = redis.Redis(host='localhost', port=6379, decode_responses=True)
+src = redis.Redis(host=HOST, port=PORT, decode_responses=True)
+graph = src.graph('Merchant')
 
-src_graph = src.graph('Merchant')
-
-checkSrcAndDestNodes(src_graph)
+if __name__ == "__main__":
+    # Search for corrupted edges in the given graph.
+    checkSrcAndDestNodes(graph)
