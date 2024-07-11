b884e2a1b4c83a0ce1701956d5b5ee4780bd7d3f
razmon <razmonsonego2@gmail.com>
2024-07-11 18:25:20 +0300

    Add rhel7 to non-sudo OSs

--- sbin/system-setup.py
+++ sbin/system-setup.py
@@ -45,7 +45,8 @@ class RedisGraphSetup(paella.Setup):
         self.run("%s/bin/getepel" % READIES, sudo=True)
         self.run("%s/bin/getgcc --modern" % READIES)
         self.install("m4 libgomp")
-        self.install_peg(sudo=False if self.osnick in ['rocky8', 'amzn2'] else True)
+        print(self.osnick)
+        self.install_peg(sudo=False if self.osnick in ['rhel8', 'rocky8', 'amzn2'] else True)
 
     def fedora(self):
         self.run("%s/bin/getgcc" % READIES)
